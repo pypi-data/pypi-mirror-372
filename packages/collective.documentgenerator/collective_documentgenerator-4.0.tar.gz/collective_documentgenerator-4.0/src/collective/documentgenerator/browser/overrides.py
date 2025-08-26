d3ef484c8c342d1c6f2f91e775b1293a0dbb3b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 15:03:36 +0000

    Add package collective.documentgenerator/4.0/collective_documentgenerator-4.0.tar.gz

---
+++ packages/collective.documentgenerator/collective_documentgenerator-4.0.tar.gz/collective_documentgenerator-4.0/src/collective/documentgenerator/browser/overrides.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from collective.documentgenerator.browser.views import ViewConfigurablePodTemplate
+from collective.documentviewer.browser.views import DocumentViewerView
+
+
+class DGDXDocumentViewerView(ViewConfigurablePodTemplate, DocumentViewerView):
+    """The documentviewer view from collective.documentviewer overrides the default DX
+    view, so override it again so our own override is taken into account."""
---
+++ packages/collective.documentgenerator/collective_documentgenerator-4.0.tar.gz/collective_documentgenerator-4.0/src/collective/documentgenerator/helper/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+from collective.documentgenerator.helper.base import DisplayProxyObject
+from collective.documentgenerator.helper.base import DocumentGenerationHelperView
+from collective.documentgenerator.helper.dexterity import DXDisplayProxyObject
+from collective.documentgenerator.helper.dexterity import DXDocumentGenerationHelperView
