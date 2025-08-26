3b02630bd7e133bcc12d05c33491e66a05ec451f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:05:23 +0000

    Add package alita-sdk/0.3.271/alita_sdk-0.3.271.tar.gz

---
+++ packages/alita-sdk/alita_sdk-0.3.271.tar.gz/alita_sdk-0.3.271/alita_sdk/configurations/service_now.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, Field, SecretStr
+
+
+class ServiceNowConfiguration(BaseModel):
+    model_config = ConfigDict(
+        json_schema_extra={
+            "metadata": {
+                "label": "ServiceNow",
+                "icon_url": "servicenow.svg",
+                "sections": {
+                    "auth": {
+                        "required": True,
+                        "subsections": [
+                            {
+                                "name": "Username & Password",
+                                "fields": ["username", "password"]
+                            }
+                        ]
+                    },
+                },
+                "section": "credentials",
+                "type": "service_now",
+                "categories": ["service management"],
+                "extra_categories": ["servicenow", "itsm", "service management", "incident"],
+            }
+        }
+    )
+    base_url: str = Field(description="ServiceNow URL")
+    username: Optional[str] = Field(description="ServiceNow Username", default=None)
+    password: Optional[SecretStr] = Field(description="ServiceNow Password", default=None)
