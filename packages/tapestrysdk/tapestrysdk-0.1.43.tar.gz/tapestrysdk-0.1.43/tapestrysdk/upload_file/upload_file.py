f2bbdc0ec4a51b7cb45c832cc710f488e182d588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:26 +0000

    Add package tapestrysdk/0.1.43/tapestrysdk-0.1.43.tar.gz

---
+++ packages/tapestrysdk/tapestrysdk-0.1.43.tar.gz/tapestrysdk-0.1.43/tapestrysdk/upload_file/upload_file.py
@@ -0,0 +1,33 @@
+import requests
+
+def upload_file(token, tapestry_id, file_url, file_title, description, caption):
+    url =  "https://inthepicture.org/admin/uplaod_file"
+    headers = {
+        "accept": "application/json, text/plain, */*",
+        "authorization": f"Bearer {token}",
+        "content-type": "application/json",
+    }
+    data = {
+        "tapestry_id": tapestry_id,
+        "file_url": file_url,
+        "file_title": file_title,
+        "description": description,
+        "caption": caption,
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    try:
+        resp_json = response.json()
+    except Exception:
+        resp_json = {}
+
+    if response.status_code == 200 and resp_json.get("success", False):
+        return resp_json
+    else:
+        message = resp_json.get("message", response.text)
+        return {
+            "success": False,
+            "code": response.status_code,
+            "message": message,
+            "body": {}
+        }
