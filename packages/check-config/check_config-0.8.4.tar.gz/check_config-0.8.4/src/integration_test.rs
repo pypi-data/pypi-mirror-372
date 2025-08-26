394149fa24103143871ddfc315eef085bf0c942b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 15:03:43 +0000

    Add package check-config/0.8.4/check_config-0.8.4.tar.gz

---
+++ packages/check-config/check_config-0.8.4.tar.gz/check_config-0.8.4/src/integration_test.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    use crate::checkers;
+    use crate::cli;
+    use dircpy::*;
+
+    #[test]
+    fn test_example() {
+        let _ = std::fs::remove_dir_all("output");
+
+        CopyBuilder::new("example/input", "output").run().unwrap();
+
+        let file_with_checks = cli::parse_path_str_to_uri("example/pyproject.toml").unwrap();
+        let checks =
+            checkers::read_checks_from_path(&file_with_checks, vec!["tool", "check-config"]);
+
+        let (action_count, success_count) = cli::run_checks(&checks, true, true);
+
+        assert_eq!(action_count, 0);
+        assert_eq!(success_count, 25);
+        assert!(!dir_diff::is_different("output", "example/expected_output").unwrap());
+
+        // std::fs::remove_dir_all("output").unwrap();
+    }
+}
