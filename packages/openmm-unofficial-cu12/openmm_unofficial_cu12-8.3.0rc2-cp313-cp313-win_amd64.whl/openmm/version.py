9b52901cb380be4b77e97ca143b582b5864c369a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:14 +0000

    Add package openmm-unofficial-cu12/8.3.0rc2/openmm_unofficial_cu12-8.3.0rc2-cp313-cp313-win_amd64.whl

---
+++ packages/openmm-unofficial-cu12/openmm_unofficial_cu12-8.3.0rc2-cp313-cp313-win_amd64.whl/openmm/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM OPENMM SETUP.PY
+from openmm import openmm_library_path
+
+short_version = '8.3.0'
+version = '8.3.0'
+full_version = '8.3.0.dev-997aacc'
+git_revision = '997aaccb96f82685e7b556a5adf2342a1f1e743a'
+release = False
+
+if not release:
+    version = full_version
