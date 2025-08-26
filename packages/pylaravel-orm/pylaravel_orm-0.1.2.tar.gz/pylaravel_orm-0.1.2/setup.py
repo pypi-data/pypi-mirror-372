791ac4eb1fc1f7d2cd8fa037c11fd696119a1fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:15 +0000

    Add package pylaravel-orm/0.1.2/pylaravel_orm-0.1.2.tar.gz

---
+++ packages/pylaravel-orm/pylaravel_orm-0.1.2.tar.gz/pylaravel_orm-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pylaravel-orm",
+    version="0.1.2",
+    author="Meysam Afghan",
+    author_email="meysamnoori010@gmail.com",
+    description="A Laravel-like ORM for MySQL in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hshmatullahnoor/pyLaravel-orm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "mysql-connector-python",
+    ],
+)
