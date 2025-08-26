a15f3552bfc56587fac66d1e69af758fbf85132d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 15:03:45 +0000

    Add package cmsdism/0.3.3/cmsdism-0.3.3.tar.gz

---
+++ packages/cmsdism/cmsdism-0.3.3.tar.gz/cmsdism-0.3.3/dismcli/lib/start_api/runtimes/docker_runtime.py
@@ -0,0 +1,57 @@
+import logging
+import signal
+import sys
+import traceback
+
+import docker
+from docker.errors import APIError, ImageNotFound
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DockerRuntime:
+    def __init__(self, args: dict) -> None:
+        self.client = docker.from_env()
+        self.args = args
+        self.container = None
+
+    def run(self):
+        try:
+            image = self.args.get("image")
+            if image is None:
+                raise ValueError("No image specified in arguments")
+
+            # Pull the image if it doesn't exist locally
+            try:
+                self.client.images.get(image)
+            except ImageNotFound:
+                LOGGER.info("Image not found locally. Pulling '%s'...", image)
+                for event in self.client.api.pull(image, stream=True, decode=True):
+                    LOGGER.info("%s", event)
+                LOGGER.info("Image '%s' pulled successfully", image)
+            except APIError:
+                LOGGER.error("Podman API error: %s", traceback.format_exc())
+                raise
+
+            # Start the container
+            self.container = self.client.containers.run(**self.args)
+            LOGGER.info("Container started. Press Ctrl+C to stop.")
+
+            # Handle SIGINT (Ctrl+C) to stop and remove container
+            signal.signal(signal.SIGINT, self.cleanup)
+
+            # Stream logs in real-time
+            for line in self.container.logs(stream=True, follow=True, stdout=True, stderr=True):
+                sys.stdout.buffer.write(line)
+                sys.stdout.flush()
+        except Exception:  # noqa: BLE001
+            LOGGER.error("Error starting container: %s", traceback.format_exc())
+            self.cleanup()
+
+    def cleanup(self, signum=None, frame=None):
+        """Stops and removes the container on Ctrl+C"""
+        if self.container:
+            self.container.stop()
+            # We do not need to explicitly remove the container, since we use "remove=True" when creating it
+            # self.container.remove()
