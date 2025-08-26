1ee8daeeae11fa0b83adddada149171c95cf07e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:28 +0000

    Add package ichec-django-core/0.0.4/ichec_django_core-0.0.4.tar.gz

---
+++ packages/ichec-django-core/ichec_django_core-0.0.4.tar.gz/ichec_django_core-0.0.4/src/ichec_django_core/urls.py
@@ -0,0 +1,29 @@
+from django.urls import include, path
+from django.contrib import admin
+from django.conf import settings
+
+from .view_sets import (
+    PortalMemberViewSet,
+    GroupViewSet,
+    OrganizationViewSet,
+    PortalFeedbackViewSet,
+    CustomAuthToken,
+)
+
+
+def register_drf_views(router):
+    router.register(r"groups", GroupViewSet)
+    router.register(r"organizations", OrganizationViewSet)
+    router.register(r"members", PortalMemberViewSet)
+    router.register(r"portal_feeedback", PortalFeedbackViewSet)
+    return router
+
+
+urlpatterns = [
+    path("api-token-auth/", CustomAuthToken.as_view()),
+    path(f"{settings.API_AUTH_URL}/", include("rest_framework.urls")),
+    path(f"{settings.ADMIN_URL}/", admin.site.urls),
+]
+
+if settings.WITH_OIDC:
+    urlpatterns += [path("oidc/", include("mozilla_django_oidc.urls"))]
