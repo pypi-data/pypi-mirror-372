1477733a3dde64879c136a65c7158e9d26076ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:37 +0000

    Add package PySide6-Addons/6.9.2/pyside6_addons-6.9.2-cp39-abi3-win_arm64.whl

---
+++ packages/PySide6-Addons/pyside6_addons-6.9.2-cp39-abi3-win_arm64.whl/PySide6/_config.py
@@ -0,0 +1,17 @@
+built_modules = list(name for name in
+    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;DBus;Designer;Xml;Help;Multimedia;MultimediaWidgets;Pdf;PdfWidgets;Positioning;Location;NetworkAuth;Nfc;Qml;Quick;Quick3D;QuickControls2;QuickTest;QuickWidgets;RemoteObjects;Scxml;Sensors;SerialPort;SerialBus;StateMachine;TextToSpeech;Charts;SpatialAudio;Svg;SvgWidgets;Graphs;GraphsWidgets;Bluetooth;UiTools;AxContainer;WebChannel;WebSockets;HttpServer"
+    .split(";"))
+
+shiboken_library_soversion = "6.9"
+pyside_library_soversion = "6.9"
+
+version = "6.9.2"
+version_info = (6, 9, 2, "", "")
+
+__build_date__ = '2025-08-19T07:10:44+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.9.2'
+
