da1ec2653825c8787d5e1fc8fa2d7caed6cf66d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:04:56 +0000

    Add package Hawc2Models/0.0.2/hawc2models-0.0.2-py3-none-any.whl

---
+++ packages/Hawc2Models/hawc2models-0.0.2-py3-none-any.whl/hawc2models/reference_models/dtu10mw.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import shutil
+
+
+from hawc2models.utils import download
+from hawc2models.reference_models._reference_model import ReferenceModel
+from wetb.hawc2.htc_file import HTCFile
+
+
+class DTU10MW(HTCFile, ReferenceModel):
+    def __init__(self, folder='DTU_10MW_RWT'):
+        folder = Path(folder)
+        if not folder.exists():
+            f_lst = download(
+                'https://gitlab.windenergy.dtu.dk/rwts/dtu-10mw-rwt/-/archive/master/dtu-10mw-rwt-master.zip',
+                known_hash='49c589e7a4b07b3c9ab5dbb88485b6a29fc36fb6df0524ad734eaa24ec86125d',
+                unzip=True)
+            cache_dir = f_lst[0][:f_lst[0].index('.zip.unzip')] + \
+                '.zip.unzip/dtu-10mw-rwt-master/aeroelastic_models/hawc2'
+            shutil.copytree(cache_dir, folder, dirs_exist_ok=True)
+        HTCFile.__init__(self, folder / 'htc/DTU_10MW_RWT_wind_steps.htc')
+
+        self.set_name('dtu_10mw_rwt')
