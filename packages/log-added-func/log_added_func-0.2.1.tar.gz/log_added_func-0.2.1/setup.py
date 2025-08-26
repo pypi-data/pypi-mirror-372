02163fea16db75925ef4b623275dd1b83409706d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 15:03:40 +0000

    Add package log-added-func/0.2.1/log_added_func-0.2.1.tar.gz

---
+++ packages/log-added-func/log_added_func-0.2.1.tar.gz/log_added_func-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", 'r',encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="log_added_func",
+    version="0.2.1",
+    author="YCKAO",
+    author_email="kao0983326221@gmail.com",
+    description="log can be inserted at the desired location.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YCkao5888/log_added_func.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+
+    # 依賴模塊
+    install_requires=[
+        "concurrent-log-handler",
+        "psutil",
+    ],
+
+    # python版本
+    python_requires=">=3",
+)
