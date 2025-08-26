f2aa1628b1587be0b5034f47d4026866e09f0a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-26 14:05:29 +0000

    Add package dtmapi/0.1.4/dtmapi-0.1.4.tar.gz

---
+++ packages/dtmapi/dtmapi-0.1.4.tar.gz/dtmapi-0.1.4/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import find_packages, setup
+
+setup(
+    name="dtmapi",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=["requests", "pandas"],
+    description="A Python package for fetching data from the IOM's Displacement Tracking Matrix (DTM).",
+    author="Luong Bang Tran",
+    author_email="lutran@iom.int",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Github": "https://github.com/Displacement-Tracking-Matrix/dtmapi",
+        "Documentation": "https://dtmapi.readthedocs.io/en/latest/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
