a17a723bb0beb9ba61eef9d4dcd4b9970ecae7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:17 +0000

    Add package django-dsfr/3.1.0/django_dsfr-3.1.0-py3-none-any.whl

---
+++ packages/django-dsfr/django_dsfr-3.1.0-py3-none-any.whl/dsfr/checksums.py
@@ -0,0 +1,24 @@
+# Integrity checks for the css/js/favicon files
+# Do not update manually!
+# Generated with the following command:
+# python manage.py integrity_checksums
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.module.min.js
+INTEGRITY_JS_MODULE = (
+    "sha384-41GU+RiqAUYv47wDfp2YXZiDUjD3kF13IEv7OxPu+c/vI226zUzWCLaxG3YFr+OE"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.nomodule.min.js
+INTEGRITY_JS_NOMODULE = (
+    "sha384-hm15UcyKDaqIboxtF7Jb9k5IxNtmYydj2vXiMAx0TaLn0tAI3hdjG/Gf0DxkYuHC"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.min.css
+INTEGRITY_CSS = (
+    "sha384-xZ7d1FPuJPGbW8x0qwQK1S+SbDZzOJ/Aqxij+8BTibC5+4mOznRXv3dREsSH8LUY"
+)
+
+# dsfr/static/dsfr/dist/utility/utility.min.css
+INTEGRITY_UTILITY_CSS = (
+    "sha384-fBRQe7b44hF/DeP+UL2ICVn+B9+t9YeHkHwp3d63HzY232x0KvifTaUjtByMIQL7"
+)
