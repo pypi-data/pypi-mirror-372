0755dd625e0a8979164c7af5f5df357521f933c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 17:06:27 +0000

    Add package emmaemb/1.1.0/emmaemb-1.1.0-py3-none-any.whl

---
+++ packages/emmaemb/emmaemb-1.1.0-py3-none-any.whl/emmaemb/config.py
@@ -0,0 +1,15 @@
+
+
+EMB_SPACE_COLORS = ["#56638A", "#BAD9B5", "#D77A61", "#D77A61", "#40531B", "#56638A", "#BAD9B5", "#D77A61", "#D77A61", "#40531B", "#56638A", "#BAD9B5", "#D77A61", "#D77A61", "#40531B"]
+
+DISTANCE_METRIC_ALIASES = {
+    "euclidean": "Euclidean",
+    "cityblock": "Manhattan",
+    "cosine": "Cosine",
+    "sqeuclidean": "Standardised Euclidean",
+    "sqeuclidean_normalised": "Normalised Standardised Euclidean",
+    "euclidean_normalised": "Normalised Euclidean",
+    "cityblock_normalised": "Normalised Manhattan",
+    "adjusted_cosine": "Adjusted Cosine",
+    "mahalanobis": "Mahalanobis",
+}
