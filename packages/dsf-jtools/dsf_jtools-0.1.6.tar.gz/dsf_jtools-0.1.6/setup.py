df5a843d5e535fa4bd934b004116ba2fa01a9891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:17 +0000

    Add package dsf-jtools/0.1.6/dsf_jtools-0.1.6.tar.gz

---
+++ packages/dsf-jtools/dsf_jtools-0.1.6.tar.gz/dsf_jtools-0.1.6/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+from jtools import __name__, __version__
+
+setup(
+    name="dsf-" + __name__,
+    version=__version__,
+    author='DSFish',
+    author_email='liumingshuo1017@gmail.com',
+    description='A simple tools module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DataScienceFish/jtools',
+    packages=find_packages(),
+    package_data={
+        'jtools': ['data/*.csv'],  # 包含 jtools/data 文件夹中的所有 CSV 文件
+    },
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9.12',
+)
