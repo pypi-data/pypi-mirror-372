f87f34ec20885b83b70dcbac118fc4f3ea4fea04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:05:45 +0000

    Add package pg-stage/0.4.1/pg_stage-0.4.1-py3-none-any.whl

---
+++ packages/pg-stage/pg_stage-0.4.1-py3-none-any.whl/pg_stage/types.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from typing import Any, Callable, Dict, List
+
+from typing_extensions import TypedDict
+
+
+class OperationChoices(Enum):
+    """Описание видов операций"""
+
+    equal = 'equal'
+    not_equal = 'not_equal'
+    by_pattern = 'by_pattern'
+
+
+class ConditionType(TypedDict):
+    """Описание типа условий для обфускации"""
+
+    column_name: str
+    operation: OperationChoices
+    value: str
+
+
+class RelationType(TypedDict):
+    """Описание типа зависимых таблиц"""
+
+    table_name: str
+    column_name: str
+    from_column_name: str
+    to_column_name: str
+
+
+ConditionTypeMany = List[ConditionType]
+RelationTypeMany = List[RelationType]
+
+
+class MapTablesValueType(TypedDict):
+    """Описание типа значения карты таблиц"""
+
+    mutation_name: str
+    mutation_func: Callable[..., str]
+    mutation_kwargs: Dict[str, Any]
+    mutation_relations: RelationTypeMany
+    mutation_conditions: ConditionTypeMany
+
+
+MapTablesValueTypeMany = List[MapTablesValueType]
