993f1c932ad01298de0d246943846045b1c4efe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:19 +0000

    Add package titiler-application/0.23.0/titiler_application-0.23.0-py3-none-any.whl

---
+++ packages/titiler-application/titiler_application-0.23.0-py3-none-any.whl/titiler/application/settings.py
@@ -0,0 +1,42 @@
+"""Titiler API settings."""
+
+from typing import Optional
+
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ApiSettings(BaseSettings):
+    """FASTAPI application settings."""
+
+    name: str = "TiTiler"
+    cors_origins: str = "*"
+    cors_allow_methods: str = "GET"
+    cachecontrol: str = "public, max-age=3600"
+    root_path: str = ""
+    debug: bool = False
+
+    disable_cog: bool = False
+    disable_stac: bool = False
+    disable_mosaic: bool = False
+
+    lower_case_query_parameters: bool = False
+
+    telemetry_enabled: bool = False
+
+    # an API key required to access any endpoint, passed via the ?access_token= query parameter
+    global_access_token: Optional[str] = None
+
+    model_config = SettingsConfigDict(
+        env_prefix="TITILER_API_", env_file=".env", extra="ignore"
+    )
+
+    @field_validator("cors_origins")
+    def parse_cors_origin(cls, v):
+        """Parse CORS origins."""
+        return [origin.strip() for origin in v.split(",")]
+
+    @field_validator("cors_allow_methods")
+    def parse_cors_allow_methods(cls, v):
+        """Parse CORS allowed methods."""
+        return [method.strip().upper() for method in v.split(",")]
