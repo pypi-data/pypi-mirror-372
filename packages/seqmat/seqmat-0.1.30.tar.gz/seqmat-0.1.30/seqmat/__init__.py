a4c9c108fa8b115f4656b896091040f177bda55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:45 +0000

    Add package seqmat/0.1.30/seqmat-0.1.30.tar.gz

---
+++ packages/seqmat/seqmat-0.1.30.tar.gz/seqmat-0.1.30/seqmat/__init__.py
@@ -0,0 +1,50 @@
+"""
+SeqMat - Lightning-fast genomic sequence matrix library
+
+A comprehensive Python library for genomic sequence analysis with full mutation tracking,
+splicing analysis, and sequence manipulation.
+"""
+
+__version__ = "0.1.30"
+__author__ = "Nicolas Lynn Vila"
+__email__ = "nicolasalynn@gmail.com"
+
+from .seqmat import SeqMat
+from .gene import Gene
+from .transcript import Transcript
+from .config import get_default_organism
+from .utils import (
+    setup_genomics_data, 
+    load_config, 
+    save_config,
+    list_available_organisms,
+    list_supported_organisms,
+    get_organism_info,
+    list_gene_biotypes,
+    count_genes,
+    get_gene_list,
+    data_summary,
+    print_data_summary,
+    search_genes,
+    get_all_genes
+)
+
+__all__ = [
+    "SeqMat",
+    "Gene", 
+    "Transcript",
+    "get_default_organism",
+    "setup_genomics_data",
+    "load_config",
+    "save_config",
+    "list_available_organisms",
+    "list_supported_organisms", 
+    "get_organism_info",
+    "list_gene_biotypes",
+    "count_genes",
+    "get_gene_list",
+    "data_summary",
+    "print_data_summary",
+    "search_genes",
+    "get_all_genes"
+]
