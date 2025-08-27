68a04c6b9d8780529fb163a594a5023a9ef64167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 15:05:19 +0000

    Add package maas-backoff-scheduler/1.0.2/maas_backoff_scheduler-1.0.2-py2-none-any.whl

---
+++ packages/maas-backoff-scheduler/maas_backoff_scheduler-1.0.2-py2-none-any.whl/backoff/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+任务退避管理工具包
+"""
+
+import logging
+from backoff.utils.logging_utils import init_logging
+
+# 全局初始化日志配置
+import os
+from pathlib import Path
+current_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+config_path = current_dir / 'conf' / 'logging.config.yaml'
+
+print(f"任务退避管理工具包初始化，配置文件路径: {config_path}")
+init_logging(config_path)
+logger = logging.getLogger()
