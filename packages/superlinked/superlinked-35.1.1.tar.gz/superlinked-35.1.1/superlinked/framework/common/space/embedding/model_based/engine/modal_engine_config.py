a7aef57bc7a7843dea6e815c793d82403dd75c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:25 +0000

    Add package superlinked/35.1.1/superlinked-35.1.1.tar.gz

---
+++ packages/superlinked/superlinked-35.1.1.tar.gz/superlinked-35.1.1/superlinked/framework/common/space/embedding/model_based/engine/modal_engine_config.py
@@ -0,0 +1,43 @@
+# Copyright 2024 Superlinked, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+
+from typing_extensions import override
+
+from superlinked.framework.common.space.embedding.model_based.engine.embedding_engine_config import (
+    EmbeddingEngineConfig,
+)
+
+
+@dataclass(frozen=True)
+class ModalEngineConfig(EmbeddingEngineConfig):  # pylint: disable=too-many-instance-attributes
+    modal_app_name: str = "App"
+    modal_class_name: str = "Embedder"
+    modal_environment_name: str = "main"
+    modal_batch_size: int = 5000
+    modal_max_retries: int = 10
+    modal_retry_delay: float = 0.2
+
+    @override
+    def __str__(self) -> str:
+        attributes = [
+            f"modal_app_name={self.modal_app_name}",
+            f"modal_class_name={self.modal_class_name}",
+            f"modal_environment_name={self.modal_environment_name}",
+            f"modal_batch_size={self.modal_batch_size}",
+            f"modal_max_retries={self.modal_max_retries}",
+            f"modal_retry_delay={self.modal_retry_delay}",
+        ]
+        return f"{super().__str__()}, " + ", ".join(attributes)
