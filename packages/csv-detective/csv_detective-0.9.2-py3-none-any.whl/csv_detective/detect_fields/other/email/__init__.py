b8262a3aefae78387f6445e428fd297987163560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 05:05:09 +0000

    Add package csv-detective/0.9.2/csv_detective-0.9.2-py3-none-any.whl

---
+++ packages/csv-detective/csv_detective-0.9.2-py3-none-any.whl/csv_detective/detect_fields/other/email/__init__.py
@@ -0,0 +1,10 @@
+import re
+
+PROPORTION = 1
+
+
+def _is(val):
+    """Detects e-mails"""
+    return isinstance(val, str) and bool(
+        re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$", val)
+    )
