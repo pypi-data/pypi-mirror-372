a0db0b71f83faa3db4c7ebff440dde3c81c4e66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 05:05:03 +0000

    Add package biom-format/2.1.17/biom_format-2.1.17.tar.gz

---
+++ packages/biom-format/biom_format-2.1.17.tar.gz/biom_format-2.1.17/README.md
@@ -0,0 +1,28 @@
+The Biological Observation Matrix (BIOM) format
+===============================================
+
+[![biom-format CI](https://github.com/biocore/biom-format/actions/workflows/python-package-conda.yml/badge.svg)](https://github.com/biocore/biom-format/actions/workflows/python-package-conda.yml)
+
+The BIOM file format (canonically pronounced *biome*) is designed to be a general-use format for representing counts of observations (e.g., OTUs, KO categories, lipid types) in one or more biological samples (e.g., microbiome samples, genomes, metagenomes).
+
+Further details can be found at http://biom-format.org.
+
+Installation
+------------
+
+The easiest way to install the latest version of BIOM-Format is through [conda-forge](https://conda-forge.org/):
+
+```
+$ conda install -c conda-forge biom-format
+```
+
+Alternatively, you can install the latest version through [PyPI](https://pypi.org/):
+
+```
+$ pip install biom-format
+```
+
+Getting help
+------------
+
+To get help with biom, please open an issue on the Github [tracker](https://github.com/biocore/biom-format/issues). 
---
+++ packages/biom-format/biom_format-2.1.17.tar.gz/biom_format-2.1.17/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2011-2020, The BIOM Format Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+long_description = """BIOM: Biological Observation Matrix
+http://www.biom-format.org
+
+The Biological Observation Matrix (BIOM) format or: how I learned to stop
+worrying and love the ome-ome
+Daniel McDonald, Jose C Clemente, Justin Kuczynski, Jai Ram Rideout,
+Jesse Stombaugh, Doug Wendel, Andreas Wilke, Susan Huse, John Hufnagle,
+Folker Meyer, Rob Knight, J Gregory Caporaso
+GigaScience 2012, 1:7.
+"""
+
+
+def get_extensions():
+    import numpy as np
+    from Cython.Build import cythonize
+
+    extensions = [
+        Extension("biom._filter",
+                  ["biom/_filter.pyx"],
+                  include_dirs=[np.get_include()]),
+        Extension("biom._transform",
+                  ["biom/_transform.pyx"],
+                  include_dirs=[np.get_include()]),
+        Extension("biom._subsample",
+                  ["biom/_subsample.pyx"],
+                  include_dirs=[np.get_include()]),
+    ]
+    return cythonize(extensions, force=True)
+
+
+setup(ext_modules=get_extensions())
