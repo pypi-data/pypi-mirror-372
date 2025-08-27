2cf7b0ba154af263950f95a96b64d98fe991500e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:18 +0000

    Add package export_to_obsidian/0.3.8/export_to_obsidian-0.3.8.tar.gz

---
+++ packages/export_to_obsidian/export_to_obsidian-0.3.8.tar.gz/export_to_obsidian-0.3.8/src/utils/template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author : bGZo
+@Date : 2025-07-27
+@Links : https://github.com/bGZo
+"""
+from dataclasses import dataclass
+
+# Webpage Meta Template
+@dataclass
+class WebPage:
+    title: str
+    source: str
+    created: str
+    modified: str
+    type: str
