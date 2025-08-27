0ce875c5c7ab4a18893c44aac41a9f5ca3b28d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:37 +0000

    Add package plugflow/1.0.3/plugflow-1.0.3-py3-none-any.whl

---
+++ packages/plugflow/plugflow-1.0.3-py3-none-any.whl/plugflow/__init__.py
@@ -0,0 +1,6 @@
+
+from .base import BasePlugin
+from .manager import PluginManager
+from ._version import __version__, __author__, __email__, __description__
+
+__all__ = ["BasePlugin", "PluginManager", "__version__", "__author__", "__email__", "__description__"]
