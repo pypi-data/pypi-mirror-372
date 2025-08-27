ff184695d562a731c5a34a9fa7cabbdb29954ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:06:13 +0000

    Add package terminal-shop/1.13.1/terminal_shop-1.13.1-py3-none-any.whl

---
+++ packages/terminal-shop/terminal_shop-1.13.1-py3-none-any.whl/terminal_shop/types/token.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .._models import BaseModel
+
+__all__ = ["Token"]
+
+
+class Token(BaseModel):
+    id: str
+    """Unique object identifier. The format and length of IDs may change over time."""
+
+    token: str
+    """Personal access token (obfuscated)."""
+
+    created: str
+    """The created time for the token."""
