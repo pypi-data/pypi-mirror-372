c9fe22c60ce402c1bfe0a9bf20732d87d6828cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:18 +0000

    Add package IOSimulator/0.0.5/IOSimulator-0.0.5-py3-none-any.whl

---
+++ packages/IOSimulator/IOSimulator-0.0.5-py3-none-any.whl/IOSimulator/IOSimulator.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import platform
+import importlib.util
+import subprocess
+import glob
+import shutil
+import pkg_resources
+
+
+
+def __bootstrap__():
+   global __bootstrap__, __loader__, __file__
+   import sys, pkg_resources
+   so_file = os.path.join(os.path.dirname(__file__),f"IOSimulator.so")
+   spec = importlib.util.spec_from_file_location("IOSimulator", so_file)
+   mylib = importlib.util.module_from_spec(spec)
+   spec.loader.exec_module(mylib)
+   sys.modules[__name__] = compiled_module
+
+# Then, load the shared library
+__bootstrap__()
