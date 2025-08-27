7d41ee7bfb873f26dfe285775c27153722828bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:26 +0000

    Add package abstract-react/0.0.0.35/abstract_react-0.0.0.35-py3-none-any.whl

---
+++ packages/abstract-react/abstract_react-0.0.0.35-py3-none-any.whl/abstract_react/consoles/imports/imports.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import re
+from typing import List, Dict, Any, Tuple
+from abstract_gui.QT6 import *
+from pathlib import Path
+from abstract_utilities import get_set_attr
+from ...react_analyzer import *
+from PyQt6 import QtGui, QtCore, QtWidgets
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
