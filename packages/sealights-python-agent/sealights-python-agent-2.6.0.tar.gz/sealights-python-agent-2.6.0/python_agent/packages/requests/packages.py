06f40e96a80993d39ac797d595d7e9a2e8479a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:36 +0000

    Add package sealights-python-agent/2.6.0/sealights-python-agent-2.6.0.tar.gz

---
+++ packages/sealights-python-agent/sealights-python-agent-2.6.0.tar.gz/sealights-python-agent-2.6.0/python_agent/packages/requests/packages.py
@@ -0,0 +1,28 @@
+import sys
+
+try:
+    import chardet
+except ImportError:
+    from python_agent.packages import charset_normalizer as chardet
+    import warnings
+
+    warnings.filterwarnings("ignore", "Trying to detect", module="charset_normalizer")
+
+# This code exists for backwards compatibility reasons.
+# I don't like it either. Just look the other way. :)
+
+for package in ("python_agent.packages.urllib3", "python_agent.packages.idna"):
+    locals()[package] = __import__(package)
+    # This traversal is apparently necessary such that the identities are
+    # preserved (requests.packages.urllib3.* is urllib3.*)
+    for mod in list(sys.modules):
+        if mod == package or mod.startswith(package + "."):
+            sys.modules["requests.packages." + mod] = sys.modules[mod]
+
+target = chardet.__name__
+for mod in list(sys.modules):
+    if mod == target or mod.startswith(target + "."):
+        sys.modules["requests.packages." + target.replace(target, "chardet")] = (
+            sys.modules[mod]
+        )
+# Kinda cool, though, right?
