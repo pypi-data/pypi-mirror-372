f046cdf9029120d5fc200214d657605ab22d624f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:04:53 +0000

    Add package ommlds/0.0.0.dev410/ommlds-0.0.0.dev410-py3-none-any.whl

---
+++ packages/ommlds/ommlds-0.0.0.dev410-py3-none-any.whl/ommlds/minichain/tools/execution/reflect.py
@@ -0,0 +1,37 @@
+"""
+TODO:
+ - one-sided, sync ^ async, reflect sig
+"""
+import inspect
+import typing as ta
+
+from omlish import lang
+
+from ..fns import ToolFn
+from ..reflect import reflect_tool_spec
+from .catalog import ToolCatalogEntry
+
+
+##
+
+
+def reflect_tool_catalog_entry(fn: ta.Callable) -> ToolCatalogEntry:
+    impl: ToolFn.Impl
+    if isinstance(fn, lang.MaysyncFn_):
+        impl = ToolFn.MaysyncImpl(fn.cast())
+    else:
+        # FIXME: assumes sync
+        impl = ToolFn.FnImpl(fn, lang.as_async(fn))
+
+    sig = inspect.signature(fn)
+    if sig.return_annotation is not str:
+        raise NotImplementedError(fn)
+
+    return ToolCatalogEntry(
+        reflect_tool_spec(fn),
+        ToolFn(
+            impl,
+            ToolFn.KwargsInput(),
+            ToolFn.RawStringOutput(),
+        ),
+    )
