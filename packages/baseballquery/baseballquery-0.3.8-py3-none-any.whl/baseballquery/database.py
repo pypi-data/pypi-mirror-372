314e6d409e62cce2f64818213a0f9fc36c5f952c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:38 +0000

    Add package baseballquery/0.3.8/baseballquery-0.3.8-py3-none-any.whl

---
+++ packages/baseballquery/baseballquery-0.3.8-py3-none-any.whl/baseballquery/database.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import sqlalchemy
+from sqlalchemy import text
+
+data_dir = Path("~/.baseballquery").expanduser()
+if not data_dir.exists():
+    data_dir.mkdir()
+# Create a SQLite database in the data directory
+db_path = data_dir / "baseballquery.db"
+engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
