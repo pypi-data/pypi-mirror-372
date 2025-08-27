a7f9c7276b484a56fd4c17aa88ffd036c5574328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:04:38 +0000

    Add package dbini/0.1.1/dbini-0.1.1.tar.gz

---
+++ packages/dbini/dbini-0.1.1.tar.gz/dbini-0.1.1/src/dbini/__init__.py
@@ -0,0 +1,25 @@
+"""
+dbini - Zero-config NoSQL backend database for Python
+
+This package provides a lightweight, zero-configuration, NoSQL-style database 
+that automatically stores data locally inside the project directory. It is 
+designed to be developer-friendly, secure, and easily usable across different 
+programming environments.
+
+Main Components:
+----------------
+- DBini   : Core database handler (create projects, collections, docs, files)
+- Server  : Lightweight server to expose REST API for frontend or external clients
+"""
+
+from .core import DBini
+from .server import Server
+
+__version__ = "0.1.1"
+__author__ = "Binidu01"
+__license__ = "MIT"
+
+__all__ = [
+    "DBini",
+    "Server",
+]
