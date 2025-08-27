58809c49cafd4d0ad5d21bb32a2e0c0963096d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:05:46 +0000

    Add package python-plus-tools/2.2.0a7/python_plus_tools-2.2.0a7-py3-none-any.whl

---
+++ packages/python-plus-tools/python_plus_tools-2.2.0a7-py3-none-any.whl/pyplus/science/units/shapes.py
@@ -0,0 +1,7 @@
+"""
+Some commonly shapes.You can use this moudle for shape opeerations.
+"""
+
+from . import units
+
+raise ModuleNotFoundError("This moudle is not completed.")
---
+++ packages/python-plus-tools/python_plus_tools-2.2.0a7-py3-none-any.whl/pyplus/tools/formula.py
@@ -0,0 +1,3 @@
+from sympy import __all__ as sympy_all
+from sympy import *
+from advancedlib.math import safe_formula_solver
