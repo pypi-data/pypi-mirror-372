1cf79cb88d586609700b678cdd497365811e7715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:33 +0000

    Add package impectPy/2.4.4/impectPy-2.4.4.tar.gz

---
+++ packages/impectPy/impectPy-2.4.4.tar.gz/impectPy-2.4.4/impectPy/__init__.py
@@ -0,0 +1,19 @@
+# define version attribute
+__version__ = "2.4.4"
+
+# import modules
+from .access_token import getAccessToken
+from .iterations import getIterations
+from .matches import getMatches
+from .events import getEvents
+from .matchsums import getPlayerMatchsums, getSquadMatchsums
+from .iteration_averages import getPlayerIterationAverages, getSquadIterationAverages
+from .player_scores import getPlayerMatchScores, getPlayerIterationScores
+from .squad_scores import getSquadMatchScores, getSquadIterationScores
+from .player_profile_scores import getPlayerProfileScores
+from .xml import generateXML
+from .set_pieces import getSetPieces
+from .squad_ratings import getSquadRatings
+from .match_info import getFormations, getSubstitutions, getStartingPositions
+from .config import Config as Config
+from .impect import Impect as Impect
