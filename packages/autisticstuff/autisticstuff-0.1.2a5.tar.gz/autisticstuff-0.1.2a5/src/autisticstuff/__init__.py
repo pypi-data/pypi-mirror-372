b32614eeac747631fc6022546ad0ba45ea3f45db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:36 +0000

    Add package autisticstuff/0.1.2a5/autisticstuff-0.1.2a5.tar.gz

---
+++ packages/autisticstuff/autisticstuff-0.1.2a5.tar.gz/autisticstuff-0.1.2a5/src/autisticstuff/__init__.py
@@ -0,0 +1,9 @@
+from . import (
+	astuff_logs as astuff_logs,
+	astuff_progbars as astuff_progbars,
+	astuff_sqla as astuff_sqla,
+	astuff_utils as astuff_utils,
+)
+
+
+__all__ = ["astuff_logs", "astuff_progbars", "astuff_sa", "astuff_utils"]
