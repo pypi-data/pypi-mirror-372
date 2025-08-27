8cfe2db2bc043370dabc2dcb857d25af4c947c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:04:29 +0000

    Add package bluer-ugv/6.728.1/bluer_ugv-6.728.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.728.1-py3-none-any.whl/bluer_ugv/sparrow/README.py
@@ -0,0 +1,13 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_ugv.README.consts import assets2_bluer_sparrow
+
+items = ImageItems(
+    {
+        f"{assets2_bluer_sparrow}/20250722_174115-2.jpg": "",
+        f"{assets2_bluer_sparrow}/20250825_190410.jpg": "",
+        f"{assets2_bluer_sparrow}/20250825_190439.jpg": "",
+        f"{assets2_bluer_sparrow}/20250825_190450.jpg": "",
+        f"{assets2_bluer_sparrow}/20250825_190512.jpg": "",
+    }
+)
