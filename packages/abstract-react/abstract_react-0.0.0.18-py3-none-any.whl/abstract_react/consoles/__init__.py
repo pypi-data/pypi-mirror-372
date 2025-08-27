d28cb364d36ce33ce6d6631f49284aedcabc09a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:42 +0000

    Add package abstract-react/0.0.0.18/abstract_react-0.0.0.18-py3-none-any.whl

---
+++ packages/abstract-react/abstract_react-0.0.0.18-py3-none-any.whl/abstract_react/consoles/__init__.py
@@ -0,0 +1,6 @@
+import os
+from abstract_gui import get_for_all_tabs
+CONSOLE_DIR_PATH = os.path.abspath(__file__)
+CONSOLE_ABS_DIR = os.path.dirname(CONSOLE_DIR_PATH)
+get_for_all_tabs(CONSOLE_ABS_DIR)
+from .main import *
