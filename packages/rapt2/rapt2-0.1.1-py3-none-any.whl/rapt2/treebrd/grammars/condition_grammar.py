91858facc7009751384589e4731d8d1c5973e61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:04:37 +0000

    Add package rapt2/0.1.1/rapt2-0.1.1-py3-none-any.whl

---
+++ packages/rapt2/rapt2-0.1.1-py3-none-any.whl/rapt2/treebrd/grammars/condition_grammar.py
@@ -0,0 +1,96 @@
+from pyparsing import oneOf, CaselessKeyword, infixNotation, opAssoc, Literal
+from pyparsing.helpers import infix_notation
+
+from .proto_grammar import ProtoGrammar
+from .syntax import Syntax
+
+
+def get_attribute_references(instring):
+    """
+    Return a list of attribute references in the condition expression.
+
+    attribute_reference ::= relation_name "." attribute_name | attribute_name
+
+    :param instring: a condition expression.
+    :return: a list of attribute references.
+    """
+    parsed = ConditionGrammar().conditions.parseString(instring)
+    result = parsed if isinstance(parsed[0], str) else parsed[0]
+    return result.attribute_reference.asList() if result.attribute_reference else []
+
+
+class ConditionGrammar(ProtoGrammar):
+    """
+    A grammar for condition expressions.
+    """
+
+    def __init__(self, syntax=None):
+        """
+        Initializes a ConditionGrammar. Uses the default syntax if none
+        is provided.
+
+        :param syntax: a syntax for this grammar.
+        """
+        self.syntax = syntax or Syntax()
+
+    @property
+    def comparator_op(self):
+        return oneOf(
+            [
+                self.syntax.equal_op,
+                self.syntax.not_equal_op,
+                self.syntax.not_equal_alt_op,
+                self.syntax.less_than_op,
+                self.syntax.less_than_equal_op,
+                self.syntax.greater_than_op,
+                self.syntax.greater_than_equal_op,
+            ]
+        )
+
+    @property
+    def not_op(self):
+        return CaselessKeyword(self.syntax.not_op)
+
+    @property
+    def logical_binary_op(self):
+        """
+        logical_binary_op ::=  and_op | or_op
+        """
+        return CaselessKeyword(self.syntax.and_op) | CaselessKeyword(self.syntax.or_op)
+
+    @property
+    def operand(self):
+        """
+        operand ::= identifier | string_literal | number
+        """
+        return (
+            self.attribute_reference("attribute_reference*")
+            | self.string_literal
+            | self.number
+        )
+
+    @property
+    def condition(self):
+        """
+        condition ::= operand comparator_op operand | not_op condition |
+                  paren_left condition paren_right
+        not_op and grouping rules are defined using infixNotation in
+        conditions.
+        """
+        return self.operand + self.comparator_op + self.operand
+
+    @property
+    def conditions(self):
+        """
+        conditions ::= condition | condition logical_binary_op conditions
+        Note: By default lpar and rpar arguments are suppressed.
+        """
+        return infix_notation(
+            base_expr=self.condition,
+            op_list=[
+                (self.not_op, 1, opAssoc.RIGHT),
+                (self.logical_binary_op, 2, opAssoc.LEFT),
+            ],
+            lpar=self.syntax.paren_left,
+            rpar=self.syntax.paren_right,
+        )
