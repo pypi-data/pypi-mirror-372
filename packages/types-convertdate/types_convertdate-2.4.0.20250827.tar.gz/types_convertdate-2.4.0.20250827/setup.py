3ba45ae2765ebfbb1474e8e3b4d27e51d431de14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 15:04:58 +0000

    Add package types-convertdate/2.4.0.20250827/types_convertdate-2.4.0.20250827.tar.gz

---
+++ packages/types-convertdate/types_convertdate-2.4.0.20250827.tar.gz/types_convertdate-2.4.0.20250827/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'convertdate-stubs': ['__init__.pyi', 'armenian.pyi', 'bahai.pyi', 'coptic.pyi', 'data/__init__.pyi', 'data/french_republican_days.pyi', 'data/positivist.pyi', 'daycount.pyi', 'dublin.pyi', 'french_republican.pyi', 'gregorian.pyi', 'hebrew.pyi', 'holidays.pyi', 'indian_civil.pyi', 'islamic.pyi', 'iso.pyi', 'julian.pyi', 'julianday.pyi', 'mayan.pyi', 'ordinal.pyi', 'persian.pyi', 'positivist.pyi', 'utils.pyi', 'METADATA.toml', 'py.typed']})
