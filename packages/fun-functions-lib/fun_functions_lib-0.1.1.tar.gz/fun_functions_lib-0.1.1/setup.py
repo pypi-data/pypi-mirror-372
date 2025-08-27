45c0291caefcc54587bd035d0d3c7cc674791a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:05:09 +0000

    Add package fun-functions-lib/0.1.1/fun_functions_lib-0.1.1.tar.gz

---
+++ packages/fun-functions-lib/fun_functions_lib-0.1.1.tar.gz/fun_functions_lib-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="fun_functions_lib",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Cove Allen",
+    description="a bunch of functions",
+    url="",
+    license="MIT",
+)
+
