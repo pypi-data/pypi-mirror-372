4bd8e2465bf01b1c6bfcfdbe659469ac1e121498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:39 +0000

    Add package pyThermoDB/1.11.12/pythermodb-1.11.12-py3-none-any.whl

---
+++ packages/pyThermoDB/pythermodb-1.11.12-py3-none-any.whl/pyThermoDB/__init__.py
@@ -0,0 +1,21 @@
+from .config import __version__, __author__, __description__
+from .docs import (
+    ThermoDB, CompBuilder,
+    TableData, TableEquation,
+    TableMatrixData, TableMatrixEquation,
+    ManageData, CustomRef
+)
+from .app import (
+    init, ref, build_thermodb, load_thermodb,
+    build_component_thermodb, build_components_thermodb
+)
+
+__all__ = [
+    '__version__', '__author__', '__description__',
+    'ThermoDB', 'CompBuilder',
+    'TableData', 'TableEquation',
+    'TableMatrixData', 'TableMatrixEquation',
+    'init', 'ref', 'build_thermodb', 'load_thermodb',
+    'build_component_thermodb', 'build_components_thermodb',
+    'ManageData', 'CustomRef',
+]
---
+++ packages/pyThermoDB/pythermodb-1.11.12-py3-none-any.whl/pyThermoDB/models/__init__.py
@@ -0,0 +1,9 @@
+from .ref import (
+    DataBookTableTypes, PayLoadType, DataResultType, MatrixDataType, 
+    DataResult, EquationResult
+    )
+
+__all__ = [
+    'DataBookTableTypes', 'PayLoadType', 'DataResultType', 'MatrixDataType', 
+    'DataResult', 'EquationResult'
+    ]
