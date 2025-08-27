6cd24b6e00f9a79a10177ab859727665079db221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:28 +0000

    Add package comfyui-frontend-package/1.25.11/comfyui_frontend_package-1.25.11-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.25.11-py3-none-any.whl/comfyui_frontend_package/static/assets/template-COFmczqT.js
@@ -0,0 +1,38 @@
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+import { markRaw, openBlock, createElementBlock, createElementVNode } from "vue";
+const _hoisted_1 = {
+  width: "1.2em",
+  height: "1.2em",
+  viewBox: "0 0 16 16",
+  fill: "none",
+  xmlns: "http://www.w3.org/2000/svg"
+};
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, _cache[0] || (_cache[0] = [
+    createElementVNode("path", {
+      d: "M13.1894 6.24254L13.6894 4.24254C13.8471 3.61139 13.3698 3 12.7192 3H3.78077C3.3219 3 2.92192 3.3123 2.81062 3.75746L2.31062 5.75746C2.15284 6.38861 2.63019 7 3.28077 7H12.2192C12.6781 7 13.0781 6.6877 13.1894 6.24254Z",
+      stroke: "currentColor",
+      "stroke-width": "1.3",
+      "stroke-linecap": "round"
+    }, null, -1),
+    createElementVNode("path", {
+      d: "M13.1894 12.2425L13.6894 10.2425C13.8471 9.61139 13.3698 9 12.7192 9H8.78077C8.3219 9 7.92192 9.3123 7.81062 9.75746L7.31062 11.7575C7.15284 12.3886 7.6302 13 8.28077 13H12.2192C12.6781 13 13.0781 12.6877 13.1894 12.2425Z",
+      stroke: "currentColor",
+      "stroke-width": "1.3",
+      "stroke-linecap": "round"
+    }, null, -1),
+    createElementVNode("path", {
+      d: "M5.18936 12.2425L5.68936 10.2425C5.84714 9.61139 5.36978 9 4.71921 9H3.78077C3.3219 9 2.92192 9.3123 2.81062 9.75746L2.31062 11.7575C2.15284 12.3886 2.6302 13 3.28077 13H4.21921C4.67808 13 5.07806 12.6877 5.18936 12.2425Z",
+      stroke: "currentColor",
+      "stroke-width": "1.3",
+      "stroke-linecap": "round"
+    }, null, -1)
+  ]));
+}
+__name(render, "render");
+const template = markRaw({ name: "comfy-template", render });
+export {
+  template as default
+};
+//# sourceMappingURL=template-COFmczqT.js.map
