6396059308fe1d069b72ace749d20f296091c97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 17:06:16 +0000

    Add package dbr-test-pckg/1.2.0/dbr_test_pckg-1.2.0-py3-none-any.whl

---
+++ packages/dbr-test-pckg/dbr_test_pckg-1.2.0-py3-none-any.whl/dbr_test_pckg/__main__.py
@@ -0,0 +1,9 @@
+from .show import python_path
+
+def main():
+    print( '=== __main__.main() - start ===' )
+    python_path()
+    print( '=== __main__.main() - end ===' )
+
+if __name__ == "__main__":    # *1/
+    main()
