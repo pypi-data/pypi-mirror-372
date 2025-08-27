467e4241319596503a52988eb5c9bc3ddea6a39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 05:05:04 +0000

    Add package array-api-extra/0.8.1/array_api_extra-0.8.1.tar.gz

---
+++ packages/array-api-extra/array_api_extra-0.8.1.tar.gz/array_api_extra-0.8.1/docs/api-reference.md
@@ -0,0 +1,24 @@
+# API Reference
+
+```{eval-rst}
+.. currentmodule:: array_api_extra
+.. autosummary::
+    :nosignatures:
+    :toctree: generated
+
+    apply_where
+    at
+    atleast_nd
+    broadcast_shapes
+    cov
+    create_diagonal
+    default_dtype
+    expand_dims
+    isclose
+    kron
+    nunique
+    one_hot
+    pad
+    setdiff1d
+    sinc
+```
---
+++ packages/array-api-extra/array_api_extra-0.8.1.tar.gz/array_api_extra-0.8.1/src/array_api_extra/__init__.py
@@ -0,0 +1,41 @@
+"""Extra array functions built on top of the array API standard."""
+
+from ._delegation import isclose, one_hot, pad
+from ._lib._at import at
+from ._lib._funcs import (
+    apply_where,
+    atleast_nd,
+    broadcast_shapes,
+    cov,
+    create_diagonal,
+    default_dtype,
+    expand_dims,
+    kron,
+    nunique,
+    setdiff1d,
+    sinc,
+)
+from ._lib._lazy import lazy_apply
+
+__version__ = "0.8.1"
+
+# pylint: disable=duplicate-code
+__all__ = [
+    "__version__",
+    "apply_where",
+    "at",
+    "atleast_nd",
+    "broadcast_shapes",
+    "cov",
+    "create_diagonal",
+    "default_dtype",
+    "expand_dims",
+    "isclose",
+    "kron",
+    "lazy_apply",
+    "nunique",
+    "one_hot",
+    "pad",
+    "setdiff1d",
+    "sinc",
+]
