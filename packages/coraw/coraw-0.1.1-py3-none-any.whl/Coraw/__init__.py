694ad5995c41107bf9ef148a5ee3ceaea0a4ac00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 03:18:45 +0000

    Add package coraw/0.1.1/coraw-0.1.1-py3-none-any.whl

---
+++ packages/coraw/coraw-0.1.1-py3-none-any.whl/Coraw/__init__.py
@@ -0,0 +1,11 @@
+from .coraw import co, ping, stop, close, forward, back, left, right, stop_move
+from .coraw import headup, headdown, liftup, liftdown, backlight, Img
+from .coraw import stopface, resumeface, paudio, saudio, setvol, tts, wait
+from .coraw import bind, camera, camerargb, tkint
+
+__all__ = [
+    "co","ping","stop","close","forward","back","left","right","stop_move",
+    "headup","headdown","liftup","liftdown","backlight","Img",
+    "stopface","resumeface","paudio","saudio","setvol","tts","wait",
+    "bind","camera","camerargb","tkint"
+]
