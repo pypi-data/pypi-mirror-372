7ac94638711f72fb1ffc490206196c33244da620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:20 +0000

    Add package treeparse/0.1.2.post1/treeparse-0.1.2.post1.tar.gz

---
+++ packages/treeparse/treeparse-0.1.2.post1.tar.gz/treeparse-0.1.2.post1/src/treeparse/__init__.py
@@ -0,0 +1,18 @@
+from .argument import argument
+from .option import option
+from .command import command
+from .chain import Chain
+from .group import group
+from .cli import cli
+from .color_config import color_config
+
+# Rebuild models to handle forward references
+argument.model_rebuild()
+option.model_rebuild()
+command.model_rebuild()
+Chain.model_rebuild()
+group.model_rebuild()
+cli.model_rebuild()
+color_config.model_rebuild()
+
+__all__ = ["argument", "option", "command", "Chain", "group", "cli", "color_config"]
