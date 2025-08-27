3acb8a40e1e2f3d1e45c599797bef2bf19241d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:28 +0000

    Add package galaxy-py-kernel/0.0.1/galaxy_py_kernel-0.0.1.tar.gz

---
+++ packages/galaxy-py-kernel/galaxy_py_kernel-0.0.1.tar.gz/galaxy_py_kernel-0.0.1/setup.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2022 bastien.saltel
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from setuptools import setup
+
+setup(name="galaxy-kernel",
+      version="0.0.1",
+      author="Bastien Saltel",
+      author_email="bastien.saltel@gmail.com",
+      description="Galaxy Framework - Kernel Module",
+      long_description="Galaxy Framework - Kernel Module",
+      long_description_content_type="text/markdown",
+      url="https://github.com/bsaltel/galaxy-py-kernel",
+      packages=[
+                "galaxy.kernel"
+                ],
+      package_dir = {
+                     "galaxy.kernel": "galaxy/kernel"
+                    },
+      license="GPL",
+      python_requires=">=3.10"
+     )
