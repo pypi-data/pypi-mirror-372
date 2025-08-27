f97bcd051d8ce92ecae21a853b4c373384a83fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:22 +0000

    Add package abstract-paths/0.0.0.134/abstract_paths-0.0.0.134-py3-none-any.whl

---
+++ packages/abstract-paths/abstract_paths-0.0.0.134-py3-none-any.whl/abstract_paths/finderConsole/__init__.py
@@ -0,0 +1,5 @@
+from .main import *
+from abstract_gui.QT6.startConsole import  startConsole
+
+def startContentFinderConsole():
+    startConsole(ContentFinderConsole)
---
+++ packages/abstract-paths/abstract_paths-0.0.0.134-py3-none-any.whl/abstract_paths/finderConsole/imports/__init__.py
@@ -0,0 +1,3 @@
+from abstract_gui.QT6.imports import *
+from abstract_gui.QT6.utils import *
+from abstract_gui.QT6.utils.functions import SearchWorker
