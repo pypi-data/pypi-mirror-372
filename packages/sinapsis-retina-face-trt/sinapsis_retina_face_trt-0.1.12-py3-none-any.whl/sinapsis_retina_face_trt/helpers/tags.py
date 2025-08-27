ab903bc28f1a963a32416c88e59939271bef411d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:49 +0000

    Add package sinapsis-retina-face-trt/0.1.12/sinapsis_retina_face_trt-0.1.12-py3-none-any.whl

---
+++ packages/sinapsis-retina-face-trt/sinapsis_retina_face_trt-0.1.12-py3-none-any.whl/sinapsis_retina_face_trt/helpers/tags.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Tags(Enum):
+    DEEPFACE = "deepface"
+    EMBEDDINGS = "embeddings"
+    EMBEDDING_EXTRACTION = "embedding_extraction"
+    FACE_LOCALISATION = "face_localisation"
+    FACE_RECOGNITION = "face_recognition"
+    FACE_VERIFICATION = "face_verification"
+    GALLERY_SEARCH = "gallery_search"
+    IMAGE = "image"
+    MODELS = "models"
+    PYTORCH = "pytorch"
+    PYTORCHTRT = "pytorchtrt"
+    RETINA_FACE = "retina_face"
+    TRT = "tensorrt"
