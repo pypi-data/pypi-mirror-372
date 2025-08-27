462a5688c5d779fabb91785cd77048a6edfb8d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 17:06:15 +0000

    Add package photcalib/0.47/photcalib-0.47.tar.gz

---
+++ packages/photcalib/photcalib-0.47.tar.gz/photcalib-0.47/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "photcalib",
+    version = 0.47,
+    description = "calibration tool for photometric data",
+    long_description = readme(),
+    long_description_content_type="text/x-rst",
+    author = "Zhen Yuan",
+    author_email = "",
+    url = "https://github.com/zyuan-astro/PhotCalib",
+    packages = find_packages(),
+    package_data = {},
+    include_package_data = True,
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    zip_safe=False
+)
