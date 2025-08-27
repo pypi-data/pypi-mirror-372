ea0935457280da24221a5ddf9e917c6706ffedbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:08 +0000

    Add package types-boto3-full/1.40.18/types_boto3_full-1.40.18-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.40.18-py3-none-any.whl/types_boto3_opsworks/waiter.py
@@ -0,0 +1,153 @@
+"""
+Type annotations for opsworks service client waiters.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from types_boto3_opsworks.client import OpsWorksClient
+    from types_boto3_opsworks.waiter import (
+        AppExistsWaiter,
+        DeploymentSuccessfulWaiter,
+        InstanceOnlineWaiter,
+        InstanceRegisteredWaiter,
+        InstanceStoppedWaiter,
+        InstanceTerminatedWaiter,
+    )
+
+    session = Session()
+    client: OpsWorksClient = session.client("opsworks")
+
+    app_exists_waiter: AppExistsWaiter = client.get_waiter("app_exists")
+    deployment_successful_waiter: DeploymentSuccessfulWaiter = client.get_waiter("deployment_successful")
+    instance_online_waiter: InstanceOnlineWaiter = client.get_waiter("instance_online")
+    instance_registered_waiter: InstanceRegisteredWaiter = client.get_waiter("instance_registered")
+    instance_stopped_waiter: InstanceStoppedWaiter = client.get_waiter("instance_stopped")
+    instance_terminated_waiter: InstanceTerminatedWaiter = client.get_waiter("instance_terminated")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from botocore.waiter import Waiter
+
+from .type_defs import (
+    DescribeAppsRequestWaitTypeDef,
+    DescribeDeploymentsRequestWaitTypeDef,
+    DescribeInstancesRequestWaitExtraExtraExtraTypeDef,
+    DescribeInstancesRequestWaitExtraExtraTypeDef,
+    DescribeInstancesRequestWaitExtraTypeDef,
+    DescribeInstancesRequestWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "AppExistsWaiter",
+    "DeploymentSuccessfulWaiter",
+    "InstanceOnlineWaiter",
+    "InstanceRegisteredWaiter",
+    "InstanceStoppedWaiter",
+    "InstanceTerminatedWaiter",
+)
+
+
+class AppExistsWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/AppExists.html#OpsWorks.Waiter.AppExists)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#appexistswaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeAppsRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/AppExists.html#OpsWorks.Waiter.AppExists.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#appexistswaiter)
+        """
+
+
+class DeploymentSuccessfulWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/DeploymentSuccessful.html#OpsWorks.Waiter.DeploymentSuccessful)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#deploymentsuccessfulwaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeDeploymentsRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/DeploymentSuccessful.html#OpsWorks.Waiter.DeploymentSuccessful.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#deploymentsuccessfulwaiter)
+        """
+
+
+class InstanceOnlineWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceOnline.html#OpsWorks.Waiter.InstanceOnline)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceonlinewaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeInstancesRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceOnline.html#OpsWorks.Waiter.InstanceOnline.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceonlinewaiter)
+        """
+
+
+class InstanceRegisteredWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceRegistered.html#OpsWorks.Waiter.InstanceRegistered)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceregisteredwaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeInstancesRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceRegistered.html#OpsWorks.Waiter.InstanceRegistered.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceregisteredwaiter)
+        """
+
+
+class InstanceStoppedWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceStopped.html#OpsWorks.Waiter.InstanceStopped)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instancestoppedwaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeInstancesRequestWaitExtraExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceStopped.html#OpsWorks.Waiter.InstanceStopped.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instancestoppedwaiter)
+        """
+
+
+class InstanceTerminatedWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceTerminated.html#OpsWorks.Waiter.InstanceTerminated)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceterminatedwaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeInstancesRequestWaitExtraExtraExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks/waiter/InstanceTerminated.html#OpsWorks.Waiter.InstanceTerminated.wait)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_opsworks/waiters/#instanceterminatedwaiter)
+        """
