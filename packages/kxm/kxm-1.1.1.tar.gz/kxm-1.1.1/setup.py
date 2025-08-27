9a1c3b6460b912d0f0cc8cabe0cc128250fe71da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 15:04:52 +0000

    Add package kxm/1.1.1/kxm-1.1.1.tar.gz

---
+++ packages/kxm/kxm-1.1.1.tar.gz/kxm-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='kxm',
+    version='1.1.1',
+    packages=find_packages(),
+    author='看小美',
+    description='奖励我看美女',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    url='https://github.com/kxm1111/kxm'
+)
