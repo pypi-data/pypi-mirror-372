9ca44f63da65a2dafef47afa1ba68ceb64d33066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:39 +0000

    Add package player-estebandev/0.0.1/player_estebandev-0.0.1-py3-none-any.whl

---
+++ packages/player-estebandev/player_estebandev-0.0.1-py3-none-any.whl/player/player.py
@@ -0,0 +1,25 @@
+"""
+    este es el modulo que incluye la clase de reproductor de musica
+"""
+
+class Player:
+    """
+    Esta clase crea un reproductor de 
+    musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio como parametro
+        en el constructor
+
+        Parameters:
+        song(str): este es un string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito, en caso de fracaso retorna 0
+        """
+        print("reporduciendo cancion")
+
+    def stop(self):
+        print()
