65472334b99acb398113eef45a7c3e2362ecfdc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:21 +0000

    Add package ffun/1.20.8/ffun-1.20.8.tar.gz

---
+++ packages/ffun/ffun-1.20.8.tar.gz/ffun-1.20.8/ffun/librarian/processors/upper_case_title.py
@@ -0,0 +1,16 @@
+from ffun.core import logging
+from ffun.librarian.processors import base
+from ffun.library.entities import Entry
+from ffun.ontology.entities import RawTag
+
+logger = logging.get_module_logger()
+
+
+class Processor(base.Processor):
+    __slots__ = ()
+
+    async def process(self, entry: Entry) -> list[RawTag]:
+        if entry.title.isupper():
+            return [RawTag(raw_uid="upper-case-title")]
+
+        return []
