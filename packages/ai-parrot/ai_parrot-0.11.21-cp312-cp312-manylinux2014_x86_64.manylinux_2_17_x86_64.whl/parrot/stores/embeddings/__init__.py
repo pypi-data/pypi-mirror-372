e946f5d8b724ef71723bb105d0af0bbc8a34ed97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:29 +0000

    Add package ai-parrot/0.11.21/ai_parrot-0.11.21-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/ai-parrot/ai_parrot-0.11.21-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/parrot/stores/embeddings/__init__.py
@@ -0,0 +1,10 @@
+supported_embeddings = {
+    'openai': 'OpenAIEmbed',
+    'google': 'GoogleEmbed',
+    'vertexai': 'VertexAIEmbed',
+    'huggingface': 'HugginfaceEmbed',
+    'fastembed': 'FastembedEmbed',
+    'bge': 'BgeEmbed',
+    'ollama': 'OllamaEmbed',
+    'transformers': 'TransformersEmbed'
+}
