fa2e6f4059ca38137f17db1ee2dacf3875059cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:04:58 +0000

    Add package xknx/3.9.0/xknx-3.9.0.tar.gz

---
+++ packages/xknx/xknx-3.9.0.tar.gz/xknx-3.9.0/xknx/secure/util.py
@@ -0,0 +1,29 @@
+"""Utilities for KNX Secure."""
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.utils import Buffer
+
+
+def bytes_xor(a: bytes, b: bytes) -> bytes:  # pylint: disable=invalid-name
+    """
+    XOR two bytes values.
+
+    Different lengths raise ValueError.
+    """
+    if len(a) != len(b):
+        raise ValueError("Length of a and b must be equal.")
+    return (int.from_bytes(a, "big") ^ int.from_bytes(b, "big")).to_bytes(len(a), "big")
+
+
+def byte_pad(data: bytes, block_size: int) -> bytes:
+    """Pad data with 0x00 until its length is a multiple of block_size."""
+    if remainder := len(data) % block_size:
+        return data + bytes(block_size - remainder)
+    return data
+
+
+def sha256_hash(data: Buffer) -> bytes:
+    """Calculate SHA256 hash of data."""
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(data)
+    return digest.finalize()
