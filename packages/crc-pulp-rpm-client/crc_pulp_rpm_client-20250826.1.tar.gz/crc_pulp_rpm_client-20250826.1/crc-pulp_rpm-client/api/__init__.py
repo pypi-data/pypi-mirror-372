2d158d8b0301b6468ca3105014616983c892003d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:04:37 +0000

    Add package crc-pulp-rpm-client/20250826.1/crc_pulp_rpm_client-20250826.1.tar.gz

---
+++ packages/crc-pulp-rpm-client/crc_pulp_rpm_client-20250826.1.tar.gz/crc_pulp_rpm_client-20250826.1/crc-pulp_rpm-client/api/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+
+# import apis into api package
+from crc-pulp_rpm-client.api.acs_rpm_api import AcsRpmApi
+from crc-pulp_rpm-client.api.content_advisories_api import ContentAdvisoriesApi
+from crc-pulp_rpm-client.api.content_distribution_trees_api import ContentDistributionTreesApi
+from crc-pulp_rpm-client.api.content_modulemd_defaults_api import ContentModulemdDefaultsApi
+from crc-pulp_rpm-client.api.content_modulemd_obsoletes_api import ContentModulemdObsoletesApi
+from crc-pulp_rpm-client.api.content_modulemds_api import ContentModulemdsApi
+from crc-pulp_rpm-client.api.content_packagecategories_api import ContentPackagecategoriesApi
+from crc-pulp_rpm-client.api.content_packageenvironments_api import ContentPackageenvironmentsApi
+from crc-pulp_rpm-client.api.content_packagegroups_api import ContentPackagegroupsApi
+from crc-pulp_rpm-client.api.content_packagelangpacks_api import ContentPackagelangpacksApi
+from crc-pulp_rpm-client.api.content_packages_api import ContentPackagesApi
+from crc-pulp_rpm-client.api.content_repo_metadata_files_api import ContentRepoMetadataFilesApi
+from crc-pulp_rpm-client.api.distributions_rpm_api import DistributionsRpmApi
+from crc-pulp_rpm-client.api.publications_rpm_api import PublicationsRpmApi
+from crc-pulp_rpm-client.api.remotes_rpm_api import RemotesRpmApi
+from crc-pulp_rpm-client.api.remotes_uln_api import RemotesUlnApi
+from crc-pulp_rpm-client.api.repositories_rpm_api import RepositoriesRpmApi
+from crc-pulp_rpm-client.api.repositories_rpm_versions_api import RepositoriesRpmVersionsApi
+from crc-pulp_rpm-client.api.rpm_comps_api import RpmCompsApi
+from crc-pulp_rpm-client.api.rpm_copy_api import RpmCopyApi
+from crc-pulp_rpm-client.api.rpm_prune_api import RpmPruneApi
+
