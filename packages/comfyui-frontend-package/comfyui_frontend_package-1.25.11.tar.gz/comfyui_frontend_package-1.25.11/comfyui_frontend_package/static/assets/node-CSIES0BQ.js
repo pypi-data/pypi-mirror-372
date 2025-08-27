f02f9502ef1a34085dd0aaa7839d595386e7f416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:29 +0000

    Add package comfyui-frontend-package/1.25.11/comfyui_frontend_package-1.25.11.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.25.11.tar.gz/comfyui_frontend_package-1.25.11/comfyui_frontend_package/static/assets/node-CSIES0BQ.js
@@ -0,0 +1,26 @@
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+import { markRaw, openBlock, createElementBlock, createElementVNode } from "vue";
+const _hoisted_1 = {
+  width: "1.2em",
+  height: "1.2em",
+  viewBox: "0 0 16 16",
+  fill: "none",
+  xmlns: "http://www.w3.org/2000/svg"
+};
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, _cache[0] || (_cache[0] = [
+    createElementVNode("path", {
+      d: "M10.6667 10L10.598 10.2577C10.4812 10.6954 10.0848 11 9.63172 11H5.30161C4.64458 11 4.16608 10.3772 4.33538 9.74234L5.40204 5.74234C5.51878 5.30458 5.91523 5 6.36828 5H10.8286C11.4199 5 11.8505 5.56051 11.6982 6.13185L11.6736 6.22389M14 8H10M4.5 8H2",
+      stroke: "currentColor",
+      "stroke-width": "1.3",
+      "stroke-linecap": "round"
+    }, null, -1)
+  ]));
+}
+__name(render, "render");
+const node = markRaw({ name: "comfy-node", render });
+export {
+  node as default
+};
+//# sourceMappingURL=node-CSIES0BQ.js.map
