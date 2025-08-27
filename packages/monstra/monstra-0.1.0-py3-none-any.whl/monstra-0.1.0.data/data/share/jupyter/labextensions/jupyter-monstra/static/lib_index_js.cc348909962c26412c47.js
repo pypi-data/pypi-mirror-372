e596f7b64b718fdbb1d20e6ab4cfd9cd171dc685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:06:30 +0000

    Add package monstra/0.1.0/monstra-0.1.0-py3-none-any.whl

---
+++ packages/monstra/monstra-0.1.0-py3-none-any.whl/monstra-0.1.0.data/data/share/jupyter/labextensions/jupyter-monstra/static/lib_index_js.cc348909962c26412c47.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunkjupyter_monstra"] = self["webpackChunkjupyter_monstra"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/**
+ * Initialization data for the jupyter-monstra extension.
+ */
+const plugin = {
+    id: 'jupyter-monstra:plugin',
+    description: 'A JupyterLab extension.',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension jupyter-monstra is activated!');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.cc348909962c26412c47.js.map
