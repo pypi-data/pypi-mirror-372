2ef98ebc450340701d304164a07095afe38ca206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 07:05:34 +0000

    Add package pyhausbus/1.0.12/pyhausbus-1.0.12.tar.gz

---
+++ packages/pyhausbus/pyhausbus-1.0.12.tar.gz/pyhausbus-1.0.12/pyhausbus/de/hausbus/homeassistant/proxy/controller/data/Pong.py
@@ -0,0 +1,15 @@
+import pyhausbus.HausBusUtils as HausBusUtils
+
+class Pong:
+  CLASS_ID = 0
+  FUNCTION_ID = 199
+
+  @staticmethod
+  def _fromBytes(dataIn:bytearray, offset):
+    return Pong()
+
+  def __str__(self):
+    return f"Pong()"
+
+
+
