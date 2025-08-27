8bad05077f4f511145757353a5061dcdd3fd3622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:04:54 +0000

    Add package omlish/0.0.0.dev410/omlish-0.0.0.dev410.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev410.tar.gz/omlish-0.0.0.dev410/omlish/marshal/trivial/forbidden.py
@@ -0,0 +1,46 @@
+import dataclasses as dc
+import typing as ta
+
+from ... import reflect as rfl
+from ...funcs import match as mfs
+from ..base import MarshalContext
+from ..base import Marshaler
+from ..base import SimpleMarshalerFactory
+from ..base import SimpleUnmarshalerFactory
+from ..base import UnmarshalContext
+from ..base import Unmarshaler
+from ..errors import ForbiddenTypeError
+
+
+C = ta.TypeVar('C')
+R = ta.TypeVar('R')
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class ForbiddenTypeFactory(mfs.MatchFn[[C, rfl.Type], R]):
+    rtys: ta.AbstractSet[rfl.Type]
+
+    def guard(self, ctx: C, rty: rfl.Type) -> bool:
+        return rty in self.rtys
+
+    def fn(self, ctx: C, rty: rfl.Type) -> R:
+        raise ForbiddenTypeError(rty)
+
+
+@dc.dataclass(frozen=True)
+class ForbiddenTypeMarshalerFactory(
+    ForbiddenTypeFactory[MarshalContext, Marshaler],
+    SimpleMarshalerFactory,
+):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class ForbiddenTypeUnmarshalerFactory(
+    ForbiddenTypeFactory[UnmarshalContext, Unmarshaler],
+    SimpleUnmarshalerFactory,
+):
+    pass
