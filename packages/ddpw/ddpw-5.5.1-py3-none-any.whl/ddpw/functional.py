f997bec741bec86c4c8b35ba1560fff282f5ef8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 15:04:53 +0000

    Add package ddpw/5.5.1/ddpw-5.5.1-py3-none-any.whl

---
+++ packages/ddpw/ddpw-5.5.1-py3-none-any.whl/ddpw/functional.py
@@ -0,0 +1,230 @@
+from random import seed as rand_seed
+from typing import Optional
+
+from numpy.random import seed as np_seed
+from torch import Tensor
+from torch import device as t_device
+from torch import distributed as dist
+from torch.cuda import (
+    current_device,
+    is_available,
+    manual_seed_all,
+)
+from torch.cuda import (
+    set_device as __set_device,
+)
+from torch.nn import (
+    BatchNorm1d,
+    BatchNorm2d,
+    BatchNorm3d,
+    Module,
+    SyncBatchNorm,
+)
+from torch.nn.parallel import DistributedDataParallel
+from torch.optim import Optimizer
+from torch.random import manual_seed
+from torch.utils.data import Dataset, DistributedSampler
+
+from .platform import Device, Platform
+
+
+def seed_generators(seed: int) -> None:
+    r"""Seed [pseudo]random number generators from various dependencies.
+
+    :param int seed: The seed.
+    """
+
+    rand_seed(seed)
+    np_seed(seed)
+    manual_seed(seed)
+    manual_seed_all(seed)
+
+
+def average_params_grads(
+    module: Module, params: bool = True, grads: bool = False
+) -> None:
+    r"""
+    Averages the parameters of the given module and/or their gradients across
+    all the GPUs (copied over from a `PyTorch blog <https://pytorch.org/tutorials/intermediate/dist_tuto.html>`_
+    and further modified here).
+
+    :param torch.nn.Module module: The module whose parameters/gradients are to
+        be averaged.
+    :param bool params: Whether to average the parameters or not. Default:
+        ``True``.
+    :param bool grads: Whether to average the gradients or not. Default:
+        ``False``.
+    """
+
+    if not (params and grads):
+        return
+
+    world_size = float(dist.get_world_size())
+
+    for p in module.parameters():
+        if p.grad is not None and grads:
+            dist.all_reduce(p.grad, op=dist.ReduceOp.SUM)
+            p.grad /= world_size
+        if p is not None and params:
+            dist.all_reduce(p, op=dist.ReduceOp.SUM)
+            p /= world_size
+
+
+def optimiser_to(optimiser: Optimizer, device: t_device) -> None:
+    r"""
+    This function offers a simple way to move all the parameters optimised by an
+    optimiser to the specified device. This function has been taken as is from a
+    `solution on PyTorch Discuss
+    <https://discuss.pytorch.org/t/moving-optimizer-from-cpu-to-gpu/96068/3>`_.
+
+    :param torch.optim.Optimizer optimiser: The optimiser to move to a device.
+    :param torch.device device: The device to which to move the optimiser.
+    """
+
+    for param in optimiser.state.values():
+        # if any global tensors in the state dict
+        if isinstance(param, Tensor):
+            param.data = param.data.to(device)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, Tensor):
+                    subparam.data = subparam.data.to(device)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device)
+
+
+def has_batch_norm(module: Module) -> bool:
+    r"""This function checks if a module has batch normalisation layer(s) in it.
+
+    :param torch.nn.Module module: The module to be checked for
+        containing any batch normalisation layers.
+    :returns bool: Whether or not the module has batch normalisation
+        layer(s) in it.
+    """
+
+    if isinstance(module, (BatchNorm1d, BatchNorm2d, BatchNorm3d)):
+        return True
+
+    for child in module.children():
+        if has_batch_norm(child):
+            return True
+
+    return False
+
+
+def to(
+    module: Module,
+    local_rank: int,
+    sync_modules: bool = True,
+    device: Device = Device.GPU,
+    *ddp_args,
+    **ddp_kwargs,
+) -> Module:
+    r"""
+    A quick and minimal function that works on all devices to move the given
+    module to the specified device: CPU, GPU, or MPS. If the platform is set up
+    in an IPC fashion, this function optionally moves the module in a
+    distributed data parallel-fashion and synchronises batch normalisation
+    layers, if any.
+
+    :param torch.nn.Module module: The module to be moved.
+    :param int local_rank: The local rank of the device.
+    :param bool sync_modules: Whether to synchronise the modules across devices
+        or not. If yes, the module becomes ``DistributedDataParallel``. Default:
+        ``True``.
+    :param Device device: The type of device to which to move the module. This
+        argument is useful because once ``device`` has been globally set, this
+        function can be called regardless of the current device in which the
+        items are and thus helps avoid additional checks. Default:
+        ``Device.GPU``.
+    :param ddp_args: Arguments to `DistributedDataParallel`.
+    :param ddp_kwargs: Keyworded arguments to `DistributedDataParallel`.
+
+    :returns torch.nn.Module: The module moved to the appropriate device.
+    """
+
+    match device:
+        case Device.CPU | Device.MPS:
+            module = module.to(device.value)
+        case _:
+            module = module.cuda(t_device(local_rank))
+
+    if sync_modules and device not in [Device.CPU, Device.MPS]:
+        if has_batch_norm(module):
+            module = SyncBatchNorm.convert_sync_batchnorm(module)
+
+        if dist.is_initialized():
+            module = DistributedDataParallel(
+                module,
+                device_ids=[local_rank],
+                *ddp_args,
+                **ddp_kwargs,
+            )
+
+    return module
+
+
+def get_dataset_sampler(
+    dataset: Dataset, global_rank: int, platform: Platform
+) -> Optional[DistributedSampler]:
+    r"""This function selects a portion of the original dataset shared by other
+    devices. If the device is CPU or MPS, no sharing is necessary.
+
+    :param torch.utils.data.Dataset dataset: The dataset from which to
+        sample for the current device.
+    :param int global_rank: The global rank of the device.
+    :param Platform platform: Platform-related configurations.
+    :returns torch.utils.data.DistributedSampler: Dataset sampler for
+        the given dataset and world size.
+    """
+
+    sampler = None
+
+    if (
+        platform.device not in [Device.CPU, Device.MPS]
+        and platform.world_size > 1
+    ):
+        sampler = DistributedSampler(
+            dataset,
+            num_replicas=platform.world_size,  # , rank=global_rank
+        )
+
+    return sampler
+
+
+def device(module: Module) -> t_device:
+    r"""Given a module, this function returns the device on which it currently
+    resides. If the module has no parameters, the current device is returned by
+    default.
+
+    :param torch.nn.Module module: The module whose device is sought.
+    :returns torch.device: The device of the module.
+    """
+
+    from torch.backends.mps import is_available as is_mps_available
+
+    p = module.parameters()
+    try:
+        device = next(p).device
+    except StopIteration:
+        if is_available():
+            device = t_device(f"cuda:{current_device()}")
+        elif is_mps_available():
+            device = t_device("mps")
+        else:
+            device = t_device("cpu")
+
+    return device
+
+
+def set_device(local_rank: int, platform: Platform) -> None:
+    r"""Sets the device for the thread from which this is called.
+
+    :param int local_rank: The local rank of the device.
+    :param Platform platform: Platform-related configurations.
+    """
+
+    if platform.device in [Device.GPU, Device.SLURM]:
+        __set_device(local_rank)
---
+++ packages/ddpw/ddpw-5.5.1-py3-none-any.whl/ddpw/wrapper.py
@@ -0,0 +1,325 @@
+from datetime import timedelta
+from os import environ
+from os.path import abspath, expanduser, isabs
+from typing import Any, Callable, Tuple
+
+from torch.distributed import (
+    GroupMember,
+    barrier,
+    destroy_process_group,
+    init_process_group,
+    new_group,
+)
+from torch.multiprocessing import Process, set_start_method
+
+from . import functional as DF
+from .io import IO
+from .platform import Device, Platform
+
+
+def setup(
+    node: int,
+    global_rank: int,
+    local_rank: int,
+    platform: Platform,
+    target: Callable[[Tuple, dict], Any],
+    *args,
+    **kwargs,
+):
+    r"""This function is called at the beginning of the process in each device
+    (CPU/GPU). Depending on the needs, this function establishes DDP
+    communication protocols, seeds random number generators, invokes the given
+    task, and performs cleanup tasks.
+
+    :param int node: Node number.
+    :param int global_rank: Global rank of the device.
+    :param int local_rank: Local rank of the device.
+    :param Platform platform: Platform-related configurations.
+    :param Callable target: The callable task to invoke upon finishing setup.
+    :param Optional[Tuple] args: Arguments to be passed to ``target``.
+        Default: ``None``.
+    :param Optional[Dict] kwargs: Keyword arguments to be passed to ``target``.
+        Default: ``None``.
+    """
+
+    node_info = f"Node {node}, GPU {global_rank}(G)/{local_rank}(L)"
+
+    IO.print(f"[{node_info}] Initialising the process.")
+
+    if platform.requires_ipc:
+        environ["MASTER_ADDR"] = platform.master_addr
+        port = environ["MASTER_PORT"] = str(platform.master_port)
+
+        im = f"{platform.ipc_protocol}://{environ['MASTER_ADDR']}"
+        if port is not None:
+            im = f"{im}:{environ['MASTER_PORT']}"
+
+        IO.print(f"[{node_info}] IPC at {im}.")
+
+        # do not specify the rank if process groups are to be used
+        init_process_group(
+            backend=platform.backend,
+            init_method=im,
+            rank=global_rank,
+            world_size=platform.world_size,
+        )
+
+    # 1. Seed random number generators
+    IO.print(
+        f"[{node_info}] "
+        + f"Seeding random number generators with {platform.seed}."
+    )
+    DF.seed_generators(platform.seed)
+
+    # organise groups
+    grp = GroupMember.WORLD
+    if (
+        platform.requires_ipc
+        and platform.ipc_groups is not None
+        and len(platform.ipc_groups) > 0
+    ):
+        device_group = grp
+        for device_group in platform.ipc_groups:
+            if global_rank in device_group:
+                break
+        grp = new_group(
+            ranks=device_group,
+            timeout=timedelta(seconds=30),
+            backend=platform.backend,
+        )
+
+    # 2. Wait for all the processes in this group to synchronise and then start
+    # the task
+    if platform.requires_ipc:
+        barrier(grp)
+
+    # 3. Invoke the given task
+    IO.print(f"[{node_info}] All setup finished.")
+    _kwargs = {
+        **kwargs,
+        "global_rank": global_rank,
+        "local_rank": local_rank,
+        "group": grp,
+        "platform": platform,
+    }
+    target(*args, **_kwargs)
+
+    # 4. Cleanup
+    if platform.requires_ipc:
+        destroy_process_group(grp)
+    IO.print(f"[{node_info}] Tasks on device complete.")
+
+
+class Wrapper:
+    r"""
+    This class is the highest level of abstraction: it accepts the
+    platform-related configurations and initialises the setup accordingly. When
+    given a task, it then runs the task according to the specified
+    configurations.
+
+    .. admonition:: Example
+        :class: tip
+
+        .. code:: python
+
+            from ddpw import Platform, Wrapper
+
+            wrapper = Wrapper(Platform(...))
+
+            wrapper.start(some_callable)
+
+    :param Platform platform: Platform-related configurations.
+    """
+
+    def __init__(self, platform: Platform):
+        IO.verbose = platform.verbose
+
+        IO.print("Initialising the DDP Wrapper.")
+        self.platform = platform
+
+        if platform.requires_ipc:
+            try:
+                set_start_method(platform.spawn_method)
+            except RuntimeError as e:
+                IO.print(
+                    f"Warning: {e}. Skipping setting the start method for forks."
+                )
+
+    def __gpu(self, target: Callable[[Tuple, dict], Any], *args, **kwargs):
+        r"""This method spins up a process for each GPU in the world. It assigns
+        the task to be run on each process, `viz.`, distributing the datasets
+        and models and commencing the task.
+
+        :param Callable target: The function to call on each GPU upon setup.
+        :param Optional[Tuple] args: Arguments to be passed to ``target``.
+            Default: ``None``.
+        :param Optional[Dict] kwargs: Keyword arguments to be passed to
+            ``target``. Default: ``None``.
+        """
+
+        if self.platform.world_size == 1:
+            node_info = "Node 0, GPU 0(G)/0(L)"
+            IO.print(f"[{node_info}] Task starting on GPU.")
+            setup(0, 0, 0, self.platform, target, *args, **kwargs)
+            return
+
+        IO.print(f"Spawning {self.platform.world_size} processes.")
+        processes = []
+
+        # create a process for each GPU in the world
+        for rank in range(self.platform.world_size):
+            p = Process(
+                target=setup,
+                args=(0, rank, rank, self.platform, target, *args),
+                kwargs=kwargs,
+            )
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
+        IO.print("All processes complete.")
+
+    def __slurm(self, target: Callable, console_logs: str):
+        r"""Similar to :py:meth:`.__gpu` but for SLURM. An additional step
+        includes spinning up a process for each node, done with ``submitit``.
+
+        :param Callable target: The function to call on each GPU upon
+            setup.
+        :param str console_logs: Location to save SLURM console logs.
+        """
+        from submitit import AutoExecutor
+
+        IO.print("Setting up the SLURM platform.")
+
+        executor = AutoExecutor(folder=console_logs)
+        executor.update_parameters(
+            name=self.platform.name,
+            mem_gb=self.platform.ram,
+            gpus_per_node=self.platform.n_gpus,
+            tasks_per_node=self.platform.n_gpus,
+            cpus_per_task=self.platform.n_cpus,
+            nodes=self.platform.n_nodes,
+            timeout_min=self.platform.timeout_min,
+            slurm_partition=self.platform.partition,
+        )
+
+        if self.platform.slurm_additional_parameters is not None:
+            executor.update_parameters(
+                slurm_additional_parameters=self.platform.slurm_additional_parameters
+            )
+
+        return executor.submit(target)
+
+    def __get_hostname(self) -> str:
+        r"""Returns the host name of the master node."""
+
+        from socket import gethostbyname
+        from subprocess import check_output
+
+        cmd = ["scontrol", "show", "hostnames", environ["SLURM_NODELIST"]]
+        host = check_output(cmd).decode().splitlines()[0]
+
+        return gethostbyname(host)
+
+    def start(self, target: Callable[[Tuple, dict], Any], *args, **kwargs):
+        r"""This method performs the necessary setup according to the specified
+        configurations and then invokes the given task.
+
+        :param Callable[[Tuple, dict], Any] target: The task, a callable.
+        :param Optional[Tuple] args: Arguments to be passed to ``target``.
+            Default: ``None``.
+        :param Optional[Dict] kwargs: Keyword arguments to be passed to
+            ``target``. Default: ``None``.
+        """
+
+        self.platform.print()
+
+        IO.print("Starting process(es).")
+
+        def finished():
+            if self.platform.upon_finish is not None and callable(
+                self.platform.upon_finish
+            ):
+                return self.platform.upon_finish()
+
+        match self.platform.device:
+            case Device.CPU | Device.MPS:
+                setup(0, 0, 0, self.platform, target, *args, **kwargs)
+                finished()
+            case Device.GPU:
+                self.__gpu(target, *args, **kwargs)
+                finished()
+            case Device.SLURM:
+
+                def individual_gpu():
+                    r"""This nested function is the starting point for each
+                    SLURM-based GPU."""
+                    from submitit import JobEnvironment
+
+                    job_env = JobEnvironment()
+
+                    self.platform.master_addr = self.__get_hostname()
+
+                    details = f"""
+                    \r • Node: {job_env.node}.
+                    \r • Global rank: {job_env.global_rank}.
+                    \r • Local rank: {job_env.local_rank}.
+                    """
+                    IO.print(details)
+
+                    setup(
+                        job_env.node,
+                        job_env.global_rank,
+                        job_env.local_rank,
+                        self.platform,
+                        target,
+                        *args,
+                        **kwargs,
+                    )
+
+                    if job_env.global_rank == 0:
+                        finished()
+
+                job = self.__slurm(individual_gpu, self.platform.console_logs)
+
+                p = self.platform.console_logs
+                if not isabs(p):
+                    p = abspath(expanduser(p))
+                details = f"""
+                \rSLURM job "{self.platform.name}" ({job.job_id}) scheduled.
+                \rSee respective device logs for output on those devices.
+                \rLogs saved at {p}.
+                """
+
+                print(details)
+
+
+def wrapper(platform: Platform):
+    r"""A decorator that can be applied to callables.
+
+    :param Platform platform: Platform details.
+
+    .. admonition:: Example
+        :class: tip
+
+        .. code:: python
+
+            from ddpw import Platform, wrapper
+
+            platform = Platform(device='gpu', n_gpus=2, n_cpus=2)
+
+            @wrapper(platform)
+            def run(*args, **kwargs):
+                # some task
+                pass
+    """
+
+    def __ddpw(fn):
+        def __wrapper(*args, **kwargs):
+            Wrapper(platform).start(fn, *args, **kwargs)
+
+        return __wrapper
+
+    return __ddpw
