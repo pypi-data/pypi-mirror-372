0282e9b93dec3cb58d83104496b1c1bb0613f900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 07:05:33 +0000

    Add package sqlmesh/0.211.1/sqlmesh-0.211.1.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.211.1.tar.gz/sqlmesh-0.211.1/sqlmesh_dbt/error.py
@@ -0,0 +1,29 @@
+import typing as t
+import logging
+from functools import wraps
+import click
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def cli_global_error_handler(
+    func: t.Callable[..., t.Any],
+) -> t.Callable[..., t.Any]:
+    @wraps(func)
+    def wrapper(*args: t.List[t.Any], **kwargs: t.Any) -> t.Any:
+        try:
+            return func(*args, **kwargs)
+        except Exception as ex:
+            # these imports are deliberately deferred to avoid the penalty of importing the `sqlmesh`
+            # package up front for every CLI command
+            from sqlmesh.utils.errors import SQLMeshError
+            from sqlglot.errors import SqlglotError
+
+            if isinstance(ex, (SQLMeshError, SqlglotError, ValueError)):
+                click.echo(click.style("Error: " + str(ex), fg="red"))
+                sys.exit(1)
+            else:
+                raise
+
+    return wrapper
