ba2b1ec439fd60245d1c5453dbd82e976a7c0865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:06:13 +0000

    Add package tomlconfigurer/0.2.2/tomlconfigurer-0.2.2.tar.gz

---
+++ packages/tomlconfigurer/tomlconfigurer-0.2.2.tar.gz/tomlconfigurer-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tomlconfigurer",
+    version="0.2.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["toml>=0.10"],
+    python_requires=">=3.6",
+    author="xystudio",
+    author_email="173288240@qq.com",
+    description="A configurer with toml.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/xystudio889/tomlconfigurer",
+    include_package_data=True,
+    entry_points={"console_scripts": ["tomlconfigurer = configurer:main"]},
+    extras_require={"dev": []},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["config", "toml", "configure", "configurer"],
+)
