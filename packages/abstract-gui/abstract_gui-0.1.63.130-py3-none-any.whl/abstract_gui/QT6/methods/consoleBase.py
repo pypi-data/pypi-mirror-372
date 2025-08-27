83bd03d5ccb259cbf27a010b9e554cb900dd7229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:30 +0000

    Add package abstract-gui/0.1.63.130/abstract_gui-0.1.63.130-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.1.63.130-py3-none-any.whl/abstract_gui/QT6/methods/consoleBase.py
@@ -0,0 +1,19 @@
+from ..imports import QObject, pyqtSignal, QWidget, QVBoxLayout, QTabWidget
+class SharedStateBus(QObject):
+    stateBroadcast = pyqtSignal(object, dict)  # (sender, state)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._snap: dict = {}
+
+    def snapshot(self) -> dict:
+        return dict(self._snap)
+
+    def push(self, sender, state: dict):
+        self._snap = dict(state)
+        self.stateBroadcast.emit(sender, self.snapshot())
+class ConsoleBase(QWidget):
+    def __init__(self, *, bus=None, parent=None):
+        super().__init__(parent)
+        self.bus = bus or SharedStateBus(self)
+        self.setLayout(QVBoxLayout())
