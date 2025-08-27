61da2b5f81e527b92ddfd75c7807a7c8b00dc225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 05:05:05 +0000

    Add package ledsign/0.2.0/ledsign-0.2.0.tar.gz

---
+++ packages/ledsign/ledsign-0.2.0.tar.gz/ledsign-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+
+VERSION="0.2.0"
+
+
+
+with open("README.md","r") as rf:
+	description=rf.read()
+setuptools.setup(
+	name="ledsign",
+	version=VERSION,
+	description="LED Sign Python API",
+	url="https://github.com/krzem5/ledsign",
+	project_urls={
+		"Source Code": "https://github.com/krzem5/ledsign",
+	},
+	author="Krzesimir Hyżyk",
+	license="BSD-3-Clause",
+	classifiers=[],
+	python_requires=">=3.9",
+	install_requires=[],
+	packages=["ledsign"],
+	long_description=description,
+	long_description_content_type="text/markdown"
+)
