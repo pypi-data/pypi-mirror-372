555ea23b71f7680e8c73f603a168d066408dc350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 08:05:17 +0000

    Add package stinger-ipc/0.0.5/stinger_ipc-0.0.5-py3-none-any.whl

---
+++ packages/stinger-ipc/stinger_ipc-0.0.5-py3-none-any.whl/stingeripc/tools/markdown_generator.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from jacobsjinjatoo import templator as jj2
+import os.path
+import typer
+from typing_extensions import Annotated
+from stingeripc import StingerInterface
+from rich import print
+from pathlib import Path
+
+def main(inname: Annotated[Path, typer.Argument(exists=True, file_okay=True, dir_okay=False, readable=True)], outdir: Annotated[Path, typer.Argument(file_okay=False, dir_okay=True, writable=True, readable=True)]):
+    print(f"[bold green]Reading:[/bold green] {inname}")
+    with open(inname, "r") as f:
+        stinger = StingerInterface.from_yaml(f)
+    params = {
+        "stinger": stinger,
+    }
+    t = jj2.CodeTemplator(output_dir=os.path.dirname(outdir))
+    t.add_template_dir(
+        os.path.join(os.path.dirname(__file__), "../templates", "markdown")
+    )
+    for output_file in [
+        "index.md",
+    ]:
+        print(f"[bold red]GENERATING:[/bold red] {output_file}")
+        t.render_template(f"{output_file}.jinja2", output_file, **params)
+
+def run():
+    typer.run(main)
+
+if __name__ == "__main__":
+    run()
