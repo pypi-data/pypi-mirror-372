e2f14d4397db7325cf33cb37229a0ee37bb5858e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:04:46 +0000

    Add package deezspot-spotizerr/3.0.0/deezspot_spotizerr-3.0.0-py3-none-any.whl

---
+++ packages/deezspot-spotizerr/deezspot_spotizerr-3.0.0-py3-none-any.whl/deezspot/models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+Deezspot models package.
+"""
+
+# Import subpackages to ensure they're included
+from deezspot.models import callback
+from deezspot.models import download
