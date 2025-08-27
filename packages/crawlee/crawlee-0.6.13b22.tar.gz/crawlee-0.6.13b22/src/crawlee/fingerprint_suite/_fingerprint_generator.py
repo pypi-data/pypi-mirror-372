5f6c76ae75a6e9c3e7e468811a6a0739800c0f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:06:28 +0000

    Add package crawlee/0.6.13b22/crawlee-0.6.13b22.tar.gz

---
+++ packages/crawlee/crawlee-0.6.13b22.tar.gz/crawlee-0.6.13b22/src/crawlee/fingerprint_suite/_fingerprint_generator.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from browserforge.fingerprints import Fingerprint
+
+
+class FingerprintGenerator(ABC):
+    """A class for creating browser fingerprints that mimic browser fingerprints of real users."""
+
+    @abstractmethod
+    def generate(self) -> Fingerprint:
+        """Generate browser fingerprints.
+
+        This is experimental feature.
+        Return type is temporarily set to `Fingerprint` from `browserforge`. This is subject to change and most likely
+        it will change to custom `Fingerprint` class defined in this repo later.
+        """
