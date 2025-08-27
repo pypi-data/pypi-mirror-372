e818b1e2606dd7bdd925dcae05f5bc527e69b92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 05:05:10 +0000

    Add package tapestrysdk/0.1.44/tapestrysdk-0.1.44.tar.gz

---
+++ packages/tapestrysdk/tapestrysdk-0.1.44.tar.gz/tapestrysdk-0.1.44/tapestrysdk/list_s3_doc/list_s3_doc.py
@@ -0,0 +1,30 @@
+import requests
+
+def list_s3_doc(token, tapestry_id):
+    url =  "https://inthepicture.org/admin/list_s3_doc"
+    headers = {
+        "accept": "application/json, text/plain, */*",
+        "authorization": f"Bearer {token}",
+        "content-type": "application/json",
+    }
+    data = {
+        "tapestry_id": tapestry_id,
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    try:
+        resp_json = response.json()
+    except Exception:
+        resp_json = {}
+
+    if response.status_code == 200 and resp_json.get("success", False):
+        return resp_json
+    else:
+        message = resp_json.get("message", response.text)
+        return {
+            "success": False,
+            "code": response.status_code,
+            "message": message,
+            "body": {}
+        }
+ 
