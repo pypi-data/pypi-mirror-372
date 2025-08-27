26ddc3a6d99ba0f3893499f8f4c52bb0965485f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:12 +0000

    Add package abstract-gui/0.1.63.141/abstract_gui-0.1.63.141-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.1.63.141-py3-none-any.whl/abstract_gui/QT6/managers/windowManagerConsole/imports/imports.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from ....imports import *
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication,QTextEdit,QMessageBox
+from PyQt6 import QtWidgets, QtGui, QtCore, QtGui, QtWidgets
+SB = QMessageBox.StandardButton
+IDR = Qt.ItemDataRole
