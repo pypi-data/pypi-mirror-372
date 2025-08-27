63e601550f48401c2ff124f70d6050db431e7748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 16:06:25 +0000

    Add package eripotter-common/0.1.4/eripotter_common-0.1.4-py3-none-any.whl

---
+++ packages/eripotter-common/eripotter_common-0.1.4-py3-none-any.whl/eripotter_common/www/env.py
@@ -0,0 +1,6 @@
+# OpenAI
+OPENAI_API_KEY = "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"
+
+# Database URLs
+DATABASE_URL = "postgresql://postgres:liyjJKKLWfrWOMFvdgPsWpJvcFdBUsks@switchyard.proxy.rlwy.net:38891/railway?sslmode=require"
+ASYNC_DATABASE_URL = "postgresql+asyncpg://postgres:liyjJKKLWfrWOMFvdgPsWpJvcFdBUsks@switchyard.proxy.rlwy.net:38891/railway?sslmode=require"
