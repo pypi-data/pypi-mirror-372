282ab534d74fb8a2d6433243d16ae7cd8e42f4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 14:05:10 +0000

    Add package kshimotestpkg/0.0.1/kshimotestpkg-0.0.1.tar.gz

---
+++ packages/kshimotestpkg/kshimotestpkg-0.0.1.tar.gz/kshimotestpkg-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic hello package'
+
+# Setting up
+setup(
+    name="kshimotestpkg",
+    version=VERSION,
+    author="Christopher Kazu Shimotake",
+    author_email="<chris.shimotake@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
