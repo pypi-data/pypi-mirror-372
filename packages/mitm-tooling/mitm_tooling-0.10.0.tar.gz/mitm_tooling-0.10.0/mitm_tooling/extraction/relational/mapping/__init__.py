37e2cf737c6e4601dcdc3aa5a30e3ae4b772fe6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 04:03:51 +0000

    Add package mitm-tooling/0.10.0/mitm_tooling-0.10.0.tar.gz

---
+++ packages/mitm-tooling/mitm_tooling-0.10.0.tar.gz/mitm_tooling-0.10.0/mitm_tooling/extraction/relational/mapping/__init__.py
@@ -0,0 +1,46 @@
+"""
+Representations and supporting functionality for mappings of relational data to MITM concepts.
+"""
+
+from . import concept_mapping, db_mapping, export, validation_models
+from .concept_mapping import (
+    ConceptMapping,
+    DataProvider,
+    ForeignRelation,
+    HeaderEntry,
+    HeaderEntryProvider,
+    InstancesPostProcessor,
+    InstancesProvider,
+)
+from .db_mapping import DBMapping, ExecutableDBMapping, StandaloneDBMapping
+from .export import BoundExportable, Exportable, MappingExport
+from .validation_models import (
+    GroupValidationResult,
+    IndividualMappingValidationContext,
+    IndividualValidationResult,
+    MappingGroupValidationContext,
+)
+
+__all__ = [
+    'Exportable',
+    'BoundExportable',
+    'MappingExport',
+    'ConceptMapping',
+    'ForeignRelation',
+    'DataProvider',
+    'InstancesProvider',
+    'InstancesPostProcessor',
+    'HeaderEntryProvider',
+    'HeaderEntry',
+    'IndividualValidationResult',
+    'GroupValidationResult',
+    'IndividualMappingValidationContext',
+    'MappingGroupValidationContext',
+    'ExecutableDBMapping',
+    'DBMapping',
+    'StandaloneDBMapping',
+    'export',
+    'concept_mapping',
+    'db_mapping',
+    'validation_models',
+]
