6c6ed05ab4cb48526c2786655c265bf4837870cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 06:04:21 +0000

    Add package replicate/1.1.0b3/replicate-1.1.0b3-py3-none-any.whl

---
+++ packages/replicate/replicate-1.1.0b3-py3-none-any.whl/replicate/__init__.py
@@ -0,0 +1,45 @@
+from replicate.client import Client
+from replicate.pagination import async_paginate as _async_paginate
+from replicate.pagination import paginate as _paginate
+from replicate.use import get_path_url, use
+
+__all__ = [
+    "Client",
+    "use",
+    "run",
+    "async_run",
+    "stream",
+    "async_stream",
+    "paginate",
+    "async_paginate",
+    "collections",
+    "deployments",
+    "files",
+    "hardware",
+    "models",
+    "predictions",
+    "trainings",
+    "webhooks",
+    "default_client",
+    "get_path_url",
+]
+
+default_client = Client()
+
+run = default_client.run
+async_run = default_client.async_run
+
+stream = default_client.stream
+async_stream = default_client.async_stream
+
+paginate = _paginate
+async_paginate = _async_paginate
+
+collections = default_client.collections
+deployments = default_client.deployments
+files = default_client.files
+hardware = default_client.hardware
+models = default_client.models
+predictions = default_client.predictions
+trainings = default_client.trainings
+webhooks = default_client.webhooks
