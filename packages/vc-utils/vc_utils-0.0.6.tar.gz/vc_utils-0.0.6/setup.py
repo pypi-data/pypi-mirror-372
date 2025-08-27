866b9531114dff88d2212616bbc6cf8406d6f93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 17:06:42 +0000

    Add package vc-utils/0.0.6/vc_utils-0.0.6.tar.gz

---
+++ packages/vc-utils/vc_utils-0.0.6.tar.gz/vc_utils-0.0.6/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="vc-utils",
+    version="0.0.6",
+    author="Chatavut Viriyasuthee",
+    author_email="chatavut@lab.ai",
+    description="Vulcan Coalition utililities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vulcan-coalition/vulcan-utils",
+    packages=["linkage"],
+    data_files=[("", ["conf/default.json"])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "shortuuid",
+        "dotenv",
+        "requests"
+    ]
+)
