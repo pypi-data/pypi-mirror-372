744c74b209def9ecbf6529fa39cac354f38b2d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-27 15:04:46 +0000

    Add package logery/1.1.3/logery-1.1.3.tar.gz

---
+++ packages/logery/logery-1.1.3.tar.gz/logery-1.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='logery',
+    version='1.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'rich',
+        'python-dotenv'
+    ],
+    description='Logs personalizados',
+    author='Miguel Tenório',
+    author_email='deepydev42@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
