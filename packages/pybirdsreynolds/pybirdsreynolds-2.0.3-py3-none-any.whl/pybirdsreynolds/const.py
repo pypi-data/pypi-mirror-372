a65c0e1a11ec6ac2dc3b5cef84531ab2b4c4c59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:06:25 +0000

    Add package pybirdsreynolds/2.0.3/pybirdsreynolds-2.0.3-py3-none-any.whl

---
+++ packages/pybirdsreynolds/pybirdsreynolds-2.0.3-py3-none-any.whl/pybirdsreynolds/const.py
@@ -0,0 +1,17 @@
+from importlib.metadata import version
+
+VERSION_PROG = version("pybirdsreynolds")
+SHORT_DESCRIPTION = (
+    "pybirdsreynolds is an interactive simulation of the Reynolds Boids model."
+)
+MARGIN = 1
+WIDTH_CONTROLS_DEFAULT = 70
+WIDTH_PARAMS_DEFAULT = 300
+HEIGHT_PARAMS_CONTROLS_DEFAULT = 450
+FONT_TYPE_LIST = [
+    "Nota Sans Mono",
+    "Consolas",
+    "Courier",
+    "Ubuntu Sans Mono",
+    "DejaVu Sans Mono",
+]
