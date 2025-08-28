c7233eb82f62f59e3c47cda1247230938987adc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 08:06:02 +0000

    Add package courts-db/0.10.26/courts_db-0.10.26-py3-none-any.whl

---
+++ packages/courts-db/courts_db-0.10.26-py3-none-any.whl/courts_db/__init__.py
@@ -0,0 +1,254 @@
+import re
+from datetime import datetime
+from typing import Optional
+
+from courts_db.text_utils import strip_punc
+
+from .utils import gather_regexes, load_courts_db, make_court_dictionary
+
+__all__ = [
+    # lazy-loaded data structures:
+    "courts",
+    "court_dict",
+    "regexes",
+    # helper functions:
+    "find_court_ids_by_name",
+    "filter_courts_by_date",
+    "filter_courts_by_bankruptcy",
+    "find_court_by_id",
+    "find_court",
+]
+
+
+def __getattr__(name):
+    """Lazy load data structures from loaders module."""
+    if name == "courts":
+        value = load_courts_db()
+    elif name == "court_dict":
+        from . import courts
+
+        value = make_court_dictionary(courts)
+    elif name == "regexes":
+        from . import courts
+
+        value = gather_regexes(courts)
+    else:
+        raise AttributeError(f"module {__name__} has no attribute {name}")
+    globals()[name] = value
+    return value
+
+
+def find_court_ids_by_name(
+    court_str: str,
+    bankruptcy: Optional[bool],
+    location: Optional[str],
+    allow_partial_matches: bool,
+) -> list[str]:
+    """Find court IDs with our courts-db regex list
+
+    :param court_str: test string
+    :param bankruptcy: Are we searhing for a bankruptcy court
+    :return: List of Court IDs matched
+    """
+    from . import regexes
+
+    assert isinstance(court_str, str), (
+        f"court_str is not a text type, it's of type {type(court_str)}"
+    )
+
+    court_matches = set()
+    matches = []
+    for (
+        regex,
+        court_id,
+        _court_name,
+        court_type,
+        court_location,
+        parent_court,
+    ) in regexes:
+        # If location provided - check if overlapped.
+        if location and court_location != location:
+            continue
+        # Filter gathered regexes by bankruptcy flag.
+        if bankruptcy is True:
+            if court_type != "bankruptcy":
+                continue
+        elif bankruptcy is False and court_type == "bankruptcy":
+            continue
+        match = re.search(regex, court_str)
+        if match:
+            if (
+                not allow_partial_matches
+                and len(court_str) != match.span()[1] - match.span()[0]
+            ):
+                continue
+            m = (match.group(0), court_id, parent_court)
+            matches.append(m)
+
+    # If no matches found - check against - Court Name - not regex patterns.
+    if not matches:
+        for court in courts:  # noqa: F821  courts is imported lazily via __getattr__
+            # Add validation for location if provided.
+            if location and court_location != location:
+                continue
+            if strip_punc(court_str.lower()) == strip_punc(
+                court["name"].lower()
+            ):
+                matches.append((court_str, court["id"], court.get("parent")))
+
+    matches = list(set(matches))
+    if len(matches) > 1:
+        matches = reduce_court_list(matches)
+
+    matched_strings = [m[0] for m in matches]
+    filtered_list = filter(
+        lambda x: [x for i in matched_strings if x in i and x != i] == [],
+        matched_strings,
+    )
+    for item in list(filtered_list):
+        for mat in matches:
+            if item == mat[0]:
+                court_matches.add(mat[1])
+    return list(court_matches)
+
+
+def reduce_court_list(court_list):
+    """Reduce to lowest possible match
+
+    :param court_list: List of matches
+    :return: Return lowest possible match
+    """
+    parent_ids = {parent_id for _, _, parent_id in court_list}
+    reduced_list = []
+    for court in court_list:
+        court_id = court[1]
+        if court_id not in parent_ids:
+            reduced_list.append(court)
+    return reduced_list
+
+
+def filter_courts_by_date(matches, date_found, strict_dates=False):
+    """Filter IDs by date found.
+
+    Strict dates should be more useful as dates are filled in.
+    :param matches:
+    :param date_found: datetime object
+    :param strict_dates: Boolean that helps tell the sytsem how to handle
+    null dates in courts-db
+    :return: List of court IDs matched
+    """
+    from . import courts
+
+    assert type(date_found) is datetime, (
+        f"date_found is not a date object, it's of type {type(date_found)}"
+    )
+
+    results = [court for court in courts if court["id"] in matches]
+    filtered_results = []
+    for result in results:
+        for date_object in result["dates"]:
+            date_start = date_object["start"]
+            date_end = date_object["end"]
+            if not strict_dates:
+                if date_start is None:
+                    date_start = "1600-01-01"
+                if date_end is None:
+                    date_end = "2100-01-01"
+            if strict_dates:
+                if date_start is None:
+                    continue
+                if date_end is None:
+                    date_end = "2100-01-01"
+
+            date_start = datetime.strptime(date_start, "%Y-%m-%d")
+            date_end = datetime.strptime(date_end, "%Y-%m-%d")
+
+            if date_start <= date_found <= date_end:
+                filtered_results.append(result["id"])
+
+    return filtered_results
+
+
+def filter_courts_by_bankruptcy(matches, bankruptcy):
+    from . import courts
+
+    results = [court for court in courts if court["id"] in matches]
+    if bankruptcy:
+        return [
+            court["id"] for court in results if court["type"] == "bankruptcy"
+        ]
+    return [court["id"] for court in results if court["type"] != "bankruptcy"]
+
+
+def find_court_by_id(court_id):
+    """Find court dictionary using court id code.
+
+    :param court_id: Court code used by Courtlistener.com
+    :return: Return dictionary court object from db
+    """
+    from . import courts
+
+    return [court for court in courts if court["id"] == court_id]
+
+
+def _filter_parents_from_list(matches: list[str]) -> list[str]:
+    """Remove parents from the list of matches.
+
+    Check if a match has a parent in the list.  If so remove it.
+
+    :param matches: List of court ids
+    :return: List of court ids without parents
+    """
+    parents = []
+    for match in matches:
+        parent = find_court_by_id(match)[0].get("parent", None)
+        if parent in matches:
+            parents.append(parent)
+    if parents:
+        matches = list(set(matches) ^ set(parents))
+    return matches
+
+
+def find_court(
+    court_str: str,
+    bankruptcy: Optional[bool] = None,
+    date_found: Optional[datetime] = None,
+    strict_dates: Optional[bool] = False,
+    location: Optional[str] = None,
+    allow_partial_matches: Optional[bool] = False,
+) -> list[str]:
+    """Finds a list of court ID for a given string and parameters
+
+    :param court_str: The unicode string we are testing
+    :param bankruptcy: Tells function to exclude or include bankruptcy cases
+    :param date_found: Date object
+    :param strict_dates: Boolean that helps tell the system how to handle
+    :param location: Where the court is located.
+    :allow_partial_matches: Allow partial string matches useful if given a sent.
+    :return: List of court IDs if any
+    """
+    court_str = strip_punc(court_str)
+    matches = find_court_ids_by_name(
+        court_str, bankruptcy, location, allow_partial_matches
+    )
+
+    # Check bankruptcy cases if appropriate
+    if bankruptcy is not None:
+        matches = filter_courts_by_bankruptcy(
+            matches=matches, bankruptcy=bankruptcy
+        )
+
+    # Match against dates
+    if date_found:
+        matches = filter_courts_by_date(
+            matches=matches, date_found=date_found, strict_dates=strict_dates
+        )
+
+    # Check if the matches are parent/children of each other
+    # for example California Court of Appeal, First Appellate District
+    # is a child of California Court of Appeals
+    # Reduce that list to just child element.
+    if len(matches) > 1:
+        matches = _filter_parents_from_list(matches)
+
+    return matches
---
+++ packages/courts-db/courts_db-0.10.26-py3-none-any.whl/courts_db/utils.py
@@ -0,0 +1,210 @@
+import json
+import os
+import re
+from glob import iglob
+from string import Template
+
+db_root = os.path.dirname(os.path.realpath(__file__))
+
+ordinals = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "nineth",
+    "tenth",
+    "eleventh",
+    "twelveth",
+    "thirteenth",
+    "fourteenth",
+    "fifteenth",
+    "sixteenth",
+    "seventeenth",
+    "eighteenth",
+    "nineteenth",
+    "twentieth",
+    "twenty(-| )first",
+    "twenty(-| )secondth",
+    "twenty(-| )third",
+    "twenty(-| )fourth",
+    "twenty(-| )fifth",
+    "twenty(-| )sixth",
+    "twenty(-| )seventh",
+    "twenty(-| )eighth",
+    "twenty(-| )nineth",
+    "thirtieth",
+    "thirty(-| )first",
+    "thirty(-| )secondth",
+    "thirty(-| )third",
+    "thirty(-| )fourth",
+    "thirty(-| )fifth",
+    "thirty(-| )sixth",
+    "thirty(-| )seventh",
+    "thirty(-| )eighth",
+    "thirty(-| )nineth",
+    "fortieth",
+    "fourty(-| )first",
+    "fourty(-| )secondth",
+    "fourty(-| )third",
+    "fourty(-| )fourth",
+    "fourty(-| )fifth",
+    "fourty(-| )sixth",
+    "fourty(-| )seventh",
+    "fourty(-| )eighth",
+    "fourty(-| )nineth",
+    "fiftieth",
+    "fifty(-| )first",
+    "fifty(-| )secondth",
+    "fifty(-| )third",
+    "fifty(-| )fourth",
+    "fifty(-| )fifth",
+    "fifty(-| )sixth",
+    "fifty(-| )seventh",
+    "fifty(-| )eighth",
+    "fifty(-| )nineth",
+    "sixtieth",
+    "sixty(-| )first",
+    "sixty(-| )secondth",
+    "sixty(-| )third",
+    "sixty(-| )fourth",
+    "sixty(-| )fifth",
+    "sixty(-| )sixth",
+    "sixty(-| )seventh",
+    "sixty(-| )eighth",
+    "sixty(-| )nineth",
+    "seventieth",
+    "seventy(-| )first",
+    "seventy(-| )secondth",
+    "seventy(-| )third",
+    "seventy(-| )fourth",
+    "seventy(-| )fifth",
+    "seventy(-| )sixth",
+    "seventy(-| )seventh",
+    "seventy(-| )eighth",
+    "seventy(-| )nineth",
+    "eightieth",
+    "eighty(-| )first",
+    "eighty(-| )secondth",
+    "eighty(-| )third",
+    "eighty(-| )fourth",
+    "eighty(-| )fifth",
+    "eighty(-| )sixth",
+    "eighty(-| )seventh",
+    "eighty(-| )eighth",
+    "eighty(-| )nineth",
+    "ninetieth",
+    "ninety(-| )first",
+    "ninety(-| )secondth",
+    "ninety(-| )third",
+    "ninety(-| )fourth",
+    "ninety(-| )fifth",
+    "ninety(-| )sixth",
+    "ninety(-| )seventh",
+    "ninety(-| )eighth",
+    "ninety(-| )nineth",
+    "one[- ]hundredth",
+]
+
+# def get_court_data_from_ids(id_list):
+#     cd = {}
+#     for id in id_list:
+#         cd[id] = court
+#     return cd
+
+
+def make_court_dictionary(courts):
+    cd = {}
+    for court in courts:
+        cd[court["id"]] = court
+    return cd
+
+
+def load_courts_db():
+    """Load the court data from disk, and render regex variables
+
+    Court data is on disk as one main JSON file, another containing variables,
+    and several others containing placenames. These get combined via Python's
+    template system and loaded as a Python object
+
+    :return: A python object containing the rendered courts DB
+    """
+    with open(
+        os.path.join(db_root, "data", "variables.json"), encoding="utf-8"
+    ) as v:
+        variables = json.load(v)
+
+    for path in iglob(os.path.join(db_root, "data", "places", "*.txt")):
+        with open(path, encoding="utf-8") as p:
+            places = f"({'|'.join(p.read().splitlines())})"
+            variables[path.split(os.path.sep)[-1].split(".txt")[0]] = places
+
+    # Add in code to allow ordinal creation for many many judicial district numbers
+    # for example 1 to 56 judicial districts
+    with open(
+        os.path.join(db_root, "data", "courts.json"), encoding="utf-8"
+    ) as f:
+        temp = f.read()
+        ord_arrays = re.findall(r"\${(\d+)-(\d+)}", temp)
+        for ord in ord_arrays:
+            re_ord = (
+                f"(({')|('.join(ordinals[int(ord[0]) - 1 : int(ord[1])])}))"
+            )
+            temp = temp.replace(f"${{{ord[0]}-{ord[1]}}}", re_ord)
+
+    with open(
+        os.path.join(db_root, "data", "courts.json"), encoding="utf-8"
+    ) as f:
+        s = Template(temp).substitute(**variables)
+    s = s.replace("\\", "\\\\")
+    data = json.loads(s)
+
+    for k in data:
+        # If a child of a parent court - add parent data to child if not present
+        # this should allow for streamed down data.  Inheritance
+        if "parent" in k and not {"dates", "type", "location"} <= set(
+            k.keys()
+        ):
+            parent = [x for x in data if x["id"] == k["parent"]][0]
+            if "dates" not in k:
+                k["dates"] = parent["dates"]
+            if "type" not in k:
+                k["type"] = parent["type"]
+            if "location" not in k:
+                k["location"] = parent["location"]
+
+    return data
+
+
+def gather_regexes(courts):
+    """Create a variable mapping regexes to court IDs
+
+    :param courts: The court DB
+    :type courts: list
+    :return: A list of tuples, with tuple[0] being a compiled regex,
+    tuple[1] being the court ID, tuple[2] being the court name, and tuple[3]
+    being the court type. Example: (<_sre.SRE_Pattern object>, u'ala',
+    'Supreme Court of Alabama', 'appellate')
+    :rtype: list
+    """
+    regexes = []
+    for court in courts:
+        court_regexes = court["regex"] + [court["name"]]
+        for reg_str in court_regexes:
+            # Unwind the extra gruff in regexes
+            reg_str = reg_str.replace("\\\\", "\\")
+            regex = re.compile(reg_str, (re.I | re.U))
+            regexes.append(
+                (
+                    regex,
+                    court["id"],
+                    court["name"],
+                    court["type"],
+                    court.get("location"),
+                    court.get("parent"),
+                )
+            )
+    return regexes
