d6e3505fb9b7b7f731445a0bf07cdffd17309bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:07:20 +0000

    Add package pyWikiCMS/0.4.6/pywikicms-0.4.6-py3-none-any.whl

---
+++ packages/pyWikiCMS/pywikicms-0.4.6-py3-none-any.whl/frontend/wikicms.py
@@ -0,0 +1,464 @@
+"""
+Created on 2020-07-27
+
+@author: wf
+"""
+
+import logging
+import re
+import traceback
+
+import requests
+from bs4 import BeautifulSoup, Comment
+from bs4.element import NavigableString, Tag
+from fastapi import Response
+from fastapi.responses import HTMLResponse
+from mogwai.core.mogwaigraph import MogwaiGraph
+from tqdm import tqdm
+from wikibot3rd.smw import SMWClient
+from wikibot3rd.wikiclient import WikiClient
+
+from backend.site import FrontendSite
+from frontend.frame import HtmlFrame
+
+
+class WikiFrontend(object):
+    """
+    Wiki Content Management System Frontend
+    """
+
+    with_login: bool = True
+
+    def __init__(
+        self,
+        frontend: FrontendSite,
+        parser: str = "lxml",
+        proxy_prefixes=["/images/", "/videos"],
+        debug: bool = False,
+        filterKeys=None,
+    ):
+        """
+        Constructor
+        Args:
+            frontend(FrontendSite): the frontend
+            parser(str): the beautiful soup parser to use e.g. html.parser
+            proxy_prefixes(list): the list of prefixes that need direct proxy access
+            debug: (bool): True if debugging should be on
+            filterKeys: (list): a list of keys for filters to be applied e.g. editsection
+        """
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.parser = parser
+        self.proxy_prefixes = proxy_prefixes
+        self.frontend = frontend
+        self.name = self.frontend.name
+        self.debug = debug
+        self.wiki = None
+        if filterKeys is None:
+            self.filterKeys = ["editsection", "parser-output", "parser-output"]
+        else:
+            self.filterKeys = []
+
+    def log(self, msg: str):
+        """
+        log the given message if debugging is true
+
+        Args:
+            msg (str): the message to log
+        """
+        if self.debug:
+            print(msg, flush=True)
+
+    @staticmethod
+    def extract_site_and_path(path: str):
+        """
+        Splits the given path into the site component and the remaining path.
+
+        This static method assumes that the 'site' is the first element of the
+        path when split by "/", and the 'path' is the rest of the string after
+        the site.
+
+        Parameters:
+        path (str): The complete path to split.
+
+        Returns:
+        tuple: A tuple where the first element is the site and the second
+               element is the subsequent path.
+        """
+        # Check if the path is empty or does not contain a "/"
+        if not path or "/" not in path:
+            return "", path
+
+        # Split the path into parts using the "/" as a separator
+        parts = path.split("/")
+
+        # The first part is the site, the rest is joined back into a path
+        site = parts[0]
+        remaining_path = "/" + "/".join(parts[1:])
+
+        return site, remaining_path
+
+    def open(self):
+        """
+        open the frontend
+
+        """
+        if self.wiki is None:
+            self.wiki = WikiClient.ofWikiId(self.frontend.wikiId)
+            if WikiFrontend.with_login:
+                self.wiki.login()
+            self.smwclient = SMWClient(self.wiki.getSite())
+            self.cms_pages = self.get_cms_pages()
+            self.frontend.enabled = True
+
+    def get_cms_pages(self) -> dict:
+        """
+        get the Content Management elements for this site
+        """
+        cms_pages = {}
+        ask_query = "[[Category:CMS]]"
+        page_records = self.smwclient.query(ask_query, "cms pages")
+        for page_title in list(page_records):
+            page_title, html, error = self.getContent(page_title)
+            if not error:
+                cms_pages[page_title] = html
+            else:
+                self.logger.warn(error)
+        return cms_pages
+
+    def errMsg(self, ex):
+        if self.debug:
+            msg = "%s\n%s" % (repr(ex), traceback.format_exc())
+        else:
+            msg = repr(ex)
+        return msg
+
+    def wikiPage(self, pagePath: str) -> str:
+        """
+        Get the wiki page for the given page path.
+
+        Args:
+            pagePath (str): The path of the page.
+
+        Returns:
+            str: The title of the page.
+        """
+        if "/index.php/" in pagePath:
+            wikipage = pagePath.replace("/index.php/", "")
+        elif pagePath.startswith("/"):
+            wikipage = pagePath[1:]
+        else:
+            wikipage = pagePath
+        return wikipage
+
+    def checkPath(self, pagePath: str) -> str:
+        """
+        check the given pathPath
+
+        Args:
+            pagePath (str): the page Path to check
+
+        Returns:
+            str: None or an error message with the illegal chars being used
+        """
+        error = None
+        self.log(pagePath)
+        illegalChars = ["{", "}", "<", ">", "[", "]", "|"]
+        for illegalChar in illegalChars:
+            if illegalChar in pagePath:
+                error = "invalid char %s in given pagePath " % (illegalChar)
+        return error
+
+    def needsProxy(self, path: str) -> bool:
+        """
+        Args:
+            path (str): the path to check
+
+        Returns:
+            bool: True if this path needs to be proxied
+        """
+        needs_proxy = False
+        for prefix in self.proxy_prefixes:
+            needs_proxy = needs_proxy or path.startswith(prefix)
+        return needs_proxy
+
+    def proxy(self, path: str) -> str:
+        """
+        Proxy a request.
+        See https://stackoverflow.com/a/50231825/1497139
+
+        Args:
+            path (str): the path to proxy
+
+        Returns:
+            the proxied result as a string
+        """
+        wikiUser = self.wiki.wikiUser
+        url = f"{wikiUser.url}{wikiUser.scriptPath}{path}"
+
+        # Get the response
+        response = requests.get(url)
+
+        return response
+
+    def filter(self, html: str) -> str:
+        """
+        filter the given html
+        """
+        return self.doFilter(html, self.filterKeys)
+
+    def fixNode(self, node, attribute, prefix, delim=None):
+        """
+        fix the given node
+
+        node (BeautifulSoup): the node
+        attribute (str): the name of the attribute e.g. "href", "src"
+        prefix (str): the prefix to replace e.g. "/", "/images", "/thumbs"
+        delim (str): if not None the delimiter for multiple values
+        """
+        siteprefix = f"/{self.frontend.name}{prefix}"
+        if attribute in node.attrs:
+            attrval = node.attrs[attribute]
+            if delim is not None:
+                vals = attrval.split(delim)
+            else:
+                vals = [attrval]
+                delim = ""
+            newvals = []
+            for val in vals:
+                if val.startswith(prefix):
+                    newvals.append(val.replace(prefix, siteprefix, 1))
+                else:
+                    newvals.append(val)
+            if delim is not None:
+                node.attrs[attribute] = delim.join(newvals)
+
+    def fix_images_and_videos(self, soup):
+        """
+        fix image and video entries in the source code
+        """
+        for img in soup.findAll("img"):
+            self.fixNode(img, "src", "/")
+            self.fixNode(img, "srcset", "/", ", ")
+        for video in soup.findAll("video"):
+            for source in video.findAll("source"):
+                self.fixNode(source, "src", "/")
+
+    def fixHtml(self, soup):
+        """
+        fix the HTML in the given soup
+
+        Args:
+            soup(BeautifulSoup): the html parser
+        """
+        self.fix_images_and_videos(soup)
+        # fix absolute hrefs
+        for a in soup.findAll("a"):
+            self.fixNode(a, "href", "/")
+        return soup
+
+    def unwrap(self, soup) -> str:
+        """
+        unwrap the soup
+        """
+        html = str(soup)
+        html = html.replace("<html><body>", "")
+        html = html.replace("</body></html>", "")
+        # Remove  empty paragraphs
+        html = re.sub(r'<p class="mw-empty-elt">\s*</p>', "", html)
+
+        # Replace multiple newline characters with a single newline character
+        html = re.sub(r"\n\s*\n", "\n", html)
+        return html
+
+    def doFilter(self, html, filterKeys):
+        # https://stackoverflow.com/questions/5598524/can-i-remove-script-tags-with-beautifulsoup
+        soup = BeautifulSoup(html, self.parser)
+        if "parser-output" in filterKeys:
+            parserdiv = soup.find("div", {"class": "mw-parser-output"})
+            if parserdiv:
+                soup = parserdiv
+                inner_html = parserdiv.decode_contents()
+                # Parse the inner HTML string to create a new BeautifulSoup object
+                soup = BeautifulSoup(inner_html, self.parser)
+                pass
+        # https://stackoverflow.com/questions/5041008/how-to-find-elements-by-class
+        if "editsection" in filterKeys:
+            for s in soup.select("span.mw-editsection"):
+                s.extract()
+        for comments in soup.findAll(text=lambda text: isinstance(text, Comment)):
+            comments.extract()
+        return soup
+
+    def getContent(self, pagePath: str):
+        """get the content for the given pagePath
+        Args:
+            pagePath(str): the pagePath
+            whatToFilter(list): list of filter keys
+        Returns:
+            str: the HTML content for the given path
+        """
+        content = None
+        error = None
+        pageTitle = "?"
+        try:
+            if pagePath == "/":
+                pageTitle = self.frontend.defaultPage
+            else:
+                error = self.checkPath(pagePath)
+                pageTitle = self.wikiPage(pagePath)
+            if error is None:
+                if self.wiki is None:
+                    raise Exception(
+                        "getContent without wiki - you might want to call open first"
+                    )
+                content = self.wiki.getHtml(pageTitle)
+                soup = self.filter(content)
+                soup = self.fixHtml(soup)
+                content = self.unwrap(soup)
+        except Exception as e:
+            error = self.errMsg(e)
+        return pageTitle, content, error
+
+    def wrapWithReveal(self, html: str):
+        """
+        wrap html content with reveal.js structure and dependencies
+        """
+        wrapped_html = f"""<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js@4.3.1/dist/reveal.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js@4.3.1/dist/theme/white.css">
+</head>
+<body>
+    <div class="reveal">
+        <div class="slides">
+{html}
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/reveal.js@4.3.1/dist/reveal.js"></script>
+    <script>Reveal.initialize({{
+    }});</script>
+</body>
+</html>"""
+        return wrapped_html
+
+    def toReveal(self, html: str):
+        """
+        convert the given html to reveal
+        see https://revealjs.com/
+        """
+        soup = BeautifulSoup(html, "lxml")
+        for h2 in soup.findChildren(recursive=True):
+            if h2.name == "h2":
+                span = h2.next_element
+                if span.name == "span":
+                    tagid = span.get("id")
+                    if tagid.startswith("⌘⌘"):
+                        section = soup.new_tag("section")
+                        h2.parent.append(section)
+                        section.insert(0, h2)
+                        tag = h2.next_element
+                        while tag is not None and tag.name != "h2":
+                            if tag.parent != h2:
+                                section.append(tag)
+                            tag = tag.next_element
+        html = self.unwrap(soup)
+        return html
+
+    def get_frame(self, page_title: str) -> str:
+        """
+        get the frame property for the given page_title
+        """
+        frame = None
+        markup = self.wiki.get_wiki_markup(page_title)
+        # {{#set:frame=reveal}}
+        # {{UseFrame|Contact.rythm|
+        patterns = [
+            r"{{#set:frame=([^}]+)}}",  # {{#set:frame=reveal}}
+            r"{{UseFrame\|([^.]+)",  # {{UseFrame|Contact.rythm|
+        ]
+
+        for pattern in patterns:
+            match = re.search(pattern, markup)
+            if match:
+                frame = match.group(1)
+        return frame
+
+    def get_path_response(self, path: str) -> str:
+        """
+        get the repsonse for the the given path
+
+        Args:
+            path(str): the path to render the content for
+
+        Returns:
+            Response: a FastAPI response
+        """
+        if self.needsProxy(path):
+            html_response = self.proxy(path)
+            # Create a FastAPI response object
+            response = Response(
+                content=html_response.content,
+                status_code=html_response.status_code,
+                headers=dict(html_response.headers),
+            )
+        else:
+            page_title, content, error = self.getContent(path)
+            html_frame = HtmlFrame(self, title=page_title)
+            html = content
+            framed_html = None
+            if error:
+                html = f"error getting {page_title} for {self.name}:<br>{error}"
+            else:
+                if "<slideshow" in html or "&lt;slideshow" in html:
+                    content = self.toReveal(content)
+                    # Complete reveal.js webpage
+                    framed_html = self.wrapWithReveal(html)
+                    html = content
+
+            if not framed_html:
+                framed_html = html_frame.frame(html)
+            response = HTMLResponse(framed_html)
+        return response
+
+
+class WikiFrontends:
+    """
+    wiki frontends
+    """
+
+    def __init__(self, servers):
+        """
+        constructor
+        """
+        self.servers = servers
+        self.wiki_frontends = {}
+
+    def enableSites(self, siteNames):
+        """
+        enable the sites given in the sites list
+        Args:
+            siteNames(list): a list of strings with wikiIds to be enabled
+        """
+        if siteNames is None:
+            return
+        for siteName in siteNames:
+            self.get_frontend(siteName)
+
+    def get_frontend(self, name: str) -> WikiFrontend:
+        """
+        Get WikiFrontend from cache or create new one
+        """
+        # Check cache first
+        if name in self.wiki_frontends:
+            cached_frontend = self.wiki_frontends[name]
+            return cached_frontend
+
+        # Create new frontend if not cached
+        frontend = self.servers.frontends_by_name.get(name)
+        if frontend:
+            wiki_frontend = WikiFrontend(frontend)
+            wiki_frontend.open()
+            # Cache it
+            self.wiki_frontends[name] = wiki_frontend
+            return wiki_frontend
---
+++ packages/pyWikiCMS/pywikicms-0.4.6-py3-none-any.whl/frontend/wikigrid.py
@@ -0,0 +1,214 @@
+"""
+Created on 2022-12-03
+
+@author: wf
+"""
+
+import glob
+import os
+import time
+from pathlib import Path
+
+from ngwidgets.lod_grid import GridConfig, ListOfDictsGrid
+from ngwidgets.progress import NiceguiProgressbar
+from ngwidgets.task_runner import TaskRunner
+from nicegui import ui
+
+from backend.site import Wikis, WikiSite
+
+
+class WikiCheck:
+    """
+    Check to be performed on  a Mediawiki.
+    """
+
+    def __init__(self, name, func, checked=True):
+        self.name = name
+        self.func = func  # the check function to be performed on a WikiState
+        self.checked = checked
+        self.checkbox = None
+
+    def as_checkbox(self):
+        """
+        Return a checkbox representation of the instance.
+        """
+        self.checkbox = ui.checkbox(self.name).bind_value(self, "checked")
+        return self.checkbox
+
+
+class WikiGrid:
+    """
+    A grid of Wikis.
+    """
+
+    def __init__(self, solution, wikis: Wikis):
+        # back reference to nicegui solution
+        self.solution = solution
+        self.wikis = wikis
+        self.task_runner = TaskRunner(timeout=40)
+
+    def setup(self):
+        """
+        setup the ui
+        """
+        self.add_checkboxes()
+        self.progressbar = NiceguiProgressbar(
+            self.wikis.get_wiki_count(), "work on wikis", "steps"
+        )
+        self.task_runner.progress = self.progressbar
+        self.as_grid()
+        self.lod_grid.update()
+
+    def as_grid(self):
+        # Configure grid with checkbox selection
+        grid_config = GridConfig(
+            key_col="#",
+            editable=False,
+            multiselect=True,
+            with_buttons=True,
+            button_names=["all", "fit"],
+            debug=False,
+        )
+        self.lod_grid = ListOfDictsGrid(lod=self.wikis.get_lod(), config=grid_config)
+        self.lod_grid.ag_grid._props["html_columns"] = [0, 1, 2]
+        return self.lod_grid
+
+    def add_checkboxes(self):
+        """
+        Add check boxes.
+        """
+        self.button_row = ui.row()
+        with self.button_row:
+            self.wiki_checks = [
+                WikiCheck("version", self.check_wiki_version),
+                WikiCheck("backup", self.check_backup),
+                WikiCheck("pages", self.check_pages),
+            ]
+            for wiki_check in self.wiki_checks:
+                wiki_check.as_checkbox()
+            ui.button(text="Checks", on_click=self.perform_wiki_checks)
+
+    async def get_selected_lod(self):
+        lod_index = self.lod_grid.get_index(
+            lenient=self.lod_grid.config.lenient, lod=self.wikis.get_lod()
+        )
+        lod = await self.lod_grid.get_selected_lod(lod_index=lod_index)
+        if len(lod) == 0:
+            with self.button_row:
+                ui.notify("Please select at least one row")
+        return lod
+
+    async def perform_wiki_checks(self, _msg):
+        """
+        react on the button for check having been clicked
+        """
+        self.select_lod = await self.get_selected_lod()
+        if self.select_lod:
+            with self.solution.content_div:
+                total = len(self.select_lod)
+                ui.notify(f"Checking {total} wikis ...")
+                # Use single task_runner
+                self.task_runner.run_blocking(self.run_all_wiki_checks)
+
+    def run_all_wiki_checks(self):
+        """
+        Process all selected wikis sequentially
+        """
+        try:
+            # Calculate total steps
+            steps = 0
+            for wiki_check in self.wiki_checks:
+                if wiki_check.checked:
+                    steps += len(self.select_lod)
+            self.progressbar.total = steps
+            self.progressbar.reset()
+
+            # Process each wiki sequentially
+            for row in self.select_lod:
+                row_no = row["#"]
+                wikisite = self.wikis.get_wikisite_by_row(row_no)
+                self.run_wiki_check(wikisite)
+
+        except BaseException as ex:
+            self.solution.handle_exception(ex)
+
+    def run_wiki_check(self, wiki_state):
+        """
+        perform the selected wiki checks for a single wiki
+        """
+        try:
+            for wiki_check in self.wiki_checks:
+                if wiki_check.checked:
+                    wiki_check.func(wiki_state)
+                with self.solution.content_div:
+                    self.lod_grid.update()
+                    # Update the progress bar
+                    self.progressbar.update(1)
+        except BaseException as ex:
+            self.solution.handle_exception(ex)
+
+    def check_pages(self, wiki_state: WikiSite):
+        """
+        Try login for wiki user and report success or failure.
+        """
+        try:
+            try:
+                client = wiki_state.wiki_client
+                stats = client.get_site_statistics()
+                pages = stats["pages"]
+                self.lod_grid.update_cell(wiki_state.row_no, "login", f"✅")
+                self.lod_grid.update_cell(wiki_state.row_no, "pages", f"✅{pages}")
+            except Exception as ex:
+                self.lod_grid.update_cell(wiki_state.row_no, "login", f"❌ {str(ex)}")
+                self.lod_grid.update_cell(wiki_state.row_no, "pages", "❌")
+                return
+        except BaseException as ex:
+            self.solution.handle_exception(ex)
+
+    def check_wiki_version(self, wiki_state: WikiSite):
+        """
+        Check the MediaWiki version for a specific WikiState.
+        """
+        try:
+            mw_version = wiki_state.check_version()
+            if not mw_version.startswith("MediaWiki"):
+                mw_version = f"MediaWiki {mw_version}"
+            row = self.lod_grid.get_row_for_key(wiki_state.row_no)
+            if row:
+                ex_version = wiki_state.wiki_user.version
+                if ex_version == mw_version:
+                    self.lod_grid.update_cell(
+                        wiki_state.row_no, "version", f"{mw_version}✅"
+                    )
+                else:
+                    self.lod_grid.update_cell(
+                        wiki_state.row_no, "version", f"{ex_version}!={mw_version}❌"
+                    )
+        except BaseException as ex:
+            self.solution.handle_exception(ex)
+
+    def check_backup(self, wiki_state):
+        """
+        Check the backup status for a specific WikiUser.
+        """
+        try:
+            row = self.lod_grid.get_row_for_key(wiki_state.row_no)
+            if row:
+                backup_path = f"{Path.home()}/wikibackup/{wiki_state.wiki_user.wikiId}"
+                if os.path.isdir(backup_path):
+                    wiki_files = glob.glob(f"{backup_path}/*.wiki")
+                    msg = f"{len(wiki_files):6} ✅"
+                    self.lod_grid.update_cell(wiki_state.row_no, "backup", msg)
+                    # https://stackoverflow.com/a/39327156/1497139
+                    if wiki_files:
+                        latest_file = max(wiki_files, key=os.path.getctime)
+                        st = os.stat(latest_file)
+                        age_days = round((time.time() - st.st_mtime) / 86400)
+                        self.lod_grid.update_cell(
+                            wiki_state.row_no, "age", f"{age_days}"
+                        )
+                else:
+                    msg = "❌"
+                    self.lod_grid.update_cell(wiki_state.row_no, "backup", msg)
+        except BaseException as ex:
+            self.solution.handle_exception(ex)
