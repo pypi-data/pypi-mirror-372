f95293f3111610b9aae1598e528deb4e8fa748d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:23 +0000

    Add package bizon/0.1.2/bizon-0.1.2-py3-none-any.whl

---
+++ packages/bizon/bizon-0.1.2-py3-none-any.whl/bizon/source/auth/authenticators/cookies.py
@@ -0,0 +1,24 @@
+from typing import Any, Mapping, Optional
+
+from pydantic import BaseModel, Field
+from requests import PreparedRequest
+from requests.auth import AuthBase
+
+
+class CookiesAuthParams(BaseModel):
+    cookies: Mapping[str, str] = Field(..., description="Cookies to be attached to the request")
+    headers: Optional[Mapping[str, Any]] = Field({}, description="Headers to be attached to the request")
+
+
+class CookiesAuthenticator(AuthBase):
+    def __init__(self, params: CookiesAuthParams):
+        super().__init__()
+        self.cookies = params.cookies
+        self.headers = params.headers
+
+    def __call__(self, request: PreparedRequest) -> PreparedRequest:
+        request.prepare_cookies(self.cookies)
+
+        if self.headers:
+            request.headers.update(self.headers)
+        return request
