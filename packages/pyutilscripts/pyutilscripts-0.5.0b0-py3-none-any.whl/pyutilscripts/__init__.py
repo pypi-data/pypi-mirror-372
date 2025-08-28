d356b2487e2bf49c0bc883064529ce7e48352386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 14:06:41 +0000

    Add package pyutilscripts/0.5.0b0/pyutilscripts-0.5.0b0-py3-none-any.whl

---
+++ packages/pyutilscripts/pyutilscripts-0.5.0b0-py3-none-any.whl/pyutilscripts/__init__.py
@@ -0,0 +1,12 @@
+"""
+PyUtilScripts 是一个基于 Python 的通用小工具集合，目标是提供编写通用任务的辅助工具。
+"""
+
+__version__ = "0.5.0"
+__status__  = "beta0"
+__author__  = "zero <zero.kwok@foxmail.com>"
+
+projectName    = 'PyUtilScripts'
+projectHome    = 'https://github.com/ZeroKwok/pyutilscripts.git'
+projectVersion = f'{__version__} {__status__}'.strip()
+projectAuthor  = __author__
