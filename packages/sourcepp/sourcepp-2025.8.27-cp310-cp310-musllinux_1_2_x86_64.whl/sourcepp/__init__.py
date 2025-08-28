1e761e519c91ea18fbfc655f947d6f7c451893da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:20 +0000

    Add package sourcepp/2025.8.27/sourcepp-2025.8.27-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/sourcepp/sourcepp-2025.8.27-cp310-cp310-musllinux_1_2_x86_64.whl/sourcepp/__init__.py
@@ -0,0 +1,5 @@
+from ._sourcepp_impl import __doc__, bsppp, gamepp, sourcepp, steampp, toolpp, vcryptpp, vtfpp
+
+__author__ = "craftablescience"
+__version__ = "2025.8.27"
+__all__ = ['__author__', '__doc__', '__version__', 'bsppp', 'gamepp', 'sourcepp', 'steampp', 'toolpp', 'vcryptpp', 'vtfpp']
