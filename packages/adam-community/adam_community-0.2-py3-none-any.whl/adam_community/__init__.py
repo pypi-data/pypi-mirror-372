435a5584d5224a74cc1c0562e607b5aa12307ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 17:06:39 +0000

    Add package adam-community/0.2/adam_community-0.2-py3-none-any.whl

---
+++ packages/adam-community/adam_community-0.2-py3-none-any.whl/adam_community/__init__.py
@@ -0,0 +1,4 @@
+from .util import messageSend, knowledgeSearch, completionCreate, runCmd
+from .tool import Tool
+
+__all__ = ['Tool', 'messageSend', 'knowledgeSearch', 'completionCreate', 'runCmd']
