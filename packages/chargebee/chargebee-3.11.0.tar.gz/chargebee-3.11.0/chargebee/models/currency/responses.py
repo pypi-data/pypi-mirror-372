a604b07cf7686c60d0577e3bf8f4045ce0b04f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:06:19 +0000

    Add package chargebee/3.11.0/chargebee-3.11.0.tar.gz

---
+++ packages/chargebee/chargebee-3.11.0.tar.gz/chargebee-3.11.0/chargebee/models/currency/responses.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from chargebee.model import Model
+from typing import Dict, List, Any
+from chargebee.response import Response
+
+
+@dataclass
+class CurrencyResponse(Model):
+    raw_data: Dict[Any, Any] = None
+    id: str = None
+    enabled: bool = None
+    forex_type: str = None
+    currency_code: str = None
+    is_base_currency: bool = None
+    manual_exchange_rate: str = None
+
+
+@dataclass
+class ListResponse(Response):
+    currency: CurrencyResponse
+
+
+@dataclass
+class RetrieveResponse(Response):
+    currency: CurrencyResponse
+
+
+@dataclass
+class CreateResponse(Response):
+    is_idempotency_replayed: bool
+    currency: CurrencyResponse
+
+
+@dataclass
+class UpdateResponse(Response):
+    is_idempotency_replayed: bool
+    currency: CurrencyResponse
+
+
+@dataclass
+class AddScheduleResponse(Response):
+    is_idempotency_replayed: bool
+    scheduled_at: int
+    currency: CurrencyResponse
+
+
+@dataclass
+class RemoveScheduleResponse(Response):
+    is_idempotency_replayed: bool
+    scheduled_at: int
+    currency: CurrencyResponse
