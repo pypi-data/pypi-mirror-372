e5527ea64dab1c5983a45442b6da4b11083e3c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:06:37 +0000

    Add package upplib/1.3.2/upplib-1.3.2.tar.gz

---
+++ packages/upplib/upplib-1.3.2.tar.gz/upplib-1.3.2/upplib/util.py
@@ -0,0 +1,42 @@
+from upplib import *
+
+
+def format_milliseconds(time_str):
+    # 匹配小数点后的数字（至少1位），并捕获时区部分
+    # param：2025-08-14T12:53:00.05382312323+07:00
+    # return：2025-08-14T12:53:00.0538+07:00
+    return re.sub(r'\.(\d+)([+-].*)?', lambda m: f".{m.group(1)[:4]}{m.group(2) or ''}", time_str)
+
+
+def get_log_msg(contents: dict) -> str:
+    """
+    获得日志
+    """
+    rs = ''
+    # time
+    _time_ = None
+    if '_time_' in contents:
+        _time_ = format_milliseconds(contents['_time_'])
+    if _time_ is None and 'time' in contents:
+        _time_ = format_milliseconds(contents['time'])
+
+    # level
+    level = None
+    if 'level' in contents:
+        level = contents['level']
+
+    # content
+    content = None
+    if 'content' in contents:
+        content = contents['content']
+    if content is None and 'message' in contents:
+        content = contents['message']
+    if content is not None and len(str(content).split(' ')) >= 2:
+        time_str = ' '.join(str(content).split(' ')[0:2])
+        time_1 = to_datetime(time_str, error_is_none=True)
+        if time_1 is None:
+            _time_ = '   '
+        else:
+            content = content[len(time_str):]
+
+    return ' '.join(filter(lambda s: s is not None, [_time_, level, content]))
