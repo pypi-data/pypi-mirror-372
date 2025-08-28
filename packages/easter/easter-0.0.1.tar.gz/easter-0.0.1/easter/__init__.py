b965d9f5fa58c7faa2f72b4eeeb56672e9a270f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:46 +0000

    Add package easter/0.0.1/easter-0.0.1.tar.gz

---
+++ packages/easter/easter-0.0.1.tar.gz/easter-0.0.1/easter/__init__.py
@@ -0,0 +1,5 @@
+from easter.message import Message
+from easter.mailbox import Mailbox
+from easter.rabbitmq import RabbitMQConsumer
+from easter.rabbitmq import RabbitMQDispatcher
+from easter.rabbitmq import RabbitMQConnectionFactory
