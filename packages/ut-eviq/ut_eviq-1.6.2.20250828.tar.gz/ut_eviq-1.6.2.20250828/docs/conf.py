98534d68055cbf68ccd9607469ca0e0b925fbe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 17:06:44 +0000

    Add package ut-eviq/1.6.2.20250828/ut_eviq-1.6.2.20250828.tar.gz

---
+++ packages/ut-eviq/ut_eviq-1.6.2.20250828.tar.gz/ut_eviq-1.6.2.20250828/docs/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+import os
+from datetime import date
+
+sys.path.insert(0, os_path.abspath("../"))
+from ut_eviq import __version__
+
+project = 'ut_eviq'
+copyright = '2025, Bernd Stroehle'
+author = 'Bernd Stroehle'
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.copybutton",
+    "sphinx.autodoc_typehints",
+    "sphinx.myst_parser",
+]
+
+intersphinx_mapping = {
+    "py": ("https://docs.python.org/3", None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'furo'
+html_static_path = ['_static']
