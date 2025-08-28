b002947d093ce0dd50cf757491889bb518862b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:06:07 +0000

    Add package easy-tornado/0.7.4/easy_tornado-0.7.4.tar.gz

---
+++ packages/easy-tornado/easy_tornado-0.7.4.tar.gz/easy_tornado-0.7.4/easy_tornado/utility/exception.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# author: 王树根
+# email: wangshugen@ict.ac.cn
+# date: 2018/11/19 11:03
+from .printext import it_print
+from ..compat import C_StandardError
+
+
+class InternalError(C_StandardError):
+
+  def __init__(self, *args, **kwargs):
+    super(InternalError, self).__init__(*args, **kwargs)
+
+
+def raise_print(*args, **kwargs):
+  """
+  print message and raise InternalError
+  :param args: see it_print
+  :param kwargs: it_print
+  """
+  _do_print(*args, **kwargs)
+  message = kwargs.pop('message', 'Unknown')
+  raise InternalError(message)
+
+
+def exit_print(*args, **kwargs):
+  """
+  print message and exit
+  :param args: see it_print
+  :param kwargs: see it_print
+  """
+  _do_print(*args, **kwargs)
+  errno = kwargs.pop('errno', 0)
+  exit(int(errno))
+
+
+def _do_print(*args, **kwargs):
+  if 'device' not in kwargs:
+    kwargs['device'] = 2
+
+  print_fn = kwargs.pop('print_fn', it_print)
+  print_fn(*args, **kwargs)
