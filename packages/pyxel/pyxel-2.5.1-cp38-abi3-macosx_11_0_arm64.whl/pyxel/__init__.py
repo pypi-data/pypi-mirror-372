5a73a4856955f77ff3ca5ea2d62b6318fc7a0221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 14:06:41 +0000

    Add package pyxel/2.5.1/pyxel-2.5.1-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/pyxel/pyxel-2.5.1-cp38-abi3-macosx_11_0_arm64.whl/pyxel/__init__.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from .pyxel_wrapper import *  # type: ignore  # noqa: F403
+
+_reset_info = {
+    "exec": sys.executable,
+    "cwd": os.getcwd(),
+    "argv": getattr(sys, "orig_argv", sys.argv[:]),
+}
+
+
+def _reset():
+    if WATCH_STATE_FILE_ENV in os.environ:  # type: ignore  # noqa: F405
+        os._exit(WATCH_RESET_EXIT_CODE)  # type: ignore  # noqa: F405
+
+    if sys.platform == "darwin":
+        try:
+            with open(os.devnull, "wb") as f:
+                os.dup2(f.fileno(), 2)
+        except OSError:
+            pass
+
+    os.environ[RESET_STATE_ENV] = _window_state()  # type: ignore  #noqa: F405
+
+    try:
+        os.chdir(_reset_info["cwd"])
+    except Exception:
+        pass
+
+    os.execvpe(
+        _reset_info["exec"],
+        [_reset_info["exec"]] + _reset_info["argv"][1:],
+        os.environ.copy(),
+    )
+
+
+_reset_func(_reset)  # type: ignore  #noqa: F405
