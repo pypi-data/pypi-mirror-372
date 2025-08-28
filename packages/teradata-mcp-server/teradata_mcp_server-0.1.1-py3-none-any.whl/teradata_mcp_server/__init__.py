1a6fcc02b608184ae5c8d7bde139eaead6bf0b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 17:06:28 +0000

    Add package teradata-mcp-server/0.1.1/teradata_mcp_server-0.1.1-py3-none-any.whl

---
+++ packages/teradata-mcp-server/teradata_mcp_server-0.1.1-py3-none-any.whl/teradata_mcp_server/__init__.py
@@ -0,0 +1,24 @@
+"""
+teradata_mcp_server
+===================
+
+Lightweight MCP server tools for Teradata.
+"""
+
+__version__ = "0.1.0"
+
+import asyncio
+
+from . import server
+
+
+def main():
+    """Main entry point for the package."""
+    asyncio.run(server.main())
+
+
+# Specify what’s available at package level
+__all__ = [
+    "main",
+    "server",
+]
