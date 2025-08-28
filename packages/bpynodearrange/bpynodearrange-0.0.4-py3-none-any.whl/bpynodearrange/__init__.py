5cf96a882c5716381504c07d4a48b0efe32ef225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 04:05:20 +0000

    Add package bpynodearrange/0.0.4/bpynodearrange-0.0.4-py3-none-any.whl

---
+++ packages/bpynodearrange/bpynodearrange-0.0.4-py3-none-any.whl/bpynodearrange/__init__.py
@@ -0,0 +1,17 @@
+"""Node Arrange - Automatic layout of nodes for Blender node trees."""
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from .arrange import graph, ordering, ranking, structs, sugiyama
+
+__all__ = [
+    "graph",
+    "ordering",
+    "ranking",
+    "structs",
+    "sugiyama",
+]
+
+__version__ = "0.0.2"
+__author__ = "Brady Johnston"
+__email__ = "brady.johnston@me.com"
