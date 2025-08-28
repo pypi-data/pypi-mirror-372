7c9e91ed1e326ef9262058510366f99705ba5101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 05:05:34 +0000

    Add package DepthTensor/1.10.0/depthtensor-1.10.0.tar.gz

---
+++ packages/DepthTensor/depthtensor-1.10.0.tar.gz/depthtensor-1.10.0/src/DepthTensor/__init__.py
@@ -0,0 +1,7 @@
+from .tensor import Tensor
+from .typing import *
+from ._core import random, create_2in_1out, create_1in_1out
+from ._core.exceptions import *
+from .autodiff import differentiate
+
+__version__ = "1.10.0"
