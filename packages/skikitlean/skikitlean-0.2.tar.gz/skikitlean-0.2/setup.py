32cedaeb480ad7e45aec82a1a640305b179c21bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:05:36 +0000

    Add package skikitlean/0.2/skikitlean-0.2.tar.gz

---
+++ packages/skikitlean/skikitlean-0.2.tar.gz/skikitlean-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='skikitlean',
+      version='0.2',
+      description='skikitlean',
+      packages=['skikitlean'],
+      author_email='lolikuzi98@gmail.com',
+      zip_safe=False)
