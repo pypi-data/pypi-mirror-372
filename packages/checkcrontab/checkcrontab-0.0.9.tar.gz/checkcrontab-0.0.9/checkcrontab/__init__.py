a4d99ccdafad351f69526111c9402cf8d6289bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 03:19:10 +0000

    Add package checkcrontab/0.0.9/checkcrontab-0.0.9.tar.gz

---
+++ packages/checkcrontab/checkcrontab-0.0.9.tar.gz/checkcrontab-0.0.9/checkcrontab/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Checkcrontab package
+"""
+
+__version__ = "0.0.9"
+__description__ = "A Python script for checking syntax of crontab files"
+__author__ = "Aleksandr Pimenov"
+__email__ = "wachawo@gmail.com"
+__url__ = "https://github.com/wachawo/checkcrontab"
+
+# Import main functions
+from . import checker, logging_config, main
+
+__all__ = [
+    "main",
+    "checker",
+    "logging_config",
+    "__version__",
+    "__description__",
+    "__author__",
+    "__email__",
+]
