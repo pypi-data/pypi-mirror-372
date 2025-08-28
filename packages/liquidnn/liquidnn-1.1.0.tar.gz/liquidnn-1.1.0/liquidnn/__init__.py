8efbf992061c1a22adc6198bb900faf11d5c77aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:37 +0000

    Add package liquidnn/1.1.0/liquidnn-1.1.0.tar.gz

---
+++ packages/liquidnn/liquidnn-1.1.0.tar.gz/liquidnn-1.1.0/liquidnn/__init__.py
@@ -0,0 +1,19 @@
+from .liquidnetwork import (
+    LiquidNeuralNetwork,
+    LTCLayer,
+    LiquidCNN,
+    LiquidRNN,
+    LiquidLSTM
+)
+from .liquidneuron import LiquidNeuron
+
+__all__ = [
+    "LiquidNeuralNetwork",
+    "LTCLayer",
+    "LiquidCNN",
+    "LiquidRNN",
+    "LiquidLSTM",
+    "LiquidNeuron",
+]
+
+__version__ = "1.1.0"
