0255c2805cbb0e3adbc8f0000382e9124c277a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:05:59 +0000

    Add package chunklet-py/1.4.0/chunklet_py-1.4.0-py3-none-any.whl

---
+++ packages/chunklet-py/chunklet_py-1.4.0-py3-none-any.whl/chunklet/__init__.py
@@ -0,0 +1,24 @@
+"""
+Chunklet: Multi_strategy, Context-aware, Multilingual Text Chunker
+
+This package provides a robust and flexible solution for splitting large texts
+into smaller, manageable chunks. It is designed with applications like Large
+Language Models (LLMs), Retrieval-Augmented Generation (RAG) pipelines, and
+other context-aware Natural Language Processing (NLP) tasks in mind.
+
+The core functionality is provided by the `Chunklet` class, which can be
+imported directly from the package alongside execptions and schemas models.
+"""
+
+from .core import Chunklet
+from .exceptions import ChunkletError, InvalidInputError, TokenNotProvidedError
+from .models import CustomSplitterConfig, ChunkletInitConfig, ChunkingConfig
+
+__version__ = "1.4.0"
+
+__all__ = [
+    "Chunklet",
+    "ChunkletError",
+    "InvalidInputError",
+    "TokenNotProvidedError",
+]
