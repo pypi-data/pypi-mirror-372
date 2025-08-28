3889df5bd5d0dc21d9d3b2fbb1d7bc9c85952991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 16:06:20 +0000

    Add package maleo-soma-types-base/0.0.1/maleo_soma_types_base-0.0.1-py3-none-any.whl

---
+++ packages/maleo-soma-types-base/maleo_soma_types_base-0.0.1-py3-none-any.whl/maleo/soma/types/base/dict.py
@@ -0,0 +1,51 @@
+from typing import Any, Dict, List, Optional, Sequence
+from .string import ListOfStrings, SequenceOfStrings
+
+
+# String key
+# Any value
+StringToAnyDict = Dict[str, Any]
+OptionalStringToAnyDict = Optional[StringToAnyDict]
+ListOfStringToAnyDict = List[StringToAnyDict]
+OptionalListOfStringToAnyDict = Optional[ListOfStringToAnyDict]
+SequenceOfStringToAnyDict = Sequence[StringToAnyDict]
+OptionalSequenceOfStringToAnyDict = Optional[SequenceOfStringToAnyDict]
+
+# Object value
+StringToObjectDict = Dict[str, object]
+OptionalStringToObjectDict = Optional[StringToObjectDict]
+ListOfStringToObjectDict = List[StringToObjectDict]
+OptionalListOfStringToObjectDict = Optional[ListOfStringToObjectDict]
+SequenceOfStringToObjectDict = Sequence[StringToObjectDict]
+OptionalSequenceOfStringToObjectDict = Optional[SequenceOfStringToObjectDict]
+
+# String value
+StringToStringDict = Dict[str, str]
+OptionalStringToStringDict = Optional[StringToStringDict]
+ListOfStringToStringDict = List[StringToStringDict]
+OptionalListOfStringToStringDict = Optional[ListOfStringToStringDict]
+SequenceOfStringToStringDict = Sequence[StringToStringDict]
+OptionalSequenceOfStringToStringDict = Optional[SequenceOfStringToStringDict]
+
+# Multi-String value
+StringToListOfStringsDict = Dict[str, ListOfStrings]
+OptionalStringToListOfStringsDict = Optional[StringToListOfStringsDict]
+StringToSequenceOfStringsDict = Dict[str, SequenceOfStrings]
+OptionalStringToSequenceOfStringsDict = Optional[StringToSequenceOfStringsDict]
+
+# Integer key
+# Any value
+IntToAnyDict = Dict[int, Any]
+OptionalIntToAnyDict = Optional[IntToAnyDict]
+ListOfIntToAnyDict = List[IntToAnyDict]
+OptionalListOfIntToAnyDict = Optional[ListOfIntToAnyDict]
+SequenceOfIntToAnyDict = Sequence[IntToAnyDict]
+OptionalSequenceOfIntToAnyDict = Optional[SequenceOfIntToAnyDict]
+
+# String value
+IntToStringDict = Dict[int, str]
+OptionalIntToStringDict = Optional[IntToStringDict]
+ListOfIntToStringDict = List[IntToStringDict]
+OptionalListOfIntToStringDict = Optional[ListOfIntToStringDict]
+SequenceOfIntToStringDict = Sequence[IntToStringDict]
+OptionalSequenceOfIntToStringDict = Optional[SequenceOfIntToStringDict]
