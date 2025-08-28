ed8546e2d7866e68e8ed2ceac468532ac6f422ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 16:06:43 +0000

    Add package spoofy-archiver/1.0.6rc2/spoofy_archiver-1.0.6rc2.tar.gz

---
+++ packages/spoofy-archiver/spoofy_archiver-1.0.6rc2.tar.gz/spoofy_archiver-1.0.6rc2/spoofy_archiver/services/metadata/__init__.py
@@ -0,0 +1,23 @@
+"""Metadata Objects for Spoofy Archiver."""
+
+from .models import (
+    AlbumListResult,
+    MetadataAlbum,
+    MetadataAlbumSummary,
+    MetadataArtist,
+    MetadataPlaylist,
+    MetadataTrack,
+    MetadataTrackList,
+    MetadataTrackSummary,
+)
+
+__all__ = [
+    "AlbumListResult",
+    "MetadataAlbum",
+    "MetadataAlbumSummary",
+    "MetadataArtist",
+    "MetadataPlaylist",
+    "MetadataTrack",
+    "MetadataTrackList",
+    "MetadataTrackSummary",
+]
