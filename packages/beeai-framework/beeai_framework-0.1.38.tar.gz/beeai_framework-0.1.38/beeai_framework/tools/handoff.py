ed6a4afb74fd133afb42f8c0360ac70380a094d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 05:05:14 +0000

    Add package beeai-framework/0.1.38/beeai_framework-0.1.38.tar.gz

---
+++ packages/beeai-framework/beeai_framework-0.1.38.tar.gz/beeai_framework-0.1.38/beeai_framework/tools/handoff.py
@@ -0,0 +1,61 @@
+# Copyright 2025 © BeeAI a Series of LF Projects, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+from functools import cached_property
+
+from pydantic import BaseModel, Field
+
+from beeai_framework.agents import AnyAgent
+from beeai_framework.backend import AssistantMessage
+from beeai_framework.context import RunContext
+from beeai_framework.emitter import Emitter
+from beeai_framework.memory import BaseMemory
+from beeai_framework.tools import StringToolOutput, Tool, ToolError, ToolRunOptions
+
+
+class HandoffSchema(BaseModel):
+    task: str = Field(description="Clearly defined task for the agent to work on based on his abilities.")
+
+
+class HandoffTool(Tool[HandoffSchema, ToolRunOptions, StringToolOutput]):
+    """Delegates a task to an expert agent"""
+
+    def __init__(self, target: AnyAgent, *, name: str | None = None, description: str | None = None) -> None:
+        super().__init__()
+        self._target = target
+        self._name = name or target.meta.name
+        self._description = description or target.meta.description
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @cached_property
+    def input_schema(self) -> type[HandoffSchema]:
+        return HandoffSchema
+
+    async def _run(self, input: HandoffSchema, options: ToolRunOptions | None, context: RunContext) -> StringToolOutput:
+        memory: BaseMemory = context.context["state"]["memory"]
+        if not memory or not isinstance(memory, BaseMemory):
+            raise ToolError("No memory found in context.")
+
+        target: AnyAgent = await self._target.clone()  # type: ignore
+        target.memory.reset()
+
+        last_message = memory.messages[-1] if memory.messages else None
+        if last_message and isinstance(last_message, AssistantMessage) and last_message.get_tool_calls():
+            await target.memory.add_many(memory.messages[:-1])
+        else:
+            await target.memory.add_many(memory.messages)
+        response = await target.run(prompt=input.task)
+        return StringToolOutput(response.result.text)
+
+    def _create_emitter(self) -> Emitter:
+        return Emitter.root().child(
+            namespace=["tool", "handoff"],
+            creator=self,
+        )
