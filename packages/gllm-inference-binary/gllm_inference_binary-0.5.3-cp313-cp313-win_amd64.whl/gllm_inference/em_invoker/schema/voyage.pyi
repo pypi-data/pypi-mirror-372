45fa1f5aa9f2bec74cfee051e09b34282959b75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:07:01 +0000

    Add package gllm-inference-binary/0.5.3/gllm_inference_binary-0.5.3-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.3-cp313-cp313-win_amd64.whl/gllm_inference/em_invoker/schema/voyage.pyi
@@ -0,0 +1,11 @@
+class Key:
+    """Defines valid keys in Voyage."""
+    CONTENT: str
+    IMAGE_BASE64: str
+    TEXT: str
+    TYPE: str
+
+class InputType:
+    """Defines valid input types in Voyage."""
+    IMAGE_BASE64: str
+    TEXT: str
