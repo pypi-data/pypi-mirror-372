aac893ed2cd24ff188de5afe36d515774029d1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:06:11 +0000

    Add package gllm-inference-binary/0.5.2/gllm_inference_binary-0.5.2-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.2-cp312-cp312-win_amd64.whl/gllm_inference/em_invoker/schema/twelvelabs.pyi
@@ -0,0 +1,15 @@
+class Key:
+    """Defines valid keys in TwelveLabs."""
+    INPUT_KEY: str
+    OUTPUT_KEY: str
+    VALUE: str
+
+class InputType:
+    """Defines valid input types in TwelveLabs."""
+    FILE_SUFFIX: str
+    TEXT: str
+
+class OutputType:
+    """Defines valid output types in TwelveLabs."""
+    EMBEDDING_SUFFIX: str
+    TEXT_EMBEDDING: str
