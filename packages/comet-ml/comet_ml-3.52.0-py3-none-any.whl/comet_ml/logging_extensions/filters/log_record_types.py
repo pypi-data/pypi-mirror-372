0b9749b1f9bacfaf4c994daa55c1c96ac95470d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 17:07:06 +0000

    Add package comet-ml/3.52.0/comet_ml-3.52.0-py3-none-any.whl

---
+++ packages/comet-ml/comet_ml-3.52.0-py3-none-any.whl/comet_ml/logging_extensions/filters/log_record_types.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at https://www.comet.com
+#  Copyright (C) 2015-2024 Comet ML INC
+#  This source code is licensed under the MIT license.
+# *******************************************************
+from .log_filter_helpers import extra_for_type
+
+METRICS_THROTTLING_MSG_EXTRA = extra_for_type("metrics-throttling-msg")
+ASSETS_UPLOAD_THROTTLING_MSG_EXTRA = extra_for_type("assets-upload-throttling-msg")
+
+# Other throttling messages fall into this category
+GENERAL_THROTTLING_MSG_EXTRA = extra_for_type("general-throttling-msg")
