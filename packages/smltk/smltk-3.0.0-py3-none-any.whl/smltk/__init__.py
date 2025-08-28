8a6acdad3aec5106e8201d7e8e210ac021aeed85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 14:10:48 +0000

    Add package smltk/3.0.0/smltk-3.0.0-py3-none-any.whl

---
+++ packages/smltk/smltk-3.0.0-py3-none-any.whl/smltk/__init__.py
@@ -0,0 +1,20 @@
+"""Simple Machine Learning Tool Kit package
+
+This package contains the modules to simplify your code for your machine learning processes.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the TDD.
+
+Package contents two classes to manage data preprocessing, testing and reporting.
+
+    >>> import smltk
+    >>> help(smltk)
+    >>> import smltk.modeling as Modeling
+    >>> help(Modeling)
+
+# license MIT
+# support https://github.com/bilardi/smltk/issues
+"""
+
+__version__ = "3.0.0"
+__author__ = "Alessandra Bilardi"
---
+++ packages/smltk/smltk-3.0.0-py3-none-any.whl/smltk/data_processing/__init__.py
@@ -0,0 +1,20 @@
+"""Simple Machine Learning Tool Kit package
+
+This package contains the modules to simplify your code for your data processing processes.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the TDD.
+
+Package contents two classes to manage data processing.
+
+    >>> import smltk
+    >>> help(smltk)
+    >>> from smltk.data_processing import DataProcessing
+    >>> help(DataProcessing)
+
+# license MIT
+# support https://github.com/bilardi/smltk/issues
+"""
+
+from .data_processing import DataProcessing
+from .ntk import Ntk
