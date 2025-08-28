a474031ae7d6adb32dc9ae1bfb169b99e250b3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 08:06:04 +0000

    Add package payloop/0.3.9/payloop-0.3.9.tar.gz

---
+++ packages/payloop/payloop-0.3.9.tar.gz/payloop-0.3.9/payloop/_invoke.py
@@ -0,0 +1,135 @@
+r"""
+ ___           _
+| _ \__ _ _  _| |___  ___ _ __
+|  _/ _` | || | / _ \/ _ \ '_ \
+|_| \__,_|\_, |_\___/\___/ .__/
+          |__/           |_|AI             07312025 / optimus codex
+"""
+
+import time
+from collections.abc import AsyncIterator
+
+from payloop._base import BaseInvoke
+from payloop._network import Collector
+from payloop._stream import AsyncStream
+from payloop._utils import merge_chunk
+
+
+class Invoke(BaseInvoke):
+    def invoke(self, **kwargs):
+        start = time.time()
+
+        raw_response = self._method(**kwargs)
+
+        Collector(self.config).fire_and_forget(
+            self._format_payload(
+                self._client_provider,
+                self._client_title,
+                self._client_version,
+                start,
+                time.time(),
+                self._format_kwargs(kwargs),
+                self._format_response(raw_response),
+            )
+        )
+
+        return raw_response
+
+
+class InvokeAsync(BaseInvoke):
+    async def invoke(self, **kwargs):
+        start = time.time()
+
+        raw_response = await self._method(**kwargs)
+
+        Collector(self.config).fire_and_forget(
+            self._format_payload(
+                self._client_provider,
+                self._client_title,
+                self._client_version,
+                start,
+                time.time(),
+                self._format_kwargs(kwargs),
+                self._format_response(raw_response),
+            )
+        )
+
+        return raw_response
+
+
+class InvokeAsyncIterator(BaseInvoke):
+    async def invoke(self, **kwargs):
+        start = time.time()
+
+        kwargs = self.configure_for_streaming_usage(kwargs)
+
+        raw_response = await self._method(**kwargs)
+        if isinstance(raw_response, AsyncIterator):
+            return (
+                AsyncStream(self.config, raw_response)
+                .configure_invoke(self)
+                .configure_request(kwargs, start)
+            )
+        else:
+            Collector(self.config).fire_and_forget(
+                self._format_payload(
+                    self._client_provider,
+                    self._client_title,
+                    self._client_version,
+                    start,
+                    time.time(),
+                    self._format_kwargs(kwargs),
+                    self._format_response(raw_response),
+                )
+            )
+
+            return raw_response
+
+
+class InvokeAsyncStream(BaseInvoke):
+    async def invoke(self, **kwargs):
+        start = time.time()
+
+        kwargs = self.configure_for_streaming_usage(kwargs)
+
+        stream = await self._method(**kwargs)
+
+        raw_response = {}
+        async for chunk in stream:
+            raw_response = merge_chunk(raw_response, chunk.__dict__)
+            yield chunk
+
+        Collector(self.config).fire_and_forget(
+            self._format_payload(
+                self._client_provider,
+                self._client_title,
+                self._client_version,
+                start,
+                time.time(),
+                self._format_kwargs(kwargs),
+                self._format_response(raw_response),
+            )
+        )
+
+
+class InvokeStream(BaseInvoke):
+    async def invoke(self, **kwargs):
+        start = time.time()
+
+        kwargs = self.configure_for_streaming_usage(kwargs)
+
+        raw_response = await self._method(**kwargs)
+
+        Collector(self.config).fire_and_forget(
+            self._format_payload(
+                self._client_provider,
+                self._client_title,
+                self._client_version,
+                start,
+                time.time(),
+                self._format_kwargs(kwargs),
+                self._format_response(raw_response),
+            )
+        )
+
+        return raw_response
---
+++ packages/payloop/payloop-0.3.9.tar.gz/payloop-0.3.9/tests/test_base.py
@@ -0,0 +1,97 @@
+from unit_test_objects import UnitTestX, UnitTestY
+
+from payloop._base import BaseInvoke
+from payloop._config import Config
+from payloop._constants import OPENAI_CLIENT_TITLE
+
+
+def test_list_to_json_native_types():
+    assert BaseInvoke(Config(), "abc").list_to_json([1, 2, 3]) == [1, 2, 3]
+
+    assert BaseInvoke(Config(), "abc").list_to_json([{"a": "b"}, {"c": "d"}]) == [
+        {"a": "b"},
+        {"c": "d"},
+    ]
+
+    assert BaseInvoke(Config(), "abc").list_to_json([[1, 2], [3, 4], [{"a", "b"}]]) == [
+        [1, 2],
+        [3, 4],
+        [{"a", "b"}],
+    ]
+
+    assert BaseInvoke(Config(), "abc").list_to_json(
+        [[1, {"a": "b"}], [{"c": "d"}, 2]]
+    ) == [
+        [1, {"a": "b"}],
+        [{"c": "d"}, 2],
+    ]
+
+
+def test_list_to_json_object_simple():
+    assert BaseInvoke(Config(), "abc").list_to_json([1, UnitTestX()]) == [
+        1,
+        {"a": 1, "b": 2},
+    ]
+
+
+def test_list_to_json_object_complex():
+    assert BaseInvoke(Config(), "abc").list_to_json([1, UnitTestY()]) == [
+        1,
+        {"c": 3, "d": {"a": 1, "b": 2}},
+    ]
+
+
+def test_list_to_json_list_list_list():
+    assert BaseInvoke(Config(), "abc").list_to_json([1, [2, [3, [4]]]]) == [
+        1,
+        [2, [3, [4]]],
+    ]
+
+
+def test_list_to_dict_to_list():
+    assert BaseInvoke(Config(), "abc").list_to_json([{"a": 1, "b": [1, [2]]}]) == [
+        {"a": 1, "b": [1, [2]]}
+    ]
+
+
+def test_dict_to_json_dict():
+    assert BaseInvoke(Config(), "abc").dict_to_json({"a": "b", "c": "d"}) == {
+        "a": "b",
+        "c": "d",
+    }
+
+
+def test_dist_to_json_dict_has_dict():
+    assert BaseInvoke(Config(), "abc").dict_to_json(
+        {"a": {"b": {"c": "d"}, "e": 123}}
+    ) == {"a": {"b": {"c": "d"}, "e": 123}}
+
+
+def test_configure_for_streaming_usage():
+    invoke = BaseInvoke(Config(), "abc")
+    invoke._client_title = OPENAI_CLIENT_TITLE
+
+    assert invoke.configure_for_streaming_usage({"abc": "def"}) == {
+        "abc": "def",
+        "stream_options": {"include_usage": True},
+    }
+
+    assert invoke.configure_for_streaming_usage(
+        {"abc": "def", "stream_options": {}}
+    ) == {"abc": "def", "stream_options": {"include_usage": True}}
+
+    assert invoke.configure_for_streaming_usage(
+        {"abc": "def", "stream_options": {"include_usage": False}}
+    ) == {"abc": "def", "stream_options": {"include_usage": True}}
+
+
+def test_configure_for_streaming_usage_streaming_options_is_not_dict():
+    invoke = BaseInvoke(Config(), "abc")
+    invoke._client_title = OPENAI_CLIENT_TITLE
+
+    assert invoke.configure_for_streaming_usage(
+        {"abc": "def", "stream_options": 123}
+    ) == {
+        "abc": "def",
+        "stream_options": {"include_usage": True},
+    }
