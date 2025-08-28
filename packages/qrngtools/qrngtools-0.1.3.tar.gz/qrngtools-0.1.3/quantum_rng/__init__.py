c585f0551106f44e23c291becbabf8f51390e2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:17 +0000

    Add package qrngtools/0.1.3/qrngtools-0.1.3.tar.gz

---
+++ packages/qrngtools/qrngtools-0.1.3.tar.gz/qrngtools-0.1.3/quantum_rng/__init__.py
@@ -0,0 +1,17 @@
+
+
+from qrngtools.qrng import (
+    get_random_bit, get_random_bits, get_random_int, get_random_float,
+    get_random_bytes, get_random_bool, get_random_choice, get_random_sample,
+    get_random_string, get_random_uuid, get_random_bit_array,
+    get_random_coin, get_random_dice, get_random_prime, get_random_gauss,
+    get_random_float_array, get_random_matrix,get_random_otp
+)
+
+__all__ = [
+    "get_random_bit", "get_random_bits", "get_random_int", "get_random_float",
+    "get_random_bytes", "get_random_bool", "get_random_choice", "get_random_sample",
+    "get_random_string", "get_random_uuid", "get_random_bit_array",
+    "get_random_coin", "get_random_dice", "get_random_prime", "get_random_gauss",
+    "get_random_float_array", "get_random_matrix", "get_random_otp"
+]
