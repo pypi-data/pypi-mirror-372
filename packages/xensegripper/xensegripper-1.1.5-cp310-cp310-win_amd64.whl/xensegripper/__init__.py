eb418d8d6ddfcd73baf53b54a1fec468fc092877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 16:06:21 +0000

    Add package xensegripper/1.1.5/xensegripper-1.1.5-cp310-cp310-win_amd64.whl

---
+++ packages/xensegripper/xensegripper-1.1.5-cp310-cp310-win_amd64.whl/xensegripper/__init__.py
@@ -0,0 +1,7 @@
+﻿import cypack; cypack.init(__name__, set([]));
+from .xense_gripper import XenseGripper
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+
+from ._version import __version__
+__all__ = ["__version__"]
