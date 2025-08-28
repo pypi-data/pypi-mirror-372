c75644ccdea668486e83507744296c5be4ce5643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 03:19:03 +0000

    Add package aiauto-client/0.1.3/aiauto_client-0.1.3.tar.gz

---
+++ packages/aiauto-client/aiauto_client-0.1.3.tar.gz/aiauto_client-0.1.3/src/aiauto/__init__.py
@@ -0,0 +1,16 @@
+from .core import AIAutoController, TrialController, CallbackTopNArtifact, StudyWrapper
+from .api import create_study
+from ._config import AIAUTO_API_TARGET
+from .constants import RUNTIME_IMAGES
+
+__version__ = "0.1.0"
+
+__all__ = [
+    'AIAutoController',
+    'TrialController', 
+    'CallbackTopNArtifact',
+    'StudyWrapper',
+    'create_study',
+    'AIAUTO_API_TARGET',
+    'RUNTIME_IMAGES',
+]
