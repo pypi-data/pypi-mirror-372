062be997163914cfed557dff027792509f9c82bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:06:38 +0000

    Add package types-PySocks/1.7.1.20250828/types_pysocks-1.7.1.20250828.tar.gz

---
+++ packages/types-PySocks/types_pysocks-1.7.1.20250828.tar.gz/types_pysocks-1.7.1.20250828/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'socks-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'sockshandler-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']})
