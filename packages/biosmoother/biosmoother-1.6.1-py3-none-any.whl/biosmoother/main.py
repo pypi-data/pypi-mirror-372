6064e66133578c554448fff07486dbc8f3fdaf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 04:04:54 +0000

    Add package biosmoother/1.6.1/biosmoother-1.6.1-py3-none-any.whl

---
+++ packages/biosmoother/biosmoother-1.6.1-py3-none-any.whl/biosmoother/main.py
@@ -0,0 +1,9 @@
+__author__ = "Markus Schmidt"
+__version__ = "1.1.0"
+__email__ = "Markus.Schmidt@lmu.de"
+
+from bin.main_layout import *
+from bokeh.plotting import curdoc
+
+# blub
+MainLayout().set_root()
