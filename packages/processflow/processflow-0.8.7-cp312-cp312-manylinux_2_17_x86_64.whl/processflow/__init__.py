20fdcad3edb989024625de1a2413dd19bf38f539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:05:55 +0000

    Add package processflow/0.8.7/processflow-0.8.7-cp312-cp312-manylinux_2_17_x86_64.whl

---
+++ packages/processflow/processflow-0.8.7-cp312-cp312-manylinux_2_17_x86_64.whl/processflow/__init__.py
@@ -0,0 +1,12 @@
+from .lane import EventType, ActivityType, GatewayType, TerminatorType
+from .processmap import ProcessMap
+from .coordinate import Side
+
+__all__ = [
+    "EventType",
+    "ActivityType",
+    "GatewayType",
+    "TerminatorType",
+    "ProcessMap",
+    "Side",
+]
