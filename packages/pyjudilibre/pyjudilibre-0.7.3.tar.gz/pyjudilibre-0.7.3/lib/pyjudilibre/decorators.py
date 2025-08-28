37e3cc4123eca434fba7126c50dd950b5388899b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 16:05:50 +0000

    Add package pyjudilibre/0.7.3/pyjudilibre-0.7.3.tar.gz

---
+++ packages/pyjudilibre/pyjudilibre-0.7.3.tar.gz/pyjudilibre-0.7.3/lib/pyjudilibre/decorators.py
@@ -0,0 +1,18 @@
+import functools
+from typing import Callable
+
+import httpx
+
+from pyjudilibre.exceptions import JudilibreWrongURLError
+
+
+def catch_wrong_url_error(function: Callable):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            results = function(*args, **kwargs)
+            return results
+        except httpx.ConnectError as exc:
+            raise JudilibreWrongURLError("URL is not reachable.") from exc
+
+    return wrapper
