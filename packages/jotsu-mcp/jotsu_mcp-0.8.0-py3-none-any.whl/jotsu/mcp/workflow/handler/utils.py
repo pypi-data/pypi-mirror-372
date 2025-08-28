740dd47e37228370b0a937ab96a33e6795b0330c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 05:05:58 +0000

    Add package jotsu-mcp/0.8.0/jotsu_mcp-0.8.0-py3-none-any.whl

---
+++ packages/jotsu-mcp/jotsu_mcp-0.8.0-py3-none-any.whl/jotsu/mcp/workflow/handler/utils.py
@@ -0,0 +1,34 @@
+import json
+
+from jotsu.mcp.types.models import WorkflowModelNode
+from jotsu.mcp.workflow.utils import pybars_render
+
+
+def get_messages(data: dict, prompt: str):
+    messages = data.get('messages', None)
+    if messages is None:
+        messages = []
+        prompt = data.get('prompt', prompt)
+        if prompt:
+            messages.append({
+                'role': 'user',
+                'content': pybars_render(prompt, data)
+            })
+    return messages
+
+
+def update_data_from_json(data: dict, content: str | dict | object, *, node: WorkflowModelNode):
+    json_data = json.loads(content) if isinstance(content, str) else content
+    if node.member:
+        node_data = data.get(node.member, {})
+        node_data.update(json_data)
+        data[node.member] = node_data
+    else:
+        data.update(json_data)
+
+
+def update_data_from_text(data: dict, text: str, *, node: WorkflowModelNode):
+    member = node.member or node.name
+    result = data.get(node.member or node.name, '')
+    result += text
+    data[member] = result
