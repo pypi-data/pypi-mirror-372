8553221fa9559bad72d051219f4eac2f2d440916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:06:57 +0000

    Add package lcviz/1.1.2/lcviz-1.1.2.tar.gz

---
+++ packages/lcviz/lcviz-1.1.2.tar.gz/lcviz-1.1.2/docs/reference/api.rst
@@ -0,0 +1,33 @@
+.. _lcviz-api:
+
+###
+API
+###
+
+.. _lcviz-api-configs:
+
+Plugins
+=======
+
+.. toctree::
+  :maxdepth: 2
+
+  api_plugins
+
+LCviz Helper
+============
+
+.. automodapi:: lcviz.helper
+   :no-inheritance-diagram:
+
+Viewers
+=======
+
+.. automodapi:: lcviz.viewers
+   :no-inheritance-diagram:
+
+Parsers
+=======
+
+.. automodapi:: lcviz.parsers
+   :no-inheritance-diagram:
---
+++ packages/lcviz/lcviz-1.1.2.tar.gz/lcviz-1.1.2/lcviz/plugins/markers/markers.py
@@ -0,0 +1,43 @@
+import numpy as np
+from traitlets import observe
+
+from jdaviz.configs.default.plugins import Markers
+from jdaviz.core.registries import tray_registry
+
+__all__ = ['Markers']
+
+
+@tray_registry('lcviz-markers', label="Markers")
+class Markers(Markers):
+    """
+    See the :ref:`Markers Plugin Documentation <markers>` for more details.
+
+    Only the following attributes and methods are available through the
+    :ref:`public plugin API <plugin-apis>`:
+
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.show`
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.open_in_tray`
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.close_in_tray`
+    * :meth:`clear_table`
+    * :meth:`~jdaviz.core.template_mixin.TableMixin.export_table`
+    """
+    _default_table_values = {'time': np.nan,
+                             'time:unit': '',
+                             'phase': np.nan,
+                             'ephemeris': '',
+                             'pixel': (np.nan, np.nan),
+                             'value': np.nan,
+                             'value:unit': ''}
+
+    def __init__(self, *args, **kwargs):
+        kwargs['headers'] = ['time', 'time:unit', 'phase', 'ephemeris',
+                             'pixel', 'value', 'value:unit', 'viewer']
+        super().__init__(*args, **kwargs)
+
+    @observe('vdocs')
+    def _update_docs_link(self, *args):
+        self.docs_link = f"https://lcviz.readthedocs.io/en/{self.vdocs}/plugins.html#markers"
+
+    @property
+    def coords_info(self):
+        return self.app.session.application._tools['lcviz-coords-info']
