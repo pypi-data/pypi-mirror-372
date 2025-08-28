27e1d3f7f7f23d0259e92482132a7f9a7174f1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 14:07:31 +0000

    Add package neurograd/0.9.0/neurograd-0.9.0.tar.gz

---
+++ packages/neurograd/neurograd-0.9.0.tar.gz/neurograd-0.9.0/neurograd/utils/aliases.py
@@ -0,0 +1,29 @@
+from neurograd.functions.activations import ReLU, LeakyReLU, Sigmoid, Softmax, Tanh, Passthrough
+from neurograd.nn.losses import MSE, RMSE, MAE, BinaryCrossEntropy, CategoricalCrossEntropy
+from neurograd.nn.initializers import Xavier, He, Zeros, Normal
+
+ACTIVATIONS = {
+    "relu": lambda: ReLU(),
+    "leaky_relu": lambda: LeakyReLU(),
+    "sigmoid": lambda: Sigmoid(),
+    "softmax": lambda: Softmax(),
+    "tanh": lambda: Tanh(),
+    "passthrough": lambda: Passthrough()
+}
+
+LOSSES = {
+    "mse": lambda: MSE(),
+    "rmse": lambda: RMSE(),
+    "mae": lambda: MAE(),
+    "binary_crossentropy": lambda: BinaryCrossEntropy(),
+    "categorical_crossentropy": lambda: CategoricalCrossEntropy()
+}
+
+
+# Note: Initializer aliases require parameters, so they're defined as dictionaries
+INITIALIZERS = {
+    "xavier": Xavier,
+    "he": He,
+    "zeros": Zeros,
+    "normal": Normal
+}
