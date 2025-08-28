903caa6f215bb79b9174de327e4f0a9fcdc11c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 16:06:09 +0000

    Add package tyk-time-converter/0.1.1/tyk_time_converter-0.1.1-py3-none-any.whl

---
+++ packages/tyk-time-converter/tyk_time_converter-0.1.1-py3-none-any.whl/tyk_time_converter/__init__.py
@@ -0,0 +1,7 @@
+"""tyk_time_converter包初始化文件"""
+
+from .tyk_time_converter_server import convert_seconds_to_time, get_current_time, current_time_iso_resource
+
+__version__ = "0.1.1"
+__author__ = "tangyongkang"
+__all__ = ["convert_seconds_to_time", "get_current_time", "current_time_iso_resource"]
