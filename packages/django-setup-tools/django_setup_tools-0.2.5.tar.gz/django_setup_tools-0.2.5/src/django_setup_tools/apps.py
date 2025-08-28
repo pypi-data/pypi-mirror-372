719711443b72ecf5c4cf907dd5c96b2eac080420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:48 +0000

    Add package django-setup-tools/0.2.5/django_setup_tools-0.2.5.tar.gz

---
+++ packages/django-setup-tools/django_setup_tools-0.2.5.tar.gz/django_setup_tools-0.2.5/src/django_setup_tools/apps.py
@@ -0,0 +1,9 @@
+"""Django app configuration for django-setup-tools."""
+from django.apps import AppConfig
+
+
+class DjangoSetupToolsConfig(AppConfig):
+    """App configuration for Django Setup Tools."""
+
+    name = "django_setup_tools"
+    default_auto_field = "django.db.models.BigAutoField"
