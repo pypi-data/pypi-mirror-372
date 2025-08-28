e0b75db571959dbb0afe7064429a5e9c1d9cbed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:05:57 +0000

    Add package gllm-core-binary/0.3.9/gllm_core_binary-0.3.9-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-core-binary/gllm_core_binary-0.3.9-cp313-cp313-win_amd64.whl/gllm_core/schema/__init__.pyi
@@ -0,0 +1,5 @@
+from gllm_core.schema.chunk import Chunk as Chunk
+from gllm_core.schema.component import Component as Component
+from gllm_core.schema.event import Event as Event
+
+__all__ = ['Chunk', 'Component', 'Event']
