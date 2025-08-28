0d24f847815a6989ca854f018ef9c800c6c1409e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:36 +0000

    Add package ironbook-a2a-extension/0.0.4/ironbook_a2a_extension-0.0.4-py3-none-any.whl

---
+++ packages/ironbook-a2a-extension/ironbook_a2a_extension-0.0.4-py3-none-any.whl/ironbook_a2a/__init__.py
@@ -0,0 +1,24 @@
+from a2a.types import AgentExtension
+
+_CORE_PATH = 'github.com/identitymachines/ironbook-a2a-extension/v1'
+IRONBOOK_EXTENSION_URI = f'https://{_CORE_PATH}'
+IRONBOOK_AGENT_DID_FIELD = f'{_CORE_PATH}/agent-did'
+IRONBOOK_AUTH_TOKEN_FIELD = f'{_CORE_PATH}/auth-token'
+IRONBOOK_CONTEXT_FIELD = f'{_CORE_PATH}/context'
+
+class IronBookExtension:
+    def agent_extension(self) -> AgentExtension:
+        """Get the AgentExtension representing this extension."""
+        return AgentExtension(
+            uri=IRONBOOK_EXTENSION_URI,
+            description='Enforces IronBook policies',
+            required=True,
+        )
+
+__all__ = [
+    'IronBookExtension',
+    'IRONBOOK_EXTENSION_URI',
+    'IRONBOOK_AGENT_DID_FIELD',
+    'IRONBOOK_AUTH_TOKEN_FIELD',
+    'IRONBOOK_CONTEXT_FIELD',
+]
