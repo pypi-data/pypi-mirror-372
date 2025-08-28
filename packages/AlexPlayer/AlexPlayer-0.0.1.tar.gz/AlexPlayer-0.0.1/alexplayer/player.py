9b52fb00cdd38d043d009c76289b884f00ea45b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 08:05:43 +0000

    Add package AlexPlayer/0.0.1/AlexPlayer-0.0.1.tar.gz

---
+++ packages/AlexPlayer/AlexPlayer-0.0.1.tar.gz/AlexPlayer-0.0.1/alexplayer/player.py
@@ -0,0 +1,23 @@
+"""
+Este es el modulo que incluye
+la clase de reproductor de musica
+"""
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+    def play(self, song):
+        """
+        Reproduce la cancion que recibe
+        
+        Parameters:
+        song (str): este es un string con la ruta de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito
+        """
+        print("reproduciendo cancion")
+    
+    def stop():
+        pass
---
+++ packages/AlexPlayer/AlexPlayer-0.0.1.tar.gz/AlexPlayer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+long_desc = Path("README.md").read_text()
+setuptools.setup(
+        name="AlexPlayer",
+        version="0.0.1",
+        long_description=long_desc,
+        packages=setuptools.find_packages(
+            exclude=["mocks","tests"]
+            )
+        )
