3899f4383ad4bb950878cb336a3a8141ddcf9ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 14:06:05 +0000

    Add package ambiegen/0.0.3/ambiegen-0.0.3.tar.gz

---
+++ packages/ambiegen/ambiegen-0.0.3.tar.gz/ambiegen-0.0.3/ambiegen/__init__.py
@@ -0,0 +1,51 @@
+from ambiegen.crossover.one_point_crossover import OnePointCrossover
+from pymoo.operators.crossover.sbx import SBX
+from ambiegen.mutation.obstacle_mutation import ObstacleMutation
+
+from pymoo.operators.mutation.pm import PM
+from ambiegen.mutation.uniform_mutation import UniformMutation
+
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.algorithms.soo.nonconvex.de import DE
+from pymoo.algorithms.soo.nonconvex.es import ES
+
+from ambiegen.sampling.abstract_sampling import AbstractSampling
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.operators.sampling.lhs import LHS
+from ambiegen.sampling.greedy_sampling import GreedySampling
+
+from pymoo.algorithms.soo.nonconvex.random_search import RandomSearch
+from ambiegen.sampling.lhs_sampling import LHSSampling
+ALGORITHMS = {
+    "ga": GA, # Genetic Algorithm,
+    "de": DE, # Differential Evolution
+    "es": ES, # Evolution Strategy
+    "random": RandomSearch
+}
+
+SAMPLERS = {
+    "random": FloatRandomSampling,
+    "lhs": LHS,
+    "abstract": AbstractSampling,
+    "lhs_sampling": LHSSampling,
+    "greedy": GreedySampling
+}
+
+CROSSOVERS = {
+    "one_point": OnePointCrossover,
+    "sbx": SBX
+}
+
+MUTATIONS = {
+    "obstacle": ObstacleMutation,
+    "pm": PM,
+    "uniform": UniformMutation,
+}
+
+
+
+
+
+
+
+
