98f25968844d7f284266050cc812b3d5631517bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 08:05:21 +0000

    Add package chonkie/1.2.0/chonkie-1.2.0.tar.gz

---
+++ packages/chonkie/chonkie-1.2.0.tar.gz/chonkie-1.2.0/setup.py
@@ -0,0 +1,32 @@
+"""Setup script for Chonkie's Cython extensions.
+
+This script configures the Cython extensions used in the Chonkie library.
+It includes the token_chunker, split, merge, and NumPy-free Savitzky-Golay extensions.
+"""
+import os
+
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+# Get the c_extensions directory
+c_extensions_dir = os.path.join("src", "chonkie", "chunker", "c_extensions")
+
+extensions = [
+    Extension(
+        "chonkie.chunker.c_extensions.split",
+        [os.path.join(c_extensions_dir, "split.pyx")],
+    ),
+    Extension(
+        "chonkie.chunker.c_extensions.merge",
+        [os.path.join(c_extensions_dir, "merge.pyx")],
+    ),
+    Extension(
+        "chonkie.chunker.c_extensions.savgol",
+        [os.path.join(c_extensions_dir, "savgol.pyx")],
+        extra_compile_args=["-O3", "-ffast-math"],
+    ),
+]
+
+setup(
+    ext_modules=cythonize(extensions, language_level=3),
+)
