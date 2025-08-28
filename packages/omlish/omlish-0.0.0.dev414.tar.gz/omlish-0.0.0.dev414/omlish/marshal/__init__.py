094ddb5537b0b0bf965362c17d37388b700e1b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 09:04:57 +0000

    Add package omlish/0.0.0.dev414/omlish-0.0.0.dev414.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev414.tar.gz/omlish-0.0.0.dev414/omlish/marshal/__init__.py
@@ -0,0 +1,189 @@
+from .base import (  # noqa
+    Marshaler,
+    Unmarshaler,
+
+    MarshalerMaker,
+    UnmarshalerMaker,
+
+    MarshalerFactory,
+    UnmarshalerFactory,
+
+    MarshalerFactory_,
+    UnmarshalerFactory_,
+
+    SimpleMarshalerFactory,
+    SimpleUnmarshalerFactory,
+
+    MarshalerFactoryMatchClass,
+    UnmarshalerFactoryMatchClass,
+
+    MultiMarshalerFactory,
+    MultiUnmarshalerFactory,
+
+    TypeMapMarshalerFactory,
+    TypeMapUnmarshalerFactory,
+
+    TypeCacheMarshalerFactory,
+    TypeCacheUnmarshalerFactory,
+
+    FuncMarshaler,
+    FuncUnmarshaler,
+
+    BaseContext,
+    MarshalContext,
+    UnmarshalContext,
+
+    RecursiveMarshalerFactory,
+    RecursiveUnmarshalerFactory,
+
+    Override,
+    ReflectOverride,
+)
+
+from .composite.iterables import (  # noqa
+    IterableMarshaler,
+    IterableUnmarshaler,
+)
+
+from .composite.wrapped import (  # noqa
+    WrappedMarshaler,
+    WrappedUnmarshaler,
+)
+
+from .errors import (  # noqa
+    ForbiddenTypeError,
+    MarshalError,
+    UnhandledTypeError,
+)
+
+from .globals import (  # noqa
+    GLOBAL_REGISTRY,
+
+    global_marshaler_factory,
+    marshal,
+
+    global_unmarshaler_factory,
+    unmarshal,
+
+    register_global,
+)
+
+from .naming import (  # noqa
+    Naming,
+    translate_name,
+)
+
+from .objects.dataclasses import (  # noqa
+    AbstractDataclassFactory,
+    DataclassMarshalerFactory,
+    DataclassUnmarshalerFactory,
+    get_dataclass_field_infos,
+    get_dataclass_metadata,
+)
+
+from .objects.helpers import (  # noqa
+    update_fields_metadata,
+    update_object_metadata,
+    with_field_metadata,
+)
+
+from .objects.marshal import (  # noqa
+    ObjectMarshaler,
+    SimpleObjectMarshalerFactory,
+)
+
+from .objects.metadata import (  # noqa
+    FieldInfo,
+    FieldInfos,
+    FieldMetadata,
+    FieldOptions,
+    ObjectMetadata,
+    ObjectSpecials,
+)
+
+from .objects.unmarshal import (  # noqa
+    ObjectUnmarshaler,
+    SimpleObjectUnmarshalerFactory,
+)
+
+from .polymorphism.marshal import (  # noqa
+    PolymorphismMarshaler,
+    PolymorphismMarshalerFactory,
+    make_polymorphism_marshaler,
+)
+
+from .polymorphism.metadata import (  # noqa
+    FieldTypeTagging,
+    Impl,
+    Impls,
+    Polymorphism,
+    TypeTagging,
+    WrapperTypeTagging,
+    polymorphism_from_impls,
+    polymorphism_from_subclasses,
+)
+
+from .polymorphism.standard import (  # noqa
+    standard_polymorphism_factories,
+)
+
+from .polymorphism.unions import (  # noqa
+    PRIMITIVE_UNION_TYPES,
+    PolymorphismUnionMarshalerFactory,
+    PolymorphismUnionUnmarshalerFactory,
+    PrimitiveUnionMarshaler,
+    PrimitiveUnionMarshalerFactory,
+    PrimitiveUnionUnmarshaler,
+    PrimitiveUnionUnmarshalerFactory,
+)
+
+from .polymorphism.unmarshal import (  # noqa
+    PolymorphismUnmarshaler,
+    PolymorphismUnmarshalerFactory,
+    make_polymorphism_unmarshaler,
+)
+
+from .singular.base64 import (  # noqa
+    BASE64_MARSHALER_FACTORY,
+    BASE64_UNMARSHALER_FACTORY,
+)
+
+from .singular.primitives import (  # noqa
+    PRIMITIVE_TYPES,
+)
+
+from .registries import (  # noqa
+    Registry,
+)
+
+from .standard import (  # noqa
+    STANDARD_MARSHALER_FACTORIES,
+    new_standard_marshaler_factory,
+
+    STANDARD_UNMARSHALER_FACTORIES,
+    new_standard_unmarshaler_factory,
+
+    install_standard_factories,
+)
+
+from .trivial.forbidden import (  # noqa
+    ForbiddenTypeMarshalerFactory,
+    ForbiddenTypeUnmarshalerFactory,
+)
+
+from .trivial.nop import (  # noqa
+    NOP_MARSHALER_UNMARSHALER,
+    NopMarshalerUnmarshaler,
+)
+
+from .values import (  # noqa
+    Value,
+)
+
+
+##
+
+
+from .. import lang as _lang
+
+_lang.trigger_conditional_imports(__package__)
---
+++ packages/omlish/omlish-0.0.0.dev414.tar.gz/omlish-0.0.0.dev414/omlish/marshal/singular/uuids.py
@@ -0,0 +1,31 @@
+import re
+import uuid
+
+from ... import check
+from ..base import MarshalContext
+from ..base import Marshaler
+from ..base import TypeMapMarshalerFactory
+from ..base import TypeMapUnmarshalerFactory
+from ..base import UnmarshalContext
+from ..base import Unmarshaler
+from ..values import Value
+
+
+##
+
+
+PATTERN = re.compile(r'([0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12})|([0-9A-Fa-f]{32})')
+
+
+class UuidMarshalerUnmarshaler(Marshaler, Unmarshaler):
+    def marshal(self, ctx: MarshalContext, o: uuid.UUID) -> Value:
+        return str(o)
+
+    def unmarshal(self, ctx: UnmarshalContext, v: Value) -> uuid.UUID:
+        return uuid.UUID(check.isinstance(v, str).replace('-', ''))
+
+
+UUID_MARSHALER_UNMARSHALER = UuidMarshalerUnmarshaler()
+
+UUID_MARSHALER_FACTORY = TypeMapMarshalerFactory({uuid.UUID: UUID_MARSHALER_UNMARSHALER})
+UUID_UNMARSHALER_FACTORY = TypeMapUnmarshalerFactory({uuid.UUID: UUID_MARSHALER_UNMARSHALER})
