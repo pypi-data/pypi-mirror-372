e827e18788caadddff30c334c0f12200f048a217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 15:06:38 +0000

    Add package types-PyMeeus/0.5.0.20250828/types_pymeeus-0.5.0.20250828.tar.gz

---
+++ packages/types-PyMeeus/types_pymeeus-0.5.0.20250828.tar.gz/types_pymeeus-0.5.0.20250828/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'pymeeus-stubs': ['Angle.pyi', 'Coordinates.pyi', 'CurveFitting.pyi', 'Earth.pyi', 'Epoch.pyi', 'Interpolation.pyi', 'Jupiter.pyi', 'JupiterMoons.pyi', 'Mars.pyi', 'Mercury.pyi', 'Minor.pyi', 'Moon.pyi', 'Neptune.pyi', 'Pluto.pyi', 'Saturn.pyi', 'Sun.pyi', 'Uranus.pyi', 'Venus.pyi', '__init__.pyi', 'base.pyi', 'METADATA.toml', 'py.typed']})
