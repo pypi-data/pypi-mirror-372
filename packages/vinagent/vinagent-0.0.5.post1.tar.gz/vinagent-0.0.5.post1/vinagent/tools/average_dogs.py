47308ab7936532b2766bc639d0337ddb9ea755cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 04:05:17 +0000

    Add package vinagent/0.0.5.post1/vinagent-0.0.5.post1.tar.gz

---
+++ packages/vinagent/vinagent-0.0.5.post1.tar.gz/vinagent-0.0.5.post1/vinagent/tools/average_dogs.py
@@ -0,0 +1,13 @@
+def weight_of_bulldog():
+    """The weight of a bulldog"""
+    return 25
+
+
+def weight_of_husky():
+    """The weight of a husky"""
+    return 20
+
+
+def average_weight_of_two_dogs(weight1: float, weight2: float):
+    """The average weight of two dogs"""
+    return (weight1 + weight2) / 2
