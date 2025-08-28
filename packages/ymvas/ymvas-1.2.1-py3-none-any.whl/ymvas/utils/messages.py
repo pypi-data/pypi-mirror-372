1fb887c037ba6a97bea154c8aa747fd3c7d5597f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 07:05:32 +0000

    Add package ymvas/1.2.1/ymvas-1.2.1-py3-none-any.whl

---
+++ packages/ymvas/ymvas-1.2.1-py3-none-any.whl/ymvas/utils/messages.py
@@ -0,0 +1,49 @@
+from . import (
+    global_config_file,
+    global_config_dir
+)
+
+errors = {
+    # system errors
+    "global-dir-not-created" : {
+        "msg"  : (
+            "Unable to create global directory, please check permisions!"
+            f"[{global_config_dir()}]"
+        ),
+        "code" : "501"
+    },
+    "global-config-not-created" : {
+        "msg" : (
+            "Unable to create global config file please check permisions!"
+            f"[{global_config_file()}]"
+        ),
+        "code" : "502"
+    },
+    "global-config-misconfigured": {
+        "msg" : (
+            f"Unable to parse config file [{global_config_file()}]"
+            ", please remove it or fix the contents"
+        ),
+        "code" : "503"
+    },
+
+
+
+
+
+
+
+
+
+
+    # compiler errors
+    "compiler-invalid-path" : {
+        "msg" : (
+            "[{src}] is not a valid repository!"
+        ),
+        "code" : '301'
+    }
+
+
+
+}
