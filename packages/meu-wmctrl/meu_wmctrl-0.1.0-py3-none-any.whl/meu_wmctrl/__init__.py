b43779e298ec87481bbdfc98e9bbd8e56f927de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 06:05:59 +0000

    Add package meu-wmctrl/0.1.0/meu_wmctrl-0.1.0-py3-none-any.whl

---
+++ packages/meu-wmctrl/meu_wmctrl-0.1.0-py3-none-any.whl/meu_wmctrl/__init__.py
@@ -0,0 +1,16 @@
+# meu_wmctrl/__init__.py
+
+from .funcoes_wmctrl import (
+    listar_janelas,
+    ativar_janela,
+    fechar_janela,
+    mudar_desktop,
+    obter_geometria_janela,
+    mover_janela,
+    redimensionar_janela,
+    maximizar_janela,
+    desmaximizar_janela,
+    minimizar_janela,
+    restaurar_janela,
+    troca_desktop_janela
+)
