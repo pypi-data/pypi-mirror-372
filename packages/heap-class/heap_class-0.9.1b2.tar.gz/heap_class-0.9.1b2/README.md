446b301d752700cde486123e3872ed58632ccc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-28 08:05:18 +0000

    Add package heap-class/0.9.1b2/heap_class-0.9.1b2.tar.gz

---
+++ packages/heap-class/heap_class-0.9.1b2.tar.gz/heap_class-0.9.1b2/README.md
@@ -0,0 +1,223 @@
+# heap_class
+
+list-like implementation of heap/PriorityQueue in Python
+
+# Installation
+
+```
+% pip install heap-class
+```
+
+Note the hyphen in the package name
+
+# Usage
+
+From `heap_class` (underscore, not hyphen) import `Heap`.
+
+Create an easy max-heap.
+
+```
+>>> from heap_class import Heap
+>>> h = Heap([3, 1, 9, 20], max=True)
+```
+
+Standard list and heap methods are all available
+
+```
+>>> h.pop()
+20
+>>> h.peek()  # same as h[0]
+9
+>>> h.push(17)  # or h.append(17)
+>>> h[0]  # same as h.peek()
+17
+>>> h[1]  # inefficient, but works
+9
+```
+
+Calling `reversed()` turns it into a min-heap
+
+```
+>>> y = reversed(h)
+>>> y.peek()
+1
+```
+
+The `__repr__` of a heap is inefficient, but 
+useful for debugging.  It shows the sorted version
+of the heap.
+
+```
+>>> y
+Heap([1, 3, 9, 17], max=False)
+```
+
+Contains checking takes place in list's normal `O(n)` time.
+
+```
+>>> 9 in y
+True
+```
+
+To view the raw structure of the heap, call `.raw()`.
+(this is the same view as a heapified list)
+
+```
+>>> y.raw()
+[1, 3, 17, 9]
+>>> set(y.raw())  # raw() is helpful for fast casting
+{1, 3, 17, 9}
+```
+
+Complex entries such as tuples are supported:
+
+```
+>>> h2 = Heap([(6,4), (6,9), (10,2)], max=True)
+>>> h2.pop()
+(10, 2)
+>>> h2.pop()
+(6, 9)
+```
+
+Max heaps with tuples of different types are rather hard 
+in heapq because of the different forms of negation each
+position needs.  But they are easy here.  This is one of
+the main implementation advantages of using heap-class.
+
+```
+>>> h2 = Heap([('aa', 4), ('aa', 5), ('zz', 2), ('zz', 1)], max=True)
+>>> h2.pop()
+('zz', 2)
+```
+
+Custom sort keys are supported.  Here we will sort the
+heap of names by the second entry (last name)
+
+```
+>>> vals = [('Adam', 'Smith'), ('Zeta', 'Jones')]
+>>> h3 = Heap(vals, key=lambda name: name[1])
+>>> h3.peek()  # Jones comes before Smith
+('Zeta', 'Jones')
+>>> h3.push(('Aaron', 'Allen'))
+>>> h3.peek()
+('Aaron', 'Allen')
+```
+
+`replace(val)` returns the current first item and
+replaces it with the new item (same as 
+`heapreplace`/`heapreplace_max` in heapq).
+
+Notice that if you replace the top item with a new item 
+the order of the heap automatically changes, so the
+new top might differ.
+
+```
+>>> h3.replace(('Annie', 'Sun'))
+('Aaron', 'Allen')
+>>> h3.peek()
+('Zeta', 'Jones')
+```
+
+(`pushpop()` is also supported, which adds the new item first
+before popping the top of the heap).
+
+It is possible to iterate further through the heap
+as a sorted container, though this is inefficient
+(useful for testing, however)
+
+```
+>>> for ordered_name in h3:
+...     print(ordered_name)
+('Zeta', 'Jones')
+('Adam', 'Smith')
+('Annie', 'Sun')
+```
+
+Note that if you plan to iterate through the whole
+Heap, sorting with the same key has the same algorithmic
+efficiency but is much faster since it is highly optimized
+in C:
+
+```
+>>> for ordered_name in sorted(h3.raw(), key=lambda name: name[1]):
+...     print(ordered_name)
+('Zeta', 'Jones')
+('Adam', 'Smith')
+('Annie', 'Sun')
+```
+
+# Change Log
+
+* 0.9.1b2 -- (2025-08)
+    - Add `key` support
+    - Added typing
+    - Add tests
+    - Removed support for `*args` creation: together with `key` support it made
+      typing a complete nightmare.
+    - Remove undocumented support for `.tup=True/False` a kludge unnecessary after 
+      `*args` creation was removed.
+    - Remove undocumented `no_private_imports.py` version of Heap. We'll
+      update this when heapq changes.
+    - Point GitHub link to the right place.
+
+* 0.9.0b1 -- (2022)
+    - Initial release
+
+
+# Credit
+
+Copyright © 2022-25 Michael Scott Asato Cuthbert
+
+# License
+
+MIT
+
+# Development/Distribution
+
+* Install build tools
+
+```
+% pip install -U build twine pytest
+```
+
+* Run tests
+
+```
+% pytest tests/tests.py
+```
+
+If you get big failures, make sure you've run `pip install -e .` in the main directory 
+(with pyproject.toml in it) to make `heap_class` visible to the tests.
+
+Fix any problems, obviously.
+
+* Update `__version__` in `heap_class/__init__.py`.
+* Empty existing builds:
+
+```
+% trash dist build *.egg-info
+```
+
+(if you don't have trash on your OS, delete the `dist` `build` and `*.egg-info` files/directories 
+manually or in another way)
+
+* Build the distribution
+
+```
+% python -m build
+```
+
+* Check that artifacts render on PyPI
+
+```
+% twine check dist/*
+```
+
+* Upload to PyPI
+
+```
+% twine upload dist/*
+```
+
+You will need the proper `.pypirc` with passwords etc.  See elsewhere on-line for
+docs on that.
---
+++ packages/heap-class/heap_class-0.9.1b2.tar.gz/heap_class-0.9.1b2/heap_class/__init__.py
@@ -0,0 +1,320 @@
+'''
+list-like implementation of heap/PriorityQueue.
+'''
+from __future__ import annotations
+
+from collections.abc import MutableSequence, Iterable, Iterator, Callable
+from typing import TypeVar, Union, Optional, Any
+# noinspection PyUnresolvedReferences,PyProtectedMember
+from heapq import (
+    heappop, heappush, heappushpop, heapreplace, heapify,
+    _heappop_max as heappop_max,
+    _heapreplace_max as heapreplace_max,
+    _heapify_max as heapify_max,
+    _siftdown_max,  # implemented in Python in heapq
+    _siftup_max,  # implemented in Python in heapq
+)
+
+C_LANGUAGE_HEAP = (type(heappop_max).__name__ == 'builtin_function_or_method')
+
+
+__version__ = '0.9.1b2'
+
+
+HeapContents = TypeVar('HeapContents')
+
+
+def heappush_max(heap, item):
+    heap.append(item)
+    _siftdown_max(heap, 0, len(heap)-1)
+
+
+def heappushpop_max(heap, item):
+    if C_LANGUAGE_HEAP:
+        # if in C, fastest will probably be push then pop
+        heappush_max(heap, item)
+        return heappop_max(item)
+    else:
+        # do the manipulation efficiently but in Python
+        if heap and heap[0] > item:
+            item, heap[0] = heap[0], item
+            _siftup_max(heap, 0)
+        return item
+
+
+class Heap(MutableSequence[HeapContents]):
+    # noinspection PyShadowingBuiltins
+    def __init__(self,
+                 items: Iterable[HeapContents],
+                 max: bool = False,  # pylint: disable=redefined-builtin
+                 key: Optional[Callable[[HeapContents], Any]] = None,
+                 _replace_heap: bool = False):
+        self.max = max
+        self.key = key
+
+        self._heap: list[Union[HeapContents, tuple[Any, HeapContents]]]
+        if _replace_heap:
+            self._heap = items
+            return
+
+        heap_items: list[HeapContents]
+        if not items:
+            heap_items = []
+        else:
+            heap_items = list(items)
+
+        if self.key is not None:
+            heap_items = [self._add_key(i) for i in heap_items]
+
+        if heap_items and self.max:
+            heapify_max(heap_items)
+        elif heap_items:
+            heapify(heap_items)
+        self._heap = heap_items
+
+    def __getitem__(self, pos: int) -> HeapContents:
+        '''
+        Quite inefficient.
+        '''
+        if pos == 0:
+            return self._del_key(self._heap[0])
+        if pos < 0:
+            pos = len(self) + pos
+
+        for i, item in enumerate(self._iter_with_key()):
+            if i == pos:
+                return self._del_key(item)
+        raise IndexError('Heap index out of range')
+
+    def __setitem__(self, pos: int, new_item: HeapContents) -> None:
+        '''
+        Quite inefficient.  Simply removes the item at that position
+        and then appends a new one.  No guarantee at all that this will
+        be true:  `h[3] = 9;  h[3] == 9`.
+        '''
+        if pos < 0:
+            pos = len(self) + pos
+        if pos > len(self):
+            raise IndexError('Heap index out of range')
+
+        new_item = self._add_key(new_item)
+
+        new_items: list[HeapContents] = []
+        for i, item in enumerate(self._iter_with_key()):
+            if i != pos:
+                new_items.append(item)
+            else:
+                new_items.append(new_item)
+        if self.max:
+            heapify_max(new_items)
+        else:
+            heapify(new_items)
+        self._heap = new_items
+
+    def __delitem__(self, pos: int) -> None:
+        '''
+        Quite inefficient
+        '''
+        if pos < 0:
+            pos = len(self) + pos
+        if pos > len(self):
+            raise IndexError('Heap index out of range')
+
+        new_items = []
+        for i, item in enumerate(self._iter_with_key()):
+            if i != pos:
+                new_items.append(item)  # we reverse maxification
+        if self.max:
+            heapify_max(new_items)
+        else:
+            heapify(new_items)
+        self._heap = new_items
+
+
+    def __len__(self) -> int:
+        return len(self._heap)
+
+    def __bool__(self) -> bool:
+        return bool(self._heap)
+
+    def __contains__(self, item: HeapContents) -> bool:
+        return self._add_key(item) in self._heap
+
+    def __iter__(self) -> Iterator[HeapContents]:
+        for item in self._iter_with_key():
+            yield self._del_key(item)
+
+    def __repr__(self) -> str:
+        return f'Heap({list(self)}, max={self.max})'
+
+    def __reversed__(self) -> 'Heap[HeapContents]':
+        return Heap(self.raw(), max=not self.max)
+
+    def _add_key(self, item: HeapContents) -> Union[HeapContents, tuple[Any, HeapContents]]:
+        if self.key is None:
+            return item
+        kv = self.key(item)
+        return (kv, item)
+
+    def _del_key(self, item: Union[HeapContents, tuple[Any, HeapContents]]) -> HeapContents:
+        if self.key is None:
+            return item
+        else:
+            return item[1]
+
+    def _iter_with_key(self) -> Iterator[Union[tuple[Any, HeapContents], HeapContents]]:
+        temp_heap = self._heap[:]
+        while temp_heap:
+            if self.max:
+                yield heappop_max(temp_heap)
+            else:
+                yield heappop(temp_heap)
+
+    def append(self, new_item: HeapContents) -> None:
+        '''
+        synonym for push -- to make it feel more list-like
+        '''
+        self.push(new_item)
+
+    def clear(self) -> None:
+        self._heap = []
+
+    def copy(self) -> 'Heap[HeapContents]':
+        return Heap(self._heap[:], max=self.max, key=self.key, _replace_heap=True)
+
+    def count(self, item: HeapContents) -> int:
+        return self._heap.count(self._add_key(item))
+
+    def extend(self, others: Iterable[HeapContents]) -> None:
+        for o in others:
+            self.push(self._add_key(o))
+
+    def index(self, item: HeapContents, start: int = 0, end: int = -1) -> int:
+        '''
+        Another inefficient operation
+        '''
+        keyed_item = self._add_key(item)
+        for i, existing in self._iter_with_key():
+            if existing == keyed_item and i >= start and (end == -1 or i < end):
+                return i
+
+        raise ValueError(f'{item!r} is not in Heap')
+
+    def insert(self, index: int, item: HeapContents) -> None:
+        '''
+        Index does not matter, except for checking input.
+        '''
+        if not isinstance(index, int):
+            raise TypeError(
+                f'{type(index).__name__}  object cannot be interpreted as an integer'
+            )
+        self.push(item)
+
+    def peek(self) -> HeapContents:
+        try:
+            return self._del_key(self._heap[0])
+        except IndexError:
+            raise IndexError('peek on empty Heap') from None
+
+    def pop(self, index: int = 0) -> HeapContents:
+        if not self._heap:
+            raise IndexError('pop from empty Heap')
+
+        if index < 0:
+            index = len(self._heap) + index
+
+        if index == 0:
+            if self.max:
+                return self._del_key(heappop_max(self._heap))
+            else:
+                return self._del_key(heappop(self._heap))
+
+        if index < 0 or index >= len(self._heap):
+            raise IndexError('pop index out of range')
+
+        # inefficient time.
+        new_items = []
+        o: HeapContents = self._heap[0]
+        i = 0
+        for i, item in enumerate(self._iter_with_key()):
+            if i == index:
+                o = self._del_key(item)
+            else:
+                new_items.append(item)
+
+        if self.max:
+            heapify_max(new_items)
+        else:
+            heapify(new_items)
+        self._heap = new_items
+        return o
+
+    def push(self, new_item: HeapContents) -> None:
+        if self.max:
+            heappush_max(self._heap, self._add_key(new_item))
+        else:
+            heappush(self._heap, self._add_key(new_item))
+
+    def pushpop(self, new_item: HeapContents) -> HeapContents:
+        '''
+        Add `new_item` to the heap and then pop the top
+        of the heap.  More efficient than pushing
+        and popping in two operations.
+        '''
+        new_item = self._add_key(new_item)
+        if self.max:
+            o = heappushpop_max(self._heap, new_item)
+        else:
+            o = heappushpop(self._heap, new_item)
+        return self._del_key(o)
+
+    def raw(self) -> list[HeapContents]:
+        '''
+        The `raw` method returns the contents of the heap in
+        arbitrary order.  Useful for efficiently calling `set(h.raw())`.
+        '''
+        return [self._del_key(i) for i in self._heap]
+
+    def remove(self, item: HeapContents) -> None:
+        '''
+        Note that if there are two items which are equal but also one is > the
+        other (a very poor example of ordering) then the item removed
+        might not be the same as if you iterated through the Heap and
+        removed the smallest/largest item that equaled item.
+        '''
+        try:
+            self._heap.remove(self._add_key(item))
+        except ValueError:
+            raise ValueError('Heap.remove(x): x not in Heap')
+
+        if self.max:
+            heapify_max(self._heap)
+        else:
+            heapify(self._heap)
+
+    def replace(self, item: HeapContents) -> HeapContents:
+        '''
+        Replace the first item with the new item.  Same as heapreplace
+        '''
+        item = self._add_key(item)
+        if self.max:
+            o = heapreplace_max(self._heap, item)
+        else:
+            o = heapreplace(self._heap, item)
+        return self._del_key(o)
+
+    def reverse(self) -> None:
+        '''
+        O(n) operation
+        '''
+        self.max = not self.max
+        if self.max:
+            heapify_max(self._heap)
+        else:
+            heapify(self._heap)
+
+    def sort(self, key=None) -> None:
+        '''
+        does nothing.  Heaps are a form of sorted.
+        '''
+        pass
