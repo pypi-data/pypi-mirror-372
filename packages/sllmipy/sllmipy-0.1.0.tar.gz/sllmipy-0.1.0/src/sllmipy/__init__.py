1119bc752caf4fc00c0be2daaa66ca5995fe8bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:06:32 +0000

    Add package sllmipy/0.1.0/sllmipy-0.1.0.tar.gz

---
+++ packages/sllmipy/sllmipy-0.1.0.tar.gz/sllmipy-0.1.0/src/sllmipy/__init__.py
@@ -0,0 +1,4 @@
+from .base_llm_model import BaseLLMModel
+from .gemini import GeminiModel
+
+__all__ = ["BaseLLMModel", "GeminiModel"]
---
+++ packages/sllmipy/sllmipy-0.1.0.tar.gz/sllmipy-0.1.0/src/sllmipy/base_llm_model.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from .config_model import ConfigModel
+
+
+class BaseLLMModel(ABC):
+
+    @abstractmethod
+    def generate(self, prompt: str | None, config: ConfigModel | None = None) -> str:
+        pass
+
+    @abstractmethod
+    def count_tokens(self, prompt: str | None) -> int:
+        pass
