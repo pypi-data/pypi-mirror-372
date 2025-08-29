10f0c1991ed76f4c3b3ebcacd6db4f9927f71064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 03:22:01 +0000

    Add package orbslam3-python/1.0.1/orbslam3_python-1.0.1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/orbslam3-python/orbslam3_python-1.0.1-cp312-cp312-manylinux_2_28_x86_64.whl/orbslam3/_version.py
@@ -0,0 +1,6 @@
+# orbslam3/_version.py
+
+# The single source of truth for the package version and additional information
+__version__ = "1.0.1"
+__url__ = "https://github.com/AlexandruRO45/ORB_SLAM-PythonBindings/"
+__dependencies__ = ["numpy", "opencv-python"]
