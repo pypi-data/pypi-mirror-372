e8d8675f6e46fb16acaa8f94e2e6c4eed083a360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:07:12 +0000

    Add package webnetwork/1.1.1/webnetwork-1.1.1.tar.gz

---
+++ packages/webnetwork/webnetwork-1.1.1.tar.gz/webnetwork-1.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="webnetwork",
+    version="1.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.28.0",
+        "scapy>=2.5.0"
+    ],
+    python_requires=">=3.8",
+    description="Automatic web server monitoring and defensive DoS/DDoS detection with blocking",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="mrfidal",
+    author_email="mrfidal@proton.me",
+    url="https://github.com/bytebreach/webnetwork",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: System :: Monitoring",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+    ],
+    entry_points={
+        "console_scripts": [
+            "webnetwork=webnetwork.detector:start"
+        ],
+    },
+)
