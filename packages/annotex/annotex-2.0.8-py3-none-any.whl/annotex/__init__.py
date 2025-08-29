349c90831f0cbeee6148a9eea26105c9a3949445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:06:36 +0000

    Add package annotex/2.0.8/annotex-2.0.8-py3-none-any.whl

---
+++ packages/annotex/annotex-2.0.8-py3-none-any.whl/annotex/__init__.py
@@ -0,0 +1,11 @@
+"""
+Annotex -  Annotation Tool
+"""
+
+__version__ = "2.0.8"
+__author__ = "Randika K. Makumbura"
+__email__ = "Randikamk.96@gmail.com"
+
+from annotex.main import main
+
+__all__ = ["main", "__version__"]
