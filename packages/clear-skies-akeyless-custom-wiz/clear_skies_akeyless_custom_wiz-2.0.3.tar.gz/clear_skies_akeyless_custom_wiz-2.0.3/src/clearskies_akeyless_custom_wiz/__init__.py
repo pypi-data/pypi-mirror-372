7c30877361b01fd09c923a2a9f55e6dbceafe27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 03:20:37 +0000

    Add package clear-skies-akeyless-custom-wiz/2.0.3/clear_skies_akeyless_custom_wiz-2.0.3.tar.gz

---
+++ packages/clear-skies-akeyless-custom-wiz/clear_skies_akeyless_custom_wiz-2.0.3.tar.gz/clear_skies_akeyless_custom_wiz-2.0.3/src/clearskies_akeyless_custom_wiz/__init__.py
@@ -0,0 +1,22 @@
+import clearskies
+from clearskies import columns, validators
+
+import clearskies_akeyless_custom_producer
+from clearskies_akeyless_custom_wiz.create import create
+from clearskies_akeyless_custom_wiz.rotate import rotate
+
+class PayloadSchema(clearskies.Schema):
+    clientId = columns.String(
+        validators=[validators.Required()]
+    )
+    clientSecret = columns.String(
+        validators=[validators.Required()]
+    )
+
+def build_wiz_producer(url: str = "") -> clearskies_akeyless_custom_producer.endpoints.NoInput:
+    return clearskies_akeyless_custom_producer.endpoints.NoInput(
+        create_callable=create,
+        rotate_callable=rotate,
+        payload_schema=PayloadSchema,
+        url=url,
+    )
