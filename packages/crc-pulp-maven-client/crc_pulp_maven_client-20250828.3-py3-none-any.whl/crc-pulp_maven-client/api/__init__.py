7130b738c095d9894b0776e72ccb1471d78afd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:56 +0000

    Add package crc-pulp-maven-client/20250828.3/crc_pulp_maven_client-20250828.3-py3-none-any.whl

---
+++ packages/crc-pulp-maven-client/crc_pulp_maven_client-20250828.3-py3-none-any.whl/crc-pulp_maven-client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from crc-pulp_maven-client.api.api_maven_api import ApiMavenApi
+from crc-pulp_maven-client.api.content_artifact_api import ContentArtifactApi
+from crc-pulp_maven-client.api.distributions_maven_api import DistributionsMavenApi
+from crc-pulp_maven-client.api.remotes_maven_api import RemotesMavenApi
+from crc-pulp_maven-client.api.repositories_maven_api import RepositoriesMavenApi
+from crc-pulp_maven-client.api.repositories_maven_versions_api import RepositoriesMavenVersionsApi
+
