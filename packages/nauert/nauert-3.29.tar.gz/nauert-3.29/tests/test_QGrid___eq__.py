3a8150f770c155a9b07b00fe5173aef7542691c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:18 +0000

    Add package nauert/3.29/nauert-3.29.tar.gz

---
+++ packages/nauert/nauert-3.29.tar.gz/nauert-3.29/tests/test_QGrid___eq__.py
@@ -0,0 +1,129 @@
+import abjad
+
+import nauert
+
+
+def test_QGrid___eq___01():
+    a = nauert.QGrid()
+    b = nauert.QGrid()
+    assert format(a) == format(b)
+    assert a != b
+
+
+def test_QGrid___eq___02():
+    a = nauert.QGrid(
+        root_node=nauert.QGridContainer(
+            (1, 1),
+            children=[
+                nauert.QGridLeaf(
+                    preprolated_duration=abjad.Duration(1, 1),
+                    q_event_proxies=[
+                        nauert.QEventProxy(
+                            nauert.SilentQEvent(abjad.duration.offset(100)),
+                            abjad.duration.offset(0.5),
+                        )
+                    ],
+                )
+            ],
+        ),
+        next_downbeat=nauert.QGridLeaf(
+            preprolated_duration=abjad.Duration(1, 1),
+            q_event_proxies=[
+                nauert.QEventProxy(
+                    nauert.TerminalQEvent(abjad.duration.offset(200)),
+                    abjad.duration.offset(0.9),
+                )
+            ],
+        ),
+    )
+    b = nauert.QGrid(
+        root_node=nauert.QGridContainer(
+            (1, 1),
+            children=[
+                nauert.QGridLeaf(
+                    preprolated_duration=abjad.Duration(1, 1),
+                    q_event_proxies=[
+                        nauert.QEventProxy(
+                            nauert.SilentQEvent(abjad.duration.offset(100)),
+                            abjad.duration.offset(0.5),
+                        )
+                    ],
+                )
+            ],
+        ),
+        next_downbeat=nauert.QGridLeaf(
+            preprolated_duration=abjad.Duration(1, 1),
+            q_event_proxies=[
+                nauert.QEventProxy(
+                    nauert.TerminalQEvent(abjad.duration.offset(200)),
+                    abjad.duration.offset(0.9),
+                )
+            ],
+        ),
+    )
+    assert format(a) == format(b)
+    assert a != b
+
+
+def test_QGrid___eq___03():
+    a = nauert.QGrid()
+    b = nauert.QGrid(
+        root_node=nauert.QGridContainer(
+            (1, 1),
+            children=[
+                nauert.QGridLeaf(
+                    preprolated_duration=abjad.Duration(1, 1),
+                    q_event_proxies=[
+                        nauert.QEventProxy(
+                            nauert.SilentQEvent(abjad.duration.offset(100)),
+                            abjad.duration.offset(0.5),
+                        )
+                    ],
+                )
+            ],
+        )
+    )
+    c = nauert.QGrid(
+        next_downbeat=nauert.QGridLeaf(
+            preprolated_duration=abjad.Duration(1, 1),
+            q_event_proxies=[
+                nauert.QEventProxy(
+                    nauert.TerminalQEvent(abjad.duration.offset(200)),
+                    abjad.duration.offset(0.9),
+                )
+            ],
+        )
+    )
+    d = (
+        nauert.QGrid(
+            root_node=nauert.QGridContainer(
+                (1, 1),
+                children=[
+                    nauert.QGridLeaf(
+                        preprolated_duration=abjad.Duration(1, 1),
+                        q_event_proxies=[
+                            nauert.QEventProxy(
+                                nauert.SilentQEvent(abjad.duration.offset(100)),
+                                abjad.duration.offset(0.5),
+                            )
+                        ],
+                    )
+                ],
+            ),
+            next_downbeat=nauert.QGridLeaf(
+                preprolated_duration=abjad.Duration(1, 1),
+                q_event_proxies=[
+                    nauert.QEventProxy(
+                        nauert.TerminalQEvent(abjad.duration.offset(200)),
+                        abjad.duration.offset(0.9),
+                    )
+                ],
+            ),
+        ),
+    )
+    assert a != b
+    assert a != c
+    assert a != d
+    assert b != c
+    assert b != d
+    assert c != d
