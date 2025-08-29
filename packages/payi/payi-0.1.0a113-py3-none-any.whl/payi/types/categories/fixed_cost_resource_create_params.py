46257f7f85f3410a745f02a874d860ef5404b014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:07:05 +0000

    Add package payi/0.1.0a113/payi-0.1.0a113-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a113-py3-none-any.whl/payi/types/categories/fixed_cost_resource_create_params.py
@@ -0,0 +1,21 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union
+from datetime import datetime
+from typing_extensions import Required, Annotated, TypedDict
+
+from ..._utils import PropertyInfo
+
+__all__ = ["FixedCostResourceCreateParams"]
+
+
+class FixedCostResourceCreateParams(TypedDict, total=False):
+    category: Required[str]
+
+    units: Required[List[str]]
+
+    cost_per_hour: float
+
+    start_timestamp: Annotated[Union[str, datetime, None], PropertyInfo(format="iso8601")]
