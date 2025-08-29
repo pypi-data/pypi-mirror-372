d8ed9f882d8d760497c1f4c73c56a5917a8cfedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:07:13 +0000

    Add package cinetica/0.3.7/cinetica-0.3.7-py3-none-any.whl

---
+++ packages/cinetica/cinetica-0.3.7-py3-none-any.whl/cinetica/__init__.py
@@ -0,0 +1,5 @@
+from .movimiento_rectilineo import MovimientoRectilineo
+from .movimiento_parabolico import MovimientoParabolico
+from .movimiento_circular import MovimientoCircular
+
+__version__ = "0.3.6"
