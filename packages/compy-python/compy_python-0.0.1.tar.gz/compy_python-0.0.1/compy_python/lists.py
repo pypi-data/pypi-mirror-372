4eacb424c8a114a68a4ae78ccb5a390cc211281e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 07:06:06 +0000

    Add package compy-python/0.0.1/compy_python-0.0.1.tar.gz

---
+++ packages/compy-python/compy_python-0.0.1.tar.gz/compy_python-0.0.1/compy_python/lists.py
@@ -0,0 +1,8 @@
+from typing import TypeVar
+
+# TODO: remove these TypeVar things because there is a better way to that now.
+T = TypeVar("T")
+
+
+def list_reserve(arr: list[T], size: int) -> None:
+    pass
