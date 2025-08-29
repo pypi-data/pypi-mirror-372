77b35b9703e9c0ced5131c92fd7cb7feff025f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:05:56 +0000

    Add package tket/0.12.6/tket-0.12.6-cp310-abi3-win32.whl

---
+++ packages/tket/tket-0.12.6-cp310-abi3-win32.whl/tket/extensions/__init__.py
@@ -0,0 +1,36 @@
+from tket_exts import (
+    opaque_bool,
+    debug,
+    guppy,
+    rotation,
+    futures,
+    qsystem,
+    qsystem_random,
+    qsystem_utils,
+    quantum,
+    result,
+    wasm,
+)
+
+# TODO: Remove once tket no longer supports tket-exts 0.10.*
+try:
+    from tket_exts import bool  # type: ignore[attr-defined]
+except ImportError:
+    bool = opaque_bool  # type: ignore[assignment]
+
+
+# TODO: Remove the deprecated `opaque_bool` export in a breaking change.
+__all__ = [
+    "debug",
+    "bool",
+    "opaque_bool",
+    "guppy",
+    "rotation",
+    "futures",
+    "qsystem",
+    "qsystem_random",
+    "qsystem_utils",
+    "quantum",
+    "result",
+    "wasm",
+]
