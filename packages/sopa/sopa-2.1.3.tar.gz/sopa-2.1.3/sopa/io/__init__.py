94f563c87f3a0d1e7e285189c2fc32ff982fdc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:15 +0000

    Add package sopa/2.1.3/sopa-2.1.3.tar.gz

---
+++ packages/sopa/sopa-2.1.3.tar.gz/sopa-2.1.3/sopa/io/__init__.py
@@ -0,0 +1,16 @@
+from .explorer import write
+from .standardize import write_standardized
+from .reader.cosmx import cosmx
+from .reader.merscope import merscope
+from .reader.xenium import xenium
+from .reader.macsima import macsima
+from .reader.phenocycler import phenocycler
+from .reader.hyperion import hyperion
+from .reader.utils import ome_tif
+from .reader.wsi import wsi, wsi_autoscale
+from .reader.generic import aicsimageio, bioio
+from .reader.visium_hd import visium_hd
+from .reader.molecular_cartography import molecular_cartography
+from .report import write_report
+from ..utils.data import blobs, toy_dataset
+from . import explorer
