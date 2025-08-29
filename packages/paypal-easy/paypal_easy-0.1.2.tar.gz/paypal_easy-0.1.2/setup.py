262fb8e713c89baeb32fd1dde31693902a4852a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 04:06:24 +0000

    Add package paypal-easy/0.1.2/paypal_easy-0.1.2.tar.gz

---
+++ packages/paypal-easy/paypal_easy-0.1.2.tar.gz/paypal_easy-0.1.2/setup.py
@@ -0,0 +1,42 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="paypal-easy",
+    version="0.1.2",
+    author="William Mabotja",
+    author_email="info@williammabotja.xyz",
+    description="A simplified wrapper for PayPal's Python Server SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doculearn/paypal-easy",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "paypal-server-sdk>=1.0.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov",
+            "black",
+            "flake8",
+        ],
+        "test": ["pytest>=6.0"]
+    },
+)
