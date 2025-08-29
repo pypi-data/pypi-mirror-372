e4d40d615854ce003b02ae8104a5e42f2302d9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:03 +0000

    Add package futhark-data/1.0.3/futhark-data-1.0.3.tar.gz

---
+++ packages/futhark-data/futhark-data-1.0.3.tar.gz/futhark-data-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(name='futhark-data',
+      version='1.0.3',
+      url='https://github.com/diku-dk/futhark-data-python',
+      license='ISC',
+      author='Troels Henriksen',
+      author_email='athas@sigkill.dk',
+      description='Reading and writing Futhark data files',
+      packages=['futhark_data'],
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: ISC License (ISCL)",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.9',
+      install_requires=[
+          'numpy'
+      ],
+      zip_safe=True)
