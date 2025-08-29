c990fddf82dffdb3998f7ec02956473444cd97a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 03:23:35 +0000

    Add package activity-browser/3.0.0b1007/activity_browser-3.0.0b1007.tar.gz

---
+++ packages/activity-browser/activity_browser-3.0.0b1007.tar.gz/activity_browser-3.0.0b1007/activity_browser/layouts/pages/__init__.py
@@ -0,0 +1,6 @@
+from .activity_details import ActivityDetailsPage
+from .welcome import WelcomePage
+from .calculation_setup import CalculationSetupPage
+from .impact_category_details import ImpactCategoryDetailsPage
+from .lca_results import LCAResultsPage
+from .parameters import ParametersPage
