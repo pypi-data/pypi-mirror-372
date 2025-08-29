3b52d52f1e111e23c41641c211208ca7ad1de8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 05:08:59 +0000

    Add package sdk-instagram/1.0.0/sdk_instagram-1.0.0-py3-none-any.whl

---
+++ packages/sdk-instagram/sdk_instagram-1.0.0-py3-none-any.whl/instagramapi/controllers/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'base_controller',
+    'users_controller',
+    'relationships_controller',
+    'media_controller',
+    'comments_controller',
+    'likes_controller',
+    'tags_controller',
+    'locations_controller',
+    'geographies_controller',
+]
