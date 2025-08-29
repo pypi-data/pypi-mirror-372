1aaea7384d7d86117e9e4d90ec6eba499f92e8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 04:06:41 +0000

    Add package scrutipy/0.1.11/scrutipy-0.1.11.tar.gz

---
+++ packages/scrutipy/scrutipy-0.1.11.tar.gz/scrutipy-0.1.11/src/main.rs
@@ -0,0 +1,15 @@
+pub mod rounding;
+pub mod sd_binary;
+pub mod utils;
+//use std::env;
+pub mod debit;
+pub mod closure;
+pub mod confusion;
+
+#[allow(unused_imports)]
+use crate::debit::debit_scalar;
+
+
+// use in future, commented out for the moment
+//env::set_var("RUST_BACKTRACE", "1");
+fn main() { }
