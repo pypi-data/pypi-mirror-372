37799fb48e990bb41228e75babd24ab14ba69477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:05:52 +0000

    Add package langflow-base/0.5.1/langflow_base-0.5.1-py3-none-any.whl

---
+++ packages/langflow-base/langflow_base-0.5.1-py3-none-any.whl/langflow/components/embeddings/__init__.py
@@ -0,0 +1,9 @@
+from .similarity import EmbeddingSimilarityComponent
+from .text_embedder import TextEmbedderComponent
+
+__all__ = [
+    "CloudflareWorkersAIEmbeddingsComponent",
+    "EmbeddingSimilarityComponent",
+    "MistralAIEmbeddingsComponent",
+    "TextEmbedderComponent",
+]
