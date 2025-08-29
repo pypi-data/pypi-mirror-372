4711c73e0b01d16631199f30ecef0b337b508f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:31 +0000

    Add package labelbox/7.2.0/labelbox-7.2.0-py3-none-any.whl

---
+++ packages/labelbox/labelbox-7.2.0-py3-none-any.whl/labelbox/data/annotation_types/__init__.py
@@ -0,0 +1,61 @@
+from .geometry import Line
+from .geometry import Point
+from .geometry import Mask
+from .geometry import Polygon
+from .geometry import Rectangle
+from .geometry import Geometry
+from .geometry import DocumentRectangle
+from .geometry import RectangleUnit
+
+from .annotation import ClassificationAnnotation
+from .annotation import ObjectAnnotation
+
+from .relationship import RelationshipAnnotation
+from .relationship import Relationship
+
+from .video import VideoClassificationAnnotation
+from .video import VideoObjectAnnotation
+from .video import MaskFrame
+from .video import MaskInstance
+from .video import VideoMaskAnnotation
+
+from .ner import ConversationEntity
+from .ner import DocumentEntity
+from .ner import DocumentTextSelection
+from .ner import TextEntity
+
+from .classification import Checklist
+from .classification import ClassificationAnswer
+from .classification import Radio
+from .classification import Text
+
+from .data import GenericDataRowData
+from .data import MaskData
+
+from .label import Label
+from .collection import LabelGenerator
+
+from .metrics import ScalarMetric
+from .metrics import ScalarMetricAggregation
+from .metrics import ConfusionMatrixMetric
+from .metrics import ConfusionMatrixAggregation
+from .metrics import ScalarMetricValue
+from .metrics import ConfusionMatrixMetricValue
+
+from .data.tiled_image import EPSG
+from .data.tiled_image import EPSGTransformer
+from .data.tiled_image import TiledBounds
+from .data.tiled_image import TiledImageData
+from .data.tiled_image import TileLayer
+
+from .llm_prompt_response.prompt import PromptText
+from .llm_prompt_response.prompt import PromptClassificationAnnotation
+
+from .mmc import (
+    MessageInfo,
+    OrderedMessageInfo,
+    MessageSingleSelectionTask,
+    MessageMultiSelectionTask,
+    MessageRankingTask,
+    MessageEvaluationTaskAnnotation,
+)
