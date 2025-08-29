aa6892fcc962d11a61517819c19dfa3d2f21c509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:05:56 +0000

    Add package foliantcontrib.confluence/0.6.22/foliantcontrib_confluence-0.6.22.tar.gz

---
+++ packages/foliantcontrib.confluence/foliantcontrib_confluence-0.6.22.tar.gz/foliantcontrib_confluence-0.6.22/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Confluence backend for Foliant documentation generator.'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.confluence',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='0.6.22',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    url='https://github.com/foliant-docs/foliantcontrib.confluence',
+    packages=['foliant.backends.confluence',
+              'foliant.preprocessors',
+              'foliant.preprocessors.confluence_final'],
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.8',
+        'atlassian-python-api==4.0.5', # version 4.0.7 causes errors, can be updated when new versions are released.
+        'foliantcontrib.utils>=1.0.2',
+        'foliantcontrib.flatten>=1.0.5',
+        'foliantcontrib.meta>=1.3.1',
+        'pyparsing',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
