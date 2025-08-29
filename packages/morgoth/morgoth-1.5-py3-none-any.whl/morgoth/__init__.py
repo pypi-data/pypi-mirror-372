e4d8e18dfc7441303f9d3a1f89bd4f0650124986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 05:06:58 +0000

    Add package morgoth/1.5/morgoth-1.5-py3-none-any.whl

---
+++ packages/morgoth/morgoth-1.5-py3-none-any.whl/morgoth/__init__.py
@@ -0,0 +1,6 @@
+from morgoth.morgoth import *
+from morgoth.multivariate_dt import *
+from morgoth.conformal_prediction import *
+from morgoth.similarity_test_train import *
+from morgoth.multi_rf_main import *
+__version__ = 1.2
