61908ef0cc77b700b6f0162dbdb17a4b725c9bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:05:52 +0000

    Add package reydb/1.1.38/reydb-1.1.38-py3-none-any.whl

---
+++ packages/reydb/reydb-1.1.38-py3-none-any.whl/reydb/rbase.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2025-07-18 23:36:56
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Base methods.
+"""
+
+
+from reykit.rbase import Base
+
+
+__all__ = (
+    'DatabaseBase',
+)
+
+
+class DatabaseBase(Base):
+    """
+    Database base type.
+    """
