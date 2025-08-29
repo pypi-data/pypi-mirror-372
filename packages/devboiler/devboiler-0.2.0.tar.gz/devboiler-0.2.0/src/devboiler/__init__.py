7a1db49ab5c4b0e5d43402300727202f1701485f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:48 +0000

    Add package devboiler/0.2.0/devboiler-0.2.0.tar.gz

---
+++ packages/devboiler/devboiler-0.2.0.tar.gz/devboiler-0.2.0/src/devboiler/__init__.py
@@ -0,0 +1,15 @@
+from .api import (
+    create_python_class,
+    create_html_page,
+    create_react_component,
+    create_project,
+)
+
+__all__ = [
+    "create_python_class",
+    "create_html_page",
+    "create_react_component",
+    "create_project",
+]
+
+
