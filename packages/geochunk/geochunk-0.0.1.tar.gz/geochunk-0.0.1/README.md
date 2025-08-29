58f58d5fae0512b92345b2fa79f4f6f8b80a773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:50 +0000

    Add package geochunk/0.0.1/geochunk-0.0.1.tar.gz

---
+++ packages/geochunk/geochunk-0.0.1.tar.gz/geochunk-0.0.1/README.md
@@ -0,0 +1,7 @@
+# geochunk
+
+Unified access protocol for geospatiotemporal data sources.
+
+**Status: Early development - API will change**
+
+Coming soon.
---
+++ packages/geochunk/geochunk-0.0.1.tar.gz/geochunk-0.0.1/geochunk/__init__.py
@@ -0,0 +1,7 @@
+"""Unified access protocol for geospatiotemporal data sources - early development version."""
+
+__version__ = "0.0.1"
+
+def placeholder():
+    """Placeholder function for initial release."""
+    return f"geochunk v{__version__} - unified data access coming soon"
