9299fe2f5e839f620b097467e133ec7bd981d4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:07:19 +0000

    Add package ckanext-essopendata-theme/0.1.2/ckanext_essopendata_theme-0.1.2.tar.gz

---
+++ packages/ckanext-essopendata-theme/ckanext_essopendata_theme-0.1.2.tar.gz/ckanext_essopendata_theme-0.1.2/setup.py
@@ -0,0 +1,24 @@
+# from setuptools import setup
+#
+# setup(
+#     name = "essopendata_theme",
+#     version = "0.0.1",
+#     author = "Coopdevs",
+#     author_email = "info@coopdevs.org",
+#     description = "CKAN ESS Opendata",
+#     long_description = "Configuració CKAN de l'open data de l'ESS",
+#     long_description_content_type = "text/markdown",
+#     classifiers = [
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: OS Independent",
+#     ],
+#     entry_points='''
+#     [ckan.plugins]
+#     essopendata_theme=ckanext.essopendata_theme.plugin:ESSOpendataThemePlugin
+#    ''',
+# )
+#
+
+
+from setuptools import setup
+setup()
