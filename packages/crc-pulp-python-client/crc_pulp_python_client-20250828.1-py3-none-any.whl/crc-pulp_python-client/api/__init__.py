07907f1b7a3967ec2590e8d807c822365fb1152b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 04:06:34 +0000

    Add package crc-pulp-python-client/20250828.1/crc_pulp_python_client-20250828.1-py3-none-any.whl

---
+++ packages/crc-pulp-python-client/crc_pulp_python_client-20250828.1-py3-none-any.whl/crc-pulp_python-client/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from crc-pulp_python-client.api.content_packages_api import ContentPackagesApi
+from crc-pulp_python-client.api.distributions_pypi_api import DistributionsPypiApi
+from crc-pulp_python-client.api.publications_pypi_api import PublicationsPypiApi
+from crc-pulp_python-client.api.pypi_api import PypiApi
+from crc-pulp_python-client.api.pypi_legacy_api import PypiLegacyApi
+from crc-pulp_python-client.api.pypi_metadata_api import PypiMetadataApi
+from crc-pulp_python-client.api.pypi_simple_api import PypiSimpleApi
+from crc-pulp_python-client.api.remotes_python_api import RemotesPythonApi
+from crc-pulp_python-client.api.repositories_python_api import RepositoriesPythonApi
+from crc-pulp_python-client.api.repositories_python_versions_api import RepositoriesPythonVersionsApi
+
