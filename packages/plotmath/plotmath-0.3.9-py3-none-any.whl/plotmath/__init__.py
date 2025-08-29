924b38d825e2244c2db2e78d1cc92b5ad4e43e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:21 +0000

    Add package plotmath/0.3.9/plotmath-0.3.9-py3-none-any.whl

---
+++ packages/plotmath/plotmath-0.3.9-py3-none-any.whl/plotmath/__init__.py
@@ -0,0 +1,27 @@
+from .plotmath import (
+    plot,
+    show,
+    savefig,
+    histogram,
+    plot_polygon,
+    polygon,
+    multiplot,
+    gca,
+    COLORS,
+    make_bar,
+    annotate,
+)
+
+__all__ = [
+    "plot",
+    "savefig",
+    "show",
+    "histogram",
+    "plot_polygon",
+    "polygon",
+    "multiplot",
+    "gca",
+    "COLORS",
+    "make_bar",
+    "annotate",
+]
