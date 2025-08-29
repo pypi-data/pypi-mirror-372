036b962088a1cd2edf61cb600d5a35fc8d2f039d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:45 +0000

    Add package bleak-esphome/3.2.0/bleak_esphome-3.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/bleak-esphome/bleak_esphome-3.2.0-cp311-cp311-win_amd64.whl/bleak_esphome/backend/scanner.py
@@ -0,0 +1,75 @@
+"""Bluetooth scanner for esphome."""
+
+from __future__ import annotations
+
+from aioesphomeapi import (
+    BluetoothLEAdvertisement,
+    BluetoothLERawAdvertisementsResponse,
+    BluetoothScannerMode,
+    BluetoothScannerStateResponse,
+)
+from bluetooth_data_tools import (
+    int_to_bluetooth_address,
+)
+from bluetooth_data_tools import (
+    monotonic_time_coarse as MONOTONIC_TIME,
+)
+from habluetooth import BluetoothScanningMode
+from habluetooth.base_scanner import BaseHaRemoteScanner
+
+
+class ESPHomeScanner(BaseHaRemoteScanner):
+    """Scanner for esphome."""
+
+    __slots__ = ()
+
+    def async_update_scanner_state(self, state: BluetoothScannerStateResponse) -> None:
+        """Update the scanner state."""
+        if state.mode == BluetoothScannerMode.ACTIVE:
+            self.set_requested_mode(BluetoothScanningMode.ACTIVE)
+            self.set_current_mode(BluetoothScanningMode.ACTIVE)
+        elif state.mode == BluetoothScannerMode.PASSIVE:
+            self.set_requested_mode(BluetoothScanningMode.PASSIVE)
+            self.set_current_mode(BluetoothScanningMode.PASSIVE)
+        else:
+            self.set_current_mode(None)
+            self.set_requested_mode(None)
+
+    def async_on_advertisement(self, adv: BluetoothLEAdvertisement) -> None:
+        """Call the registered callback."""
+        # The mac address is a uint64, but we need a string
+        self._async_on_advertisement(
+            int_to_bluetooth_address(adv.address),
+            adv.rssi,
+            adv.name,
+            adv.service_uuids,
+            adv.service_data,
+            adv.manufacturer_data,
+            None,
+            {"address_type": adv.address_type},
+            MONOTONIC_TIME(),
+        )
+
+    def async_on_raw_advertisements(
+        self, raw: BluetoothLERawAdvertisementsResponse
+    ) -> None:
+        """Call the registered callback."""
+        now = MONOTONIC_TIME()
+        advertisements = raw.advertisements
+        # We avoid __iter__ on the protobuf object because
+        # the the protobuf library has an expensive internal
+        # debug logging when it reaches the end of a repeated field.
+        # https://github.com/Bluetooth-Devices/bleak-esphome/pull/90
+        # To work around this we use a for loop to iterate over
+        # the repeated field since `PyUpb_RepeatedContainer_Subscript`
+        # does not trigger the debug logging.
+        on_raw = self._async_on_raw_advertisement
+        for i in range(len(advertisements)):
+            adv = advertisements[i]
+            on_raw(
+                int_to_bluetooth_address(adv.address),
+                adv.rssi,
+                adv.data,
+                {"address_type": adv.address_type},
+                now,
+            )
