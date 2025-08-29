c41ad80b4e52ce9fcb67b07cf9d3e3220387fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:07:22 +0000

    Add package ckanext-essopendata-theme/0.1.3/ckanext_essopendata_theme-0.1.3.tar.gz

---
+++ packages/ckanext-essopendata-theme/ckanext_essopendata_theme-0.1.3.tar.gz/ckanext_essopendata_theme-0.1.3/ckanext/essopendata_theme/plugin.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+'''plugin.py
+
+'''
+from ckan.common import CKANConfig
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class ESSOpendataThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
+
+    '''
+    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config: CKANConfig):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        # 'templates' is the path to the templates dir, relative to this
+        # plugin.py file.
+        toolkit.add_template_directory(config, 'templates')
+
+        # Add this plugin's public dir to CKAN's extra_public_paths, so
+        # that CKAN will use this plugin's custom static files.
+        toolkit.add_public_directory(config, 'public')
+
+
