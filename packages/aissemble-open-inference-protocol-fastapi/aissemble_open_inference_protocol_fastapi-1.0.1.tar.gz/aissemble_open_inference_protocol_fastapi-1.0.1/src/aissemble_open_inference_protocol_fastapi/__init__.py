f3f269735680c45e95c51819ce7aa79a5227c900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:03 +0000

    Add package aissemble-open-inference-protocol-fastapi/1.0.1/aissemble_open_inference_protocol_fastapi-1.0.1.tar.gz

---
+++ packages/aissemble-open-inference-protocol-fastapi/aissemble_open_inference_protocol_fastapi-1.0.1.tar.gz/aissemble_open_inference_protocol_fastapi-1.0.1/src/aissemble_open_inference_protocol_fastapi/__init__.py
@@ -0,0 +1,9 @@
+###
+# #%L
+# aiSSEMBLE::Open Inference Protocol::FastAPI
+# %%
+# Copyright (C) 2024 Booz Allen Hamilton Inc.
+# %%
+# This software package is licensed under the Booz Allen Public License. All Rights Reserved.
+# #L%
+###
