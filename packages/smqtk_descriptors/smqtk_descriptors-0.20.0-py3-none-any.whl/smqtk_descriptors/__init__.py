3247d735ad43b6acf623aaf347c4eda8f21bba1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 07:06:00 +0000

    Add package smqtk_descriptors/0.20.0/smqtk_descriptors-0.20.0-py3-none-any.whl

---
+++ packages/smqtk_descriptors/smqtk_descriptors-0.20.0-py3-none-any.whl/smqtk_descriptors/__init__.py
@@ -0,0 +1,4 @@
+from .descriptor_element_factory import DescriptorElementFactory  # noqa: F401
+from .interfaces.descriptor_element import DescriptorElement  # noqa: F401
+from .interfaces.descriptor_set import DescriptorSet  # noqa: F401
+from .interfaces.descriptor_generator import DescriptorGenerator  # noqa: F401
