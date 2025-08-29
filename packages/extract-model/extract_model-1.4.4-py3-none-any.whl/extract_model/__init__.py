337f1b1d41fdf33d4f10a56eb4fe4f66a69560fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:51 +0000

    Add package extract-model/1.4.4/extract_model-1.4.4-py3-none-any.whl

---
+++ packages/extract-model/extract_model-1.4.4-py3-none-any.whl/extract_model/__init__.py
@@ -0,0 +1,22 @@
+"""
+Select model output using xarray and `isel` or interpolation through xarray or xesmf.
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+import cf_xarray as cfxr  # noqa: F401
+
+import extract_model.accessor  # noqa: F401
+import extract_model.preprocessing  # noqa: F401
+import extract_model.utils  # noqa: F401
+
+from .extract_model import sel2d, sel2dcf, select, selZ  # noqa: F401
+from .preprocessing import preprocess
+from .utils import filter, guess_model_type, order, sub_bbox, sub_grid  # noqa: F401
+
+
+try:
+    __version__ = version("extract_model")
+except PackageNotFoundError:
+    # package is not installed
+    pass
