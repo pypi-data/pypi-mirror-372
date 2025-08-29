a57c82ba4ee90990d1ff6bd6a77fe5775564656b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 05:06:58 +0000

    Add package morgoth/1.5/morgoth-1.5.tar.gz

---
+++ packages/morgoth/morgoth-1.5.tar.gz/morgoth-1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='morgoth',
+    version='1.5',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy',
+                      'scipy', 'scikit-learn', 'multiprocess', 'fireducks; sys_platform=="linux"'],
+    author='Lisa-Marie Rolli',
+    author_email='lisa-marie.rolli@uni-saarland.de',
+    description='Interpretable and reliable multivariate random forest for simultaneous classification and regression',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/volkamerlab/MORGOTH.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
