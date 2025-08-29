cbfe15a83f56ab5d7a7a6fca3ee6903ffb3f42f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:55 +0000

    Add package korean-romanizer/0.28.0/korean_romanizer-0.28.0-py3-none-any.whl

---
+++ packages/korean-romanizer/korean_romanizer-0.28.0-py3-none-any.whl/korean_romanizer/romanizer.py
@@ -0,0 +1,140 @@
+import re
+
+from korean_romanizer.syllable import (
+    Syllable,
+    unicode_compatible_consonants,
+    unicode_initial,
+)
+from korean_romanizer.pronouncer import Pronouncer
+
+'''
+### Transcribing vowels ###
+'''
+
+vowel = {
+    # 단모음 monophthongs
+    'ㅏ' : 'a',
+    'ㅓ' : 'eo',
+    'ㅗ' : 'o',
+    'ㅜ' : 'u',
+    'ㅡ' : 'eu',
+    'ㅣ' : 'i',
+    'ㅐ' : 'ae',
+    'ㅔ' : 'e',
+    'ㅚ' : 'oe',
+    'ㅟ' : 'wi',
+    
+    # 이중모음 diphthongs
+    'ㅑ' : 'ya',
+    'ㅕ' : 'yeo',
+    'ㅛ' : 'yo',
+    'ㅠ' : 'yu',
+    'ㅒ' : 'yae',
+    'ㅖ' : 'ye',
+    'ㅘ' : 'wa',
+    'ㅙ' : 'wae',
+    'ㅝ' : 'wo',
+    'ㅞ' : 'we',
+    'ㅢ' : 'ui', # [붙임 1] ‘ㅢ’는 ‘ㅣ’로 소리 나더라도 ‘ui’로 적는다.
+}
+
+'''
+### Transcribing consonants ###
+
+Consonants are defined in separate dicts, choseong and jongseong,
+for some characters are pronounced differently depending on 
+its position in the syllable.
+
+e.g. ㄱ, ㄷ, ㅂ, ㄹ are (g, d, b, r) in onset,
+                  but (k, t, p, l) in coda.
+e.g. ㅇ is a null sound when placed in onset, but becomes [ng] in coda.
+'''
+
+# 초성 Choseong (Syllable Onset)
+onset = {
+    # 파열음 stops/plosives
+    'ᄀ' : 'g',
+    'ᄁ' : 'kk',
+    'ᄏ' : 'k',
+    'ᄃ' : 'd',
+    'ᄄ' : 'tt',
+    'ᄐ' : 't',
+    'ᄇ' : 'b',
+    'ᄈ' : 'pp',
+    'ᄑ' : 'p',
+    # 파찰음 affricates
+    'ᄌ' : 'j',
+    'ᄍ' : 'jj',
+    'ᄎ' : 'ch',
+    # 마찰음 fricatives
+    'ᄉ' : 's',
+    'ᄊ' : 'ss',
+    'ᄒ' : 'h',
+    # 비음 nasals
+    'ᄂ' : 'n',
+    'ᄆ' : 'm',
+    # 유음 liquids
+    'ᄅ' : 'r',
+    # Null sound
+    'ᄋ' : '',
+}
+
+'''
+종성 Jongseong (Syllable Coda)
+
+"The 7 Jongseongs (7종성)"
+Only the seven consonants below may appear in coda position
+'''
+
+coda = {
+    # 파열음 stops/plosives
+    'ᆨ' : 'k',
+    'ᆮ' : 't',
+    'ᆸ' : 'p',
+    # 비음 nasals
+    'ᆫ' : 'n',
+    'ᆼ' : 'ng',
+    'ᆷ' : 'm',
+    # 유음 liquids
+    'ᆯ' : 'l',
+    
+    None: '',
+}
+
+# Compatibility jamo (e.g. ㄱ, ㄴ) do not appear as part of a full syllable.
+# Map them to their onset romanization so single jamo can be transliterated.
+compat_onset = {
+    comp: onset[unicode_initial[i]]
+    for i, comp in enumerate(unicode_compatible_consonants)
+}
+    
+class Romanizer(object):
+    def __init__(self, text):
+        self.text = text
+
+    def romanize(self):
+        pronounced = Pronouncer(self.text).pronounced
+        hangul = r"[가-힣ㄱ-ㅣ]"
+        _romanized = ""
+        for char in pronounced:
+            if (re.match(hangul, char)):
+                s = Syllable(char)
+
+                if not s.medial and not s.final:
+                    # s is NOT a full syllable (e.g. characters)
+                    if char in vowel:
+                        _romanized += vowel[char]
+                    elif char in onset:
+                        _romanized += onset[char]
+                    elif char in compat_onset:
+                        _romanized += compat_onset[char]
+                    else:
+                        _romanized += char
+                else:
+                    # s is a full syllable
+                    _romanized += onset[s.initial] + vowel[s.medial] + coda[s.final]
+
+            else:
+                _romanized += char
+
+        return _romanized
