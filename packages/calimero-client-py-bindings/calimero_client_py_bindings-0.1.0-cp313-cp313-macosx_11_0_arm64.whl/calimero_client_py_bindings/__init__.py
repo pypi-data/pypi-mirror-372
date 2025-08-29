606f160f375ab3d1c250db2b018b156ef86d0bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 03:20:29 +0000

    Add package calimero-client-py-bindings/0.1.0/calimero_client_py_bindings-0.1.0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/calimero-client-py-bindings/calimero_client_py_bindings-0.1.0-cp313-cp313-macosx_11_0_arm64.whl/calimero_client_py_bindings/__init__.py
@@ -0,0 +1,5 @@
+from .calimero_client_py_bindings import *
+
+__doc__ = calimero_client_py_bindings.__doc__
+if hasattr(calimero_client_py_bindings, "__all__"):
+    __all__ = calimero_client_py_bindings.__all__
