fdcec95f76653e96815d3f467f446e9dc9000e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:53 +0000

    Add package cfncli/0.4.0/cfncli-0.4.0.tar.gz

---
+++ packages/cfncli/cfncli-0.4.0.tar.gz/cfncli-0.4.0/setup.py
@@ -0,0 +1,73 @@
+# -*- encoding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+__author__ = 'andyfase'
+__date__ = '29-July-2025'
+
+# here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = open('README.md').read()
+
+install_requires = [
+    'backoff>2.2',
+    'boto3>1.39',
+    'botocore>1.39',
+    'click>8',
+    'click-completion>0.5',
+    'colorama>0.4',
+    'jsonschema>4',
+    'pyyaml>6',
+    's3transfer>=0.13',
+    'black>=25.1',
+    'deepmerge>=2.0'
+]
+
+setup(
+    # Project Name
+    name='cfncli',
+
+    # Version and description
+    version='0.4.0',
+    description='CloudFormation CLI Wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # Author details
+    author='Fase, Andy',
+    author_email='andyfase@gmail.com',
+
+    # Project home
+    url='https://github.com/andyfase/cfncli',
+
+    # License detail
+    license='MIT',
+
+    # Classification and Keywords
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Utilities',
+    ],
+    keywords='aws cfn cli cloudformation changeset sam serverless',
+    python_requires='>=3.7',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    package_data={
+        'cfncli.config': ['*.json', '*.yaml']
+    },
+    install_requires=install_requires,
+    entry_points='''
+    [console_scripts]
+    cfn-cli=cfncli.__main__:main
+    cfn=cfncli.__main__:main
+    ''',
+)
