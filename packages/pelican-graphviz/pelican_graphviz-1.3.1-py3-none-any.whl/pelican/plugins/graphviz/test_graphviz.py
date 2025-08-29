88891a13f485316c65c0bbf692355663830b7099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:06:14 +0000

    Add package pelican-graphviz/1.3.1/pelican_graphviz-1.3.1-py3-none-any.whl

---
+++ packages/pelican-graphviz/pelican_graphviz-1.3.1-py3-none-any.whl/pelican/plugins/graphviz/test_graphviz.py
@@ -0,0 +1,251 @@
+"""Unit testing suite for the Graphviz plugin."""
+
+# Copyright (C) 2015, 2021, 2023, 2025  Rafael Laboissière <rafael@laboissiere.net>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+import os
+import re
+from shutil import rmtree
+from tempfile import mkdtemp
+import unittest
+
+from pelican import Pelican
+from pelican.settings import read_settings
+
+from . import graphviz
+
+TEST_FILE_STEM = "test"
+TEST_DIR_PREFIX = "pelicantests."
+GRAPHVIZ_RE = (
+    r'<{0} class="{1}"><img alt="{2}" '
+    r'src="data:image/svg\+xml;base64,[0-9a-zA-Z+=]+"></{0}>'
+)
+
+GRAPHVIZ_RE_XML = r'<svg width="\d+pt" height="\d+pt"'
+
+
+class TestGraphviz(unittest.TestCase):
+    """Class for testing the URL output of the Graphviz plugin."""
+
+    def setUp(
+        self,
+        block_start="..graphviz",
+        image_class="graphviz",
+        html_element="div",
+        alt_text="GRAPH",
+        compress=True,
+        options=None,
+        expected_html_element=None,
+        expected_image_class=None,
+        expected_alt_text=None,
+        digraph_id="G",
+    ):
+        """Set up the test environment."""
+        # Set the paths for the input (content) and output (html) files
+        self.output_path = mkdtemp(prefix=TEST_DIR_PREFIX)
+        self.content_path = mkdtemp(prefix=TEST_DIR_PREFIX)
+
+        # Configuration setting for the Pelican process
+        settings = {
+            "PATH": self.content_path,
+            "OUTPUT_PATH": self.output_path,
+            "PLUGINS": [graphviz],
+            "CACHE_CONTENT": False,
+            "GRAPHVIZ_HTML_ELEMENT": html_element,
+            "GRAPHVIZ_BLOCK_START": block_start,
+            "GRAPHVIZ_IMAGE_CLASS": image_class,
+            "GRAPHVIZ_ALT_TEXT": alt_text,
+            "GRAPHVIZ_COMPRESS": compress,
+        }
+
+        # Store the image_class and the html_element in self, since they will
+        # be needed in the test_output method defined below
+        self.image_class = image_class
+        self.html_element = html_element
+        self.alt_text = alt_text
+
+        # Get default expected values
+        if not expected_image_class:
+            self.expected_image_class = self.image_class
+        else:
+            self.expected_image_class = expected_image_class
+        if not expected_html_element:
+            self.expected_html_element = self.html_element
+        else:
+            self.expected_html_element = expected_html_element
+        if not expected_alt_text:
+            self.expected_alt_text = digraph_id if digraph_id else alt_text
+        else:
+            self.expected_alt_text = expected_alt_text
+
+        # Create the article file
+        with open(os.path.join(self.content_path, f"{TEST_FILE_STEM}.md"), "w") as fid:
+            # Write header
+            fid.write(f"Title: {TEST_FILE_STEM}\nDate: 1970-01-01\n")
+            # Write Graphviz block
+            fid.write(
+                f"""
+{block_start}{f" [{options}] " if options else " "}dot
+digraph {digraph_id if digraph_id else ""} {{
+  graph [rankdir = LR];
+  Hello -> World
+}}
+"""
+            )
+
+        # Run the Pelican instance
+        self.settings = read_settings(override=settings)
+        pelican = Pelican(settings=self.settings)
+        pelican.run()
+
+    def tearDown(self):
+        """Tidy up the test environment."""
+        rmtree(self.output_path)
+        rmtree(self.content_path)
+
+    def test_output(self):
+        """Test for default values of the configuration variables."""
+        # Open the output HTML file
+        with open(os.path.join(self.output_path, f"{TEST_FILE_STEM}.html")) as fid:
+            content = fid.read()
+            found = False
+            # Iterate over the lines and look for the HTML element corresponding
+            # to the generated Graphviz figure
+            for line in content.splitlines():
+                if self.settings["GRAPHVIZ_COMPRESS"]:
+                    if re.search(
+                        GRAPHVIZ_RE.format(
+                            self.expected_html_element,
+                            self.expected_image_class,
+                            self.expected_alt_text,
+                        ),
+                        line,
+                    ):
+                        found = True
+                        break
+                elif re.search(GRAPHVIZ_RE_XML, line):
+                    found = True
+                    break
+            assert found, content
+
+
+class TestGraphvizHtmlElement(TestGraphviz):
+    """Class for exercising the configuration variable GRAPHVIZ_HTML_ELEMENT."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(self, html_element="span")
+
+    def test_output(self):
+        """Test for GRAPHVIZ_HTML_ELEMENT setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizBlockStart(TestGraphviz):
+    """Class for exercising the configuration variable GRAPHVIZ_BLOCK_START."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(self, block_start="==foobar")
+
+    def test_output(self):
+        """Test for GRAPHVIZ_BLOCK_START setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizImageClass(TestGraphviz):
+    """Class for exercising configuration variable GRAPHVIZ_IMAGE_CLASS."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(self, image_class="foo")
+
+    def test_output(self):
+        """Test for GRAPHVIZ_IMAGE_CLASS setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizImageNoCompress(TestGraphviz):
+    """Class for exercising configuration variable GRAPHVIZ_COMPRESS."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(self, compress=False)
+
+    def test_output(self):
+        """Test for GRAPHVIZ_COMPRESS setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizLocallyOverrideConfiguration(TestGraphviz):
+    """Class for exercising the override of a configuration variable."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(
+            self,
+            html_element="div",
+            options="html-element=span",
+            expected_html_element="span",
+        )
+
+    def test_output(self):
+        """Test for overrind the configuration."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizAltText(TestGraphviz):
+    """Class for exercising configuration variable GRAPHVIZ_ALT_TEXT."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(self, alt_text="foo")
+
+    def test_output(self):
+        """Test for GRAPHVIZ_IMAGE_CLASS setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizAltTextWithoutID(TestGraphviz):
+    """Class for testing the case where the Graphviz element has no id."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        TestGraphviz.setUp(
+            self,
+            digraph_id=None,
+            alt_text="foo",
+        )
+
+    def test_output(self):
+        """Test for GRAPHVIZ_IMAGE_CLASS setting."""
+        TestGraphviz.test_output(self)
+
+
+class TestGraphvizAltTextViaOption(TestGraphviz):
+    """Class for testing the alternative text given via the alt-text option."""
+
+    def setUp(self):
+        """Initialize the configuration."""
+        text = "A wonderful graph"
+        TestGraphviz.setUp(
+            self,
+            options=f'alt-text="{text}"',
+            expected_alt_text=text,
+        )
+
+    def test_output(self):
+        """Test for GRAPHVIZ_IMAGE_CLASS setting."""
+        TestGraphviz.test_output(self)
