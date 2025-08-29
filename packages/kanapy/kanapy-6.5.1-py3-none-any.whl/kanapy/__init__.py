501456393fe0bedcd258e6e18fe69930207cb97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:06 +0000

    Add package kanapy/6.5.1/kanapy-6.5.1-py3-none-any.whl

---
+++ packages/kanapy/kanapy-6.5.1-py3-none-any.whl/kanapy/__init__.py
@@ -0,0 +1,50 @@
+"""
+Top-level package for kanapy with default ORIX backend
+
+use pip install kanapy-mtex for version based on MTEX library, depending on Matlab
+
+ Copyright (C) 2025  by {__author__} ICAMS / Ruhr University Bochum, Germany
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+import logging
+from importlib.metadata import version
+from importlib.resources import files
+
+# Re-export shared core and texture modules for convenience
+#if os.path.abspath('.core') not in sys.path:
+#    sys.path.insert(0, os.path.abspath('.core'))
+#if os.path.abspath('./texture') not in sys.path:
+#    sys.path.insert(0, os.path.abspath('./texture'))
+from .core import Microstructure, set_stats, pickle2microstructure, import_voxels,\
+     import_stats, write_stats, start, triple_surf
+
+from .texture import EBSDmap, ODF, createOriset, createOrisetRandom, \
+    get_ipf_colors, plot_inverse_pole_figure, plot_inverse_pole_figure_density
+
+log_level = 20  # Levels for logging: 10: DEBUG, 20: INFO, 30: WARNING, 40: ERROR
+logging.basicConfig(level=log_level)  # set log level
+poly_scale = 1.6
+__author__ = ('Mahesh R.G Prasad, Abhishek Biswas, Golsa Tolooei Eshlaghi, Ronak Shoghi, '
+              'Napat Vajragupta, Yousef Rezek, Hrushikesh Uday Bhimavarapu, Alexander Hartmaier')
+__email__ = 'alexander.hartmaier@rub.de'
+__version__ = version('kanapy')
+__backend__ = "orix"
+__all__ = ["Microstructure", "set_stats", "pickle2microstructure", "import_voxels",
+           "import_stats", "write_stats", "start", "EBSDmap", "ODF",  "createOriset",
+           "createOrisetRandom", "get_ipf_colors", "plot_inverse_pole_figure",
+           "plot_inverse_pole_figure_density",]
