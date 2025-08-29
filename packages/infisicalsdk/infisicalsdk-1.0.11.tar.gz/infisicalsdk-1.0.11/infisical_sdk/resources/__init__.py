11cec4c8941d78b0985ba21c5906bddc90984ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:06:35 +0000

    Add package infisicalsdk/1.0.11/infisicalsdk-1.0.11.tar.gz

---
+++ packages/infisicalsdk/infisicalsdk-1.0.11.tar.gz/infisicalsdk-1.0.11/infisical_sdk/resources/__init__.py
@@ -0,0 +1,3 @@
+from .secrets import V3RawSecrets
+from .kms import KMS
+from .auth import Auth
---
+++ packages/infisicalsdk/infisicalsdk-1.0.11.tar.gz/infisicalsdk-1.0.11/infisical_sdk/resources/auth.py
@@ -0,0 +1,11 @@
+from infisical_sdk.infisical_requests import InfisicalRequests
+from infisical_sdk.resources.auth_methods import AWSAuth
+from infisical_sdk.resources.auth_methods import UniversalAuth
+from infisical_sdk.resources.auth_methods import OidcAuth
+from typing import Callable
+class Auth:
+    def __init__(self, requests: InfisicalRequests, setToken: Callable[[str], None]):
+        self.requests = requests
+        self.aws_auth = AWSAuth(requests, setToken)
+        self.universal_auth = UniversalAuth(requests, setToken)
+        self.oidc_auth = OidcAuth(requests, setToken)
