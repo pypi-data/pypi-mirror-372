9be143810a524184b28ab1ed80036cef694d7a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:11 +0000

    Add package T3ChatApi/1.2/t3chatapi-1.2.tar.gz

---
+++ packages/T3ChatApi/t3chatapi-1.2.tar.gz/t3chatapi-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import secrets
+
+setup(
+    name=secrets.name,
+    version='1.2',
+    packages=find_packages(),
+    install_requires=secrets.packages,
+    description=secrets.description,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        f'Operating System :: {secrets.opsy}',
+    ],
+    python_requires='>=3.6',
+)
