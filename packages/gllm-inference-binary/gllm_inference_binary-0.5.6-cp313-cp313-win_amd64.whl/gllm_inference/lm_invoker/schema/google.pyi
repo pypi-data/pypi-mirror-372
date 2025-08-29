619975ab4de8b3c7f2a6b7408f13817b70d2c61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 16:06:18 +0000

    Add package gllm-inference-binary/0.5.6/gllm_inference_binary-0.5.6-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.6-cp313-cp313-win_amd64.whl/gllm_inference/lm_invoker/schema/google.pyi
@@ -0,0 +1,21 @@
+class Key:
+    """Defines valid keys in Google."""
+    ARGS: str
+    CONTENT: str
+    FINISH_MESSAGE: str
+    FINISH_REASON: str
+    FUNCTION: str
+    FUNCTION_CALL: str
+    NAME: str
+    SYSTEM_INSTRUCTION: str
+    THINKING_CONFIG: str
+    TOOLS: str
+    RESPONSE_SCHEMA: str
+    RESPONSE_MIME_TYPE: str
+    VERTEXAI: str
+
+class InputType:
+    """Defines valid input types in Google."""
+    APPLICATION_JSON: str
+    MODEL: str
+    USER: str
