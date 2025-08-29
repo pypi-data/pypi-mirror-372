f3e65c6cfc47c75ba56c759b5d96966f14d26ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:56 +0000

    Add package crc-pulp-file-client/20250828.3/crc_pulp_file_client-20250828.3-py3-none-any.whl

---
+++ packages/crc-pulp-file-client/crc_pulp_file_client-20250828.3-py3-none-any.whl/crc-pulp_file-client/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from crc-pulp_file-client.api.acs_file_api import AcsFileApi
+from crc-pulp_file-client.api.content_files_api import ContentFilesApi
+from crc-pulp_file-client.api.distributions_file_api import DistributionsFileApi
+from crc-pulp_file-client.api.publications_file_api import PublicationsFileApi
+from crc-pulp_file-client.api.remotes_file_api import RemotesFileApi
+from crc-pulp_file-client.api.repositories_file_api import RepositoriesFileApi
+from crc-pulp_file-client.api.repositories_file_versions_api import RepositoriesFileVersionsApi
+
