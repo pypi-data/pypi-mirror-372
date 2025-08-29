f16dd00c0491ede5da70e217f55e991244d52d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 03:20:39 +0000

    Add package apscale-installer/1.0.0/apscale_installer-1.0.0.tar.gz

---
+++ packages/apscale-installer/apscale_installer-1.0.0.tar.gz/apscale_installer-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apscale_installer",
+    version="1.0.0",
+    author="Till-Hendrik Macher",
+    author_email="macher@uni-trier.de",
+    description="Advanced Pipeline for Simple yet Comprehensive AnaLysEs of DNA metabarcoding data - Conda installer (workaround)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/apscale_installer/",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+                    'Requests==2.32.3'
+                      ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        "console_scripts": [
+            "apscale_installer = apscale_installer.__main__:main",
+        ]
+    },
+)
+
