8d4f3ea470a49db27a79482d6750b67cf5410678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 04:06:04 +0000

    Add package tala/28.0.1/tala-28.0.1.tar.gz

---
+++ packages/tala/tala-28.0.1.tar.gz/tala-28.0.1/tala/utils/user_rates_constants.py
@@ -0,0 +1,17 @@
+OFFER_ID = "OfferID"
+TS_DIALOGUE_ID = "TSDialogueID"
+
+OFFER_AUTHOR_ID = "UserID"
+AUTHOR_ID = "AuthorUserID"
+
+OFFER_QUOTA = "OfferQuota"
+AUTHOR_QUOTA = "AuthorQuota"
+
+NUM_CALLS = "NumCalls"
+CALLS_LAST_PERIOD = "CallsLastPeriod"
+ROW_KEY = "RowKey"
+
+PARTITION_KEY = "PartitionKey"
+HANDLER_DATA = "HandlerData"
+BUDDY_GENERATOR_DATA = "BuddyGeneratorData"
+USER_RATES = "UserRates"
