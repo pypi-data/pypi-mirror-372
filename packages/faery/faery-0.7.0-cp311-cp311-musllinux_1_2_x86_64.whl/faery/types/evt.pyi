628413f84e40cde15ee4303345ef34bb9829f379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:50 +0000

    Add package faery/0.7.0/faery-0.7.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/faery/faery-0.7.0-cp311-cp311-musllinux_1_2_x86_64.whl/faery/types/evt.pyi
@@ -0,0 +1,46 @@
+import pathlib
+import types
+import typing
+
+import numpy
+
+class Decoder:
+    version: typing.Literal["evt2", "evt2.1", "evt3"]
+    dimensions: tuple[int, int]
+
+    def __init__(
+        self,
+        path: typing.Union[pathlib.Path, str],
+        dimensions_fallback: typing.Optional[tuple[int, int]],
+        version_fallback: typing.Optional[typing.Literal["evt2", "evt2.1", "evt3"]],
+    ): ...
+    def __enter__(self) -> Decoder: ...
+    def __exit__(
+        self,
+        exception_type: typing.Optional[typing.Type[BaseException]],
+        value: typing.Optional[BaseException],
+        traceback: typing.Optional[types.TracebackType],
+    ) -> bool: ...
+    def __iter__(self) -> Decoder: ...
+    def __next__(self) -> dict[typing.Literal["events", "triggers"], numpy.ndarray]: ...
+
+class Encoder:
+    def __init__(
+        self,
+        path: typing.Union[pathlib.Path, str],
+        version: typing.Literal["evt2", "evt2.1", "evt3"],
+        zero_t0: bool,
+        dimensions: tuple[int, int],
+        enforce_monotonic: bool,
+    ): ...
+    def __enter__(self) -> Encoder: ...
+    def __exit__(
+        self,
+        exception_type: typing.Optional[typing.Type[BaseException]],
+        value: typing.Optional[BaseException],
+        traceback: typing.Optional[types.TracebackType],
+    ) -> bool: ...
+    def t0(self) -> typing.Optional[int]: ...
+    def write(
+        self, packet: dict[typing.Literal["events", "triggers"], numpy.ndarray]
+    ): ...
