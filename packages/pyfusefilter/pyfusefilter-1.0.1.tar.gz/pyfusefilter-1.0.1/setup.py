7f942e27df7fe2233ea6b861e1dcc1646119e9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:07:18 +0000

    Add package pyfusefilter/1.0.1/pyfusefilter-1.0.1.tar.gz

---
+++ packages/pyfusefilter/pyfusefilter-1.0.1.tar.gz/pyfusefilter-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension, find_packages
+import os
+
+setup(
+    name="pyfusefilter",
+    version="1.0.1",
+    description="Python bindings for C implementation of xorfilter",
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    author="Amey Narkhede",
+    author_email="ameynarkhede02@gmail.com",
+    url="https://github.com/glitzflitz/pyfusefilter",
+    license="Apache 2.0",
+    python_requires=">=3.0",
+    packages=find_packages(),
+    ext_package="pyfusefilter",
+    install_requires=["cffi","xxhash"],
+    cffi_modules=["pyfusefilter/ffibuild.py:ffi"],
+)
+
