d8701472df31339477610b18acfdd6e455001a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:06:30 +0000

    Add package file-detector/0.2.5/file_detector-0.2.5-py3-none-manylinux2014_x86_64.whl

---
+++ packages/file-detector/file_detector-0.2.5-py3-none-manylinux2014_x86_64.whl/file_detector/__init__.py
@@ -0,0 +1,11 @@
+from .detect import detect_file, detect_buffer, is_text, FileCategory, FileSubtype, Kind
+
+__version__ = "0.2.5"
+__all__ = [
+    "detect_file",
+    "detect_buffer",
+    "is_text",
+    "FileCategory",
+    "FileSubtype",
+    "Kind",
+]
