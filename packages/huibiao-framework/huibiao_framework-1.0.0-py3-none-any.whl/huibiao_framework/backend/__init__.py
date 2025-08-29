35defe8c68f26d60783b189fc24c88c5469706d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:07:08 +0000

    Add package huibiao-framework/1.0.0/huibiao_framework-1.0.0-py3-none-any.whl

---
+++ packages/huibiao-framework/huibiao_framework-1.0.0-py3-none-any.whl/huibiao_framework/backend/__init__.py
@@ -0,0 +1,12 @@
+from .status_code import BasicStatusCode, CommonStatusCode
+from .exception import BasicException, BasicCommonException
+from .vo import BaseRespVo
+
+
+__all__ = [
+    "BasicStatusCode",
+    "CommonStatusCode",
+    "BasicException",
+    "BasicCommonException",
+    "BaseRespVo",
+]
