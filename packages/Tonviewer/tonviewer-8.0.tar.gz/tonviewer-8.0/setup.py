078a0872397d808909041ff0641f51d1523cba13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 15:07:17 +0000

    Add package Tonviewer/8.0/tonviewer-8.0.tar.gz

---
+++ packages/Tonviewer/tonviewer-8.0.tar.gz/tonviewer-8.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    lng_description = fh.read()
+
+setuptools.setup(
+    name="Tonviewer",
+    version="8.0",
+    author="deep",
+    author_email="asyncpy@proton.me",
+    license="MIT",
+    description="TON Crypto Info Scraper is a Python library that allows you to fetch ( real-time balance of wallet , transactions , price ) data without needing any APIs.",
+    long_description=lng_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
