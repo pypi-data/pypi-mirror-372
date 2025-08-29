4406079480285a4258cb5ccba8a1a07ec01f5dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:05:54 +0000

    Add package dbmini/0.1.3/dbmini-0.1.3.tar.gz

---
+++ packages/dbmini/dbmini-0.1.3.tar.gz/dbmini-0.1.3/README.md
@@ -0,0 +1,23 @@
+# dbmini
+
+`dbmini` is a lightweight Python library to quickly connect to **MySQL**, **SQLite**, and **MongoDB** databases.  
+It is designed to be simple, flexible, and easy to integrate into any project.
+
+---
+
+## 🔹 Features
+
+- Connect to **MySQL** databases.
+- Connect to **SQLite** databases (local or in-memory).
+- Connect to **MongoDB** databases (e.g., hosted on filess.io or local).
+- Minimal configuration — credentials are passed as parameters.
+- Interactive database operations can be done easily in Python scripts.
+
+---
+
+## 🔹 Installation
+
+Install via PyPI:
+
+```bash
+pip install dbmini
---
+++ packages/dbmini/dbmini-0.1.3.tar.gz/dbmini-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dbmini",  # must be unique on PyPI
+    version="0.1.3",
+    author="Prathamesh Patil ",
+    author_email="prathameshpatil0545@gmail.com",
+    description="A lightweight DB connector supporting MySQL, SQLite, and MongoDB",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/prathamesh9669/dbmini",
+    packages=find_packages(),
+    install_requires=[
+        "mysql-connector-python",
+        "pymongo",
+        "python-dotenv",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
