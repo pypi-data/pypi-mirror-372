86e16c0f5d3282410f304c1ff47b85f2daa5537a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 04:06:03 +0000

    Add package amusic/2.4.0/amusic-2.4.0.tar.gz

---
+++ packages/amusic/amusic-2.4.0.tar.gz/amusic-2.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amusic',
+    version='2.4.0', # if it 3.0.0 or onwards that means its actually stable an im happy with it
+    packages=find_packages(),
+    install_requires=[
+        'mido',
+        'pygame'
+    ],
+    author='SolamateanTehCoder',
+    description='A MIDI visualizer with VFX.',
+    package_data={'amusic': ['*.sf2']},
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SolamateanTehCoder/amusic',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
