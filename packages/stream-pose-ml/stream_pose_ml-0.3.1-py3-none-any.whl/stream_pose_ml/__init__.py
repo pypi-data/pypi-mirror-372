e596e41a5aff3259fd1c55eecd7429fdb9d5657b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:06:36 +0000

    Add package stream-pose-ml/0.3.1/stream_pose_ml-0.3.1-py3-none-any.whl

---
+++ packages/stream-pose-ml/stream_pose_ml-0.3.1-py3-none-any.whl/stream_pose_ml/__init__.py
@@ -0,0 +1,8 @@
+"""
+Stream Pose ML module.
+"""
+
+# Make top-level components accessible directly from the module
+from stream_pose_ml.stream_pose_client import StreamPoseClient as StreamPoseClient
+
+from .ml_flow_client import MLFlowClient as MLFlowClient
---
+++ packages/stream-pose-ml/stream_pose_ml-0.3.1-py3-none-any.whl/stream_pose_ml/tests/conftest.py
@@ -0,0 +1,6 @@
+"""
+Conftest file for module tests.
+"""
+
+
+# The project root directory is already in the Python path via pip install -e .
