66f75778029fa37368189e1def0ef4d5fc1ec96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 14:07:05 +0000

    Add package payi/0.1.0a113/payi-0.1.0a113.tar.gz

---
+++ packages/payi/payi-0.1.0a113.tar.gz/payi-0.1.0a113/src/payi/types/limits/tag_remove_params.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Required, TypedDict
+
+__all__ = ["TagRemoveParams"]
+
+
+class TagRemoveParams(TypedDict, total=False):
+    limit_tags: Required[List[str]]
+    """List of limit tags"""
