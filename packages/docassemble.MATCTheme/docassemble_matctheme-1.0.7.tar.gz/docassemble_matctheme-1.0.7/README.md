bf84b25a489d7428a89057bc2e9a975f038bf31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:57 +0000

    Add package docassemble.MATCTheme/1.0.7/docassemble_matctheme-1.0.7.tar.gz

---
+++ packages/docassemble.MATCTheme/docassemble_matctheme-1.0.7.tar.gz/docassemble_matctheme-1.0.7/README.md
@@ -0,0 +1,8 @@
+# docassemble.MATCTheme
+
+A Docassemble Bootstrap theme for the Massachusetts Trial Court
+
+## Author
+
+Sam Glover, sam.glover@suffolk.edu
+
---
+++ packages/docassemble.MATCTheme/docassemble_matctheme-1.0.7.tar.gz/docassemble_matctheme-1.0.7/docassemble/MATCTheme/data/static/menu.js
@@ -0,0 +1,4 @@
+$(document).on('daPageLoad', function() {
+  let label = val('navbar_dropdown_menu_label');
+  $('.navbar-nav .dropdown-toggle').text('Menu');
+});
