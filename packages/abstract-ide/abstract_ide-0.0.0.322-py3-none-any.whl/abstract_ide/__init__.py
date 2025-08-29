bcd9453f26832fb1b8bd3272e74c03bca17d78c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:49 +0000

    Add package abstract-ide/0.0.0.322/abstract_ide-0.0.0.322-py3-none-any.whl

---
+++ packages/abstract-ide/abstract_ide-0.0.0.322-py3-none-any.whl/abstract_ide/__init__.py
@@ -0,0 +1,7 @@
+import os
+from .main import *
+from .imports import startConsole
+from .logTab import logTab,startLogConsole
+def startAbstractIde():
+    startConsole(abstractIde)
+
---
+++ packages/abstract-ide/abstract_ide-0.0.0.322-py3-none-any.whl/abstract_ide/test_gui.py
@@ -0,0 +1,2 @@
+from consoles import *
+startReactIdleConsole()
