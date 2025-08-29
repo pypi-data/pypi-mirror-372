0985ce2e8b0880fc2a8beb3d369364c9650825cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 06:06:38 +0000

    Add package dbmini/0.2.0/dbmini-0.2.0-py3-none-any.whl

---
+++ packages/dbmini/dbmini-0.2.0-py3-none-any.whl/db/db.py
@@ -0,0 +1,33 @@
+# dblite.py
+
+import mysql.connector
+import sqlite3
+from pymongo import MongoClient
+
+
+def mysql(user, password, host, port):
+    conn = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password
+    )
+    print("✅ MySQL connection established.")
+    cur = conn.cursor()
+    return conn, cur
+
+
+def sqlite(db_path):
+    conn = sqlite3.connect(db_path)
+    print("✅ SQLite connection established.")
+    cur = conn.cursor()
+    return conn, cur
+
+
+def mongo(mongo_url,mongo_db):
+    client = MongoClient(mongo_url)
+    db = client[mongo_db]
+    print("✅ MongoDB (filess.io) connection established.")
+    return client, db
+
+
