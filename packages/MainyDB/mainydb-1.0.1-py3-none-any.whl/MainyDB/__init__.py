845a6a8f2e8132f09e4c15ecd08eb9f8ba8960d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-29 08:06:50 +0000

    Add package MainyDB/1.0.1/mainydb-1.0.1-py3-none-any.whl

---
+++ packages/MainyDB/mainydb-1.0.1-py3-none-any.whl/MainyDB/__init__.py
@@ -0,0 +1,9 @@
+from .core import MainyDB, MongoClient
+from .core import Collection, Database
+from .core import ObjectId
+
+__version__ = '1.0.1'
+__author__ = 'devid'
+
+# Easter egg: Hidden message
+__easter_egg__ = "You've found the secret message! MainyDB is watching you... always."
