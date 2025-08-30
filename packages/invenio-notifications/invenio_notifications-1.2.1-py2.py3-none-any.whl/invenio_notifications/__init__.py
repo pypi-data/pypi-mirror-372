3d43518128a1388aa8c4bf089ded10b29cae2ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:17:03 +0000

    Add package invenio-notifications/1.2.1/invenio_notifications-1.2.1-py2.py3-none-any.whl

---
+++ packages/invenio-notifications/invenio_notifications-1.2.1-py2.py3-none-any.whl/invenio_notifications/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023-2025 CERN.
+# Copyright (C) 2023-2024 Graz University of Technology.
+#
+# Invenio-Notifications is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio module for notifications support."""
+
+from .ext import InvenioNotifications
+from .proxies import current_notifications, current_notifications_manager
+
+__version__ = "1.2.1"
+
+__all__ = (
+    "__version__",
+    "current_notifications",
+    "current_notifications_manager",
+    "InvenioNotifications",
+)
