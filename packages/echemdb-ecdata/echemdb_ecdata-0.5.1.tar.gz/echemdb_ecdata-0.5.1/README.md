073e3d2b43296bb869754b8c01f6913810fdba90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:05:52 +0000

    Add package echemdb-ecdata/0.5.1/echemdb_ecdata-0.5.1.tar.gz

---
+++ packages/echemdb-ecdata/echemdb_ecdata-0.5.1.tar.gz/echemdb_ecdata-0.5.1/README.md
@@ -0,0 +1,62 @@
+# Electrochemistry-data
+
+This repository contains data used for the creation of entries on [echemdb.org](https://wwww.echemdb.org/cv).
+The data consist of SVG, YAML and BIB files which are converted into frictionless based
+[`unitpackages`](https://echemdb.github.io/unitpackage/) using [`svgdigitizer`](https://echemdb.github.io/svgdigitizer/).
+All input YAML files are validated against the [echemdb-metadata schema](https://github.com/echemdb/metadata-schema).
+
+## Contributing
+
+The preparation and of the files and the extraction of the data from a PDF source is
+described [here](https://echemdb.github.io/svgdigitizer/workflow.html).
+
+## Accessing Data
+
+***Direct download***
+
+The resulting data can be downloaded as a ZIP from the release section.
+
+Alternatively you can use the [`unitpackages`](https://echemdb.github.io/unitpackage/) interface
+(see [`unitpackages` installation instructions](https://echemdb.github.io/unitpackage/installaton.html)).
+
+***From the API***
+
+Install the latest version of the module.
+
+```sh
+pip install git+https://github.com/echemdb/electrochemistry-data.git
+```
+
+In your preferred Python environment retrieve the URL via
+
+```py
+from echemdb_ecdata.url import ECHEMDB_DATABASE_URL
+ECHEMDB_DATABASE_URL
+```
+
+## Development
+
+Clone the repository
+
+```sh
+git clone git@github.com:echemdb/electrochemistry-data.git
+cd electrochemistry-data
+```
+
+Install dependencies via conda (or mamba).
+
+```sh
+conda create -f environment.yaml
+conda activate echemdb-data
+```
+
+***Create data locally***
+
+Run the makefile in the `data` folder to create data locally.
+
+```sh
+cd data
+make
+```
+
+To use multiple core use `make -j4` (in this case 4 cores).
---
+++ packages/echemdb-ecdata/echemdb_ecdata-0.5.1.tar.gz/echemdb_ecdata-0.5.1/echemdb_ecdata/url.py
@@ -0,0 +1,48 @@
+r"""
+URL to a ZIP containing the latest echemdb electrochemistry data.
+"""
+
+# ********************************************************************
+#  This file is part of electrochemistry-data.
+#
+#        Copyright (C) 2024 Albert Engstfeld
+#
+#  electrochemistry-data is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  electrochemistry-data is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with electrochemistry-data. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
+
+import os
+
+
+def get_echemdb_database_url(version="0.5.1"):
+    """Returns a URL to an asset of a certain version of the echemdb electrochemistry-data.
+
+    EXAMPLES:
+
+    By default a URL is returned with the most recent version::
+
+        >>> from echemdb_ecdata.url import get_echemdb_database_url
+        >>> get_echemdb_database_url()
+        'https://github.com/echemdb/electrochemistry-data/releases/download/0.5.1/data-0.5.1.zip'
+
+    A URL with a specific version can be created::
+
+        >>> from echemdb_ecdata.url import get_echemdb_database_url
+        >>> get_echemdb_database_url(version="0.5.1")
+        'https://github.com/echemdb/electrochemistry-data/releases/download/0.5.1/data-0.5.1.zip'
+
+    """
+    return os.environ.get(
+        "ECHEMDB_DATABASE_URL",
+        f"https://github.com/echemdb/electrochemistry-data/releases/download/{version}/data-{version}.zip",
+    )
