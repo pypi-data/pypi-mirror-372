6a38c6bb207977a9fb46d14707fda4643ff2a31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:07:16 +0000

    Add package hacka/0.5.0/hacka-0.5.0.tar.gz

---
+++ packages/hacka/hacka-0.5.0.tar.gz/hacka-0.5.0/src/hacka/__init__.py
@@ -0,0 +1,11 @@
+from . import pod, gamemaster, player
+
+# HackaGames Net Elements
+Pod= pod.Pod
+
+# Game masters and players
+Player= player.Player
+PlayerShell= player.PlayerShell
+
+SequentialGameMaster= gamemaster.SequentialGameMaster
+SimultaneousMaster= gamemaster.SimultaneousGameMaster
