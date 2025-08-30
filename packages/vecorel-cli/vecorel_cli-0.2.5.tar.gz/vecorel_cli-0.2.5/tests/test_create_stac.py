10528c37d0cb447ed84d2299efe08454cec45578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:17:27 +0000

    Add package vecorel-cli/0.2.5/vecorel_cli-0.2.5.tar.gz

---
+++ packages/vecorel-cli/vecorel_cli-0.2.5.tar.gz/vecorel_cli-0.2.5/tests/test_create_stac.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import pytest
+
+from vecorel_cli.create_stac import CreateStacCollection
+from vecorel_cli.vecorel.util import load_file
+
+files = [("de-sh.parquet", "collection-parquet.json"), ("de-sh.json", "collection-json.json")]
+
+
+@pytest.mark.parametrize("file, expected_file", files)
+def test_create_stac_collection(tmp_folder: Path, file: str, expected_file: str):
+    base = Path("tests/data-files/stac/")
+    source = base / file
+    expected_file = base / expected_file
+    out_file = tmp_folder / "collection.json"
+
+    gj = CreateStacCollection()
+    gj.create_cli(source, out_file, temporal_property="determination_datetime", indent=2)
+
+    assert out_file.exists()
+
+    created_file = load_file(out_file)
+    expected = load_file(expected_file)
+
+    assert isinstance(created_file, dict), "Created file is not a valid JSON dict"
+
+    # Cater for environment differences in paths
+    del expected["assets"]["data"]["href"]
+    assert "assets" in created_file
+    assert "data" in created_file["assets"]
+    assert "href" in created_file["assets"]["data"]
+    del created_file["assets"]["data"]["href"]
+    # Cater for floating point differences
+    del expected["extent"]["spatial"]["bbox"]
+    assert "extent" in created_file
+    assert "spatial" in created_file["extent"]
+    assert "bbox" in created_file["extent"]["spatial"]
+    del created_file["extent"]["spatial"]["bbox"]
+
+    assert created_file == expected
