f7e3db60172e480c401e813ace710c996612d7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:42 +0000

    Add package podigeeconnector/0.4.0/podigeeconnector-0.4.0-py3-none-any.whl

---
+++ packages/podigeeconnector/podigeeconnector-0.4.0-py3-none-any.whl/podigeeconnector/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package allows you to fetch data from the inofficial Podigee Podcast API.
+The API is not documented and may change at any time. Use at your own risk.
+"""
+
+from .connector import PodigeeConnector
+
+__all__ = ["PodigeeConnector"]
