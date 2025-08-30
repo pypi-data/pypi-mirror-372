6baad29957c04d229e266ed2d38853359f5c4006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:29 +0000

    Add package pyglowx/1.5.0/pyglowx-1.5.0.tar.gz

---
+++ packages/pyglowx/pyglowx-1.5.0.tar.gz/pyglowx-1.5.0/pyglow/pyglow.py
@@ -0,0 +1,21 @@
+from .parser import PyGlowParser
+from .mapping import ANSI_RESET
+
+
+class Glow:
+
+    @staticmethod
+    def parse(text: str) -> str:
+        return PyGlowParser.parse(text)
+
+    @staticmethod
+    def print(text: str):
+        print(Glow.parse(text))
+
+    @staticmethod
+    def printc(text: str):
+        print(f"{text}{ANSI_RESET}")
+
+    @staticmethod
+    def prints(text: str, style: str):
+        Glow.print(f"[{style}]{text}[/]")
