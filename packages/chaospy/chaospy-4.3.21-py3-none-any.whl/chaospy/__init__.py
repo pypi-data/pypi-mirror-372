312309d42c8a88e05efe96f00bd0a51fe7fa1837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:20 +0000

    Add package chaospy/4.3.21/chaospy-4.3.21-py3-none-any.whl

---
+++ packages/chaospy/chaospy-4.3.21-py3-none-any.whl/chaospy/__init__.py
@@ -0,0 +1,63 @@
+"""
+Uncertainty Quantification Toolbox
+==================================
+
+This module contains tools for performing uncertainty quantification of models.
+"""
+import logging
+import os
+
+try:  # Python < 3.10
+    from importlib_metadata import version
+except ImportError:
+    from importlib.metadata import version
+
+from numpoly import *
+
+import chaospy.descriptives
+import chaospy.distributions
+import chaospy.expansion
+import chaospy.spectral
+import chaospy.quadrature
+import chaospy.saltelli
+import chaospy.regression
+import chaospy.recurrence
+
+from chaospy.distributions import *
+from chaospy.expansion import *
+from chaospy.spectral import *
+from chaospy.quadrature import *
+from chaospy.saltelli import *
+from chaospy.descriptives import *
+from chaospy.regression import *
+from chaospy.external import *
+from chaospy.recurrence import *
+
+__version__ = version("chaospy")
+
+
+def configure_logging():
+    """Configure logging for Chaospy."""
+    logpath = os.environ.get("CHAOSPY_LOGPATH", os.devnull)
+    logging.basicConfig(level=logging.DEBUG, filename=logpath, filemode="w")
+    streamer = logging.StreamHandler()
+    loglevel = (
+        logging.DEBUG if os.environ.get("CHAOSPY_DEBUG", "") == "1" else logging.WARNING
+    )
+    streamer.setLevel(loglevel)
+
+    logger = logging.getLogger("chaospy")
+    logger.addHandler(streamer)
+    logger = logging.getLogger("numpoly")
+    logger.addHandler(streamer)
+
+
+configure_logging()
+
+
+class StochasticallyDependentError(ValueError):
+    """Error related to stochastically dependent variables."""
+
+
+class UnsupportedFeature(NotImplementedError):
+    """Error when dependencies are not correctly handled."""
