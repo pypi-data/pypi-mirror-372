2c0fbf5d2bd54427a9798b45924cfec78ccde637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:03 +0000

    Add package ORD/1.8.6/ord-1.8.6.tar.gz

---
+++ packages/ORD/ord-1.8.6.tar.gz/ord-1.8.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from io import open
+
+
+def read(filename):
+    """Прочитаем наш README.md для того, чтобы установить большое описание."""
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+setup(
+    name='ORD',
+    version='1.8.6',
+    author='Vladimir Smirnov',
+    author_email='volodya@brandshop.ru',
+    description='Module for working with the ATOL cash register driver',
+    url='https://brandshop.ru',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
