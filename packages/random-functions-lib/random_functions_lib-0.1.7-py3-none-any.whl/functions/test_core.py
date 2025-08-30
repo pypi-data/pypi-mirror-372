13a1049c5072de2e470fcc849291b5a105baa5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:07:51 +0000

    Add package random-functions-lib/0.1.7/random_functions_lib-0.1.7-py3-none-any.whl

---
+++ packages/random-functions-lib/random_functions_lib-0.1.7-py3-none-any.whl/functions/test_core.py
@@ -0,0 +1,12 @@
+import functions
+from functions import scramble
+
+name = "cove"
+name1 =functions.encode(name,"singeli")
+print(name1)
+print(functions.decode(name1,"singeli"))
+print(functions.invert_caps("HeLLo"))
+print(functions.reverse("hello"))
+print(functions.scramble("hello"))
+print(functions.flip_a_coin())
+print(functions.shout("hello"))
