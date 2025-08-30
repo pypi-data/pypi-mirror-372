80721ee3c3cfecef7badee430a3ebd9b8678bdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:17:19 +0000

    Add package open-api-apimatic/1.0.0/open_api_apimatic-1.0.0-py3-none-any.whl

---
+++ packages/open-api-apimatic/open_api_apimatic-1.0.0-py3-none-any.whl/openai/controllers/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'base_controller',
+    'models_controller',
+    'completions_controller',
+    'edits_controller',
+    'images_controller',
+    'embeddings_controller',
+    'files_controller',
+    'fine_tunes_controller',
+    'moderations_controller',
+    'engines_controller',
+]
