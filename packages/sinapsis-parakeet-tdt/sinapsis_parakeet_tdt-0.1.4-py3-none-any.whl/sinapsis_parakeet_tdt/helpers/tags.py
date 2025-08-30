4fa705106afebe5c1b4c791eb00c627431ca21d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:05 +0000

    Add package sinapsis-parakeet-tdt/0.1.4/sinapsis_parakeet_tdt-0.1.4-py3-none-any.whl

---
+++ packages/sinapsis-parakeet-tdt/sinapsis_parakeet_tdt-0.1.4-py3-none-any.whl/sinapsis_parakeet_tdt/helpers/tags.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Tags(Enum):
+    AUDIO = "audio"
+    SPEECH = "speech"
+    SPEECH_RECOGNITION = "speech_recognition"
+    PARAKEET_TDT = "parakeet_tdt"
+    SPEECH_TO_TEXT = "speech_to_text"
+    TRANSCRIPTION = "transcription"
