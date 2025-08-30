4899e9eaefd6b4b0bd39ae75e401946bb9cec90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:05:55 +0000

    Add package pytntprog/2.0.0/pytntprog-2.0.0.tar.gz

---
+++ packages/pytntprog/pytntprog-2.0.0.tar.gz/pytntprog-2.0.0/README.md
@@ -0,0 +1,45 @@
+# :tv: pytntprog
+
+pytntprog displays the program of tnt tv in France
+
+
+# 💫 demo
+
+![demo](https://user-images.githubusercontent.com/45128847/137587565-c2dc81ef-95ca-4312-89ae-e9085ac964c7.gif)
+
+# 🚀 How use **pytntprog**
+
+- ``pytntprog`` display the tnt programm for the current day with id index
+- ``pytntprog -h`` show the help message
+- ``pytntprog -i [id]`` display the details of the programm with id
+- ``pytntprog -c`` display current programm
+- ``pytntprog -l [INT]`` filter programm with minimal length in minutes
+- ``pytntprog -a`` display all tnt programm (current day by default)
+- ``pytntprog -f "TF1"`` display the tnt programm for the current day with filter TF1
+- ``pytntprog -f "Stade 2" "France 3"`` display the tnt programm for the current day with filter Stade 2 + France 3
+- ``pytntprog -F "sport"`` display the tnt programm for the current day with agressive filter "sport" (description and resume)
+- ``pytntprog -C`` or ``pytntprog --chache`` download the distant xml file event if the cache is not finished (24h by default)
+- ``pytntprog -u`` to update pytntprog
+
+If you add ``--nocolor/-n`` option, you disable the colors in sysout
+
+# :gear: Install
+
+See [this page](INSTALL.md)
+
+# :construction_worker: Contribution
+
+See [this page](CONTRIBUTING.md)
+
+# :package: Changelog
+
+
+See [this page](CHANGELOG.md)
+
+# :pencil: License
+
+MIT License
+
+Copyright (c) 2021 [thib1984](https://github.com/thib1984)
+
+See [this page](LICENSE.txt) for details
---
+++ packages/pytntprog/pytntprog-2.0.0.tar.gz/pytntprog-2.0.0/pytntprog/args.py
@@ -0,0 +1,84 @@
+"""
+pygitscrum argparse gestion
+"""
+
+import argparse
+import sys
+
+
+def compute_args():
+    """
+    check args and return them
+    """
+    my_parser = argparse.ArgumentParser(
+        description="pytntprog displays the program of tnt tv in France",
+        epilog="""
+        Full documentation at: <https://github.com/thib1984/pytntprog>.
+        Report bugs to <https://github.com/thib1984/pytntprog/issues>.
+        MIT Licence.
+        Copyright (c) 2021 thib1984.
+        This is free software: you are free to change and redistribute it.
+        There is NO WARRANTY, to the extent permitted by law.
+        Written by thib1984.""",
+    )
+    my_parser.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="update pytntprog",
+    )
+    my_parser.add_argument(
+        "-i",
+        "--id",
+        type=str,
+        help="show full details for id programm",
+    )
+    my_parser.add_argument(
+        "-l",
+        "--length",
+        type=int,
+        default=0,
+        help="duree minimale en minute",
+    )           
+    my_parser.add_argument(
+        "-f",
+        "--filter",
+        action="store",
+        nargs="+",
+        type=str,
+        help="cumultative filter the results",
+    )
+    my_parser.add_argument(
+        "-F",
+        "--Filter",
+        action="store",
+        nargs="+",
+        type=str,
+        help="cumultative filter-full the results",
+    )    
+    my_parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        help="display all days results (current day by default)",
+    )        
+    my_parser.add_argument(
+        "-C",
+        "--cache",
+        action="store_true",
+        help="download the xml file even if the cache is not finished (24 hours by default)",
+    ) 
+    my_parser.add_argument(
+        "-c",
+        "--current",
+        action="store_true",
+        help="current programm",
+    )
+    my_parser.add_argument(
+        "-n",
+        "--nocolor",
+        action="store_true",
+        help="disable color in sysout",
+    )                 
+    args = my_parser.parse_args()
+    return args
