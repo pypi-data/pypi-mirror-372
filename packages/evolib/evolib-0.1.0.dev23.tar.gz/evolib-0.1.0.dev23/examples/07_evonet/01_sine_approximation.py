779f46c7692224205869366b1d43b85a1240f818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:07:22 +0000

    Add package evolib/0.1.0.dev23/evolib-0.1.0.dev23.tar.gz

---
+++ packages/evolib/evolib-0.1.0.dev23.tar.gz/evolib-0.1.0.dev23/examples/07_evonet/01_sine_approximation.py
@@ -0,0 +1,56 @@
+"""Approximating sin(x) using a feedforward network defined via EvoNet."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from evolib import Indiv, Pop, mse_loss, save_combined_net_plot
+
+# Define target function
+X_RAW = np.linspace(0, 2 * np.pi, 100)
+X_NORM = (X_RAW - np.pi) / np.pi
+Y_TRUE = np.sin(X_RAW)
+
+
+# Fitness function
+def evonet_fitness(indiv: Indiv) -> None:
+    predictions = []
+    net = indiv.para["nnet"]
+
+    for x_norm in X_NORM:
+        output = net.calc([x_norm])
+        predictions.append(output[0])
+
+    indiv.fitness = mse_loss(Y_TRUE, np.array(predictions))
+
+
+def on_improvement(pop: Pop, gen: int) -> None:
+    indiv = pop.best()
+    net = indiv.para["nnet"].net
+    y_pred = np.array([net.calc([x])[0] for x in X_NORM])
+
+    save_combined_net_plot(net, X_RAW, Y_TRUE, y_pred, f"01_frames/gen_{gen:04d}.png")
+
+
+def on_end(pop: Pop) -> None:
+    # Visualize result
+    y_best = [pop.best().para["nnet"].calc([x])[0] for x in X_NORM]
+
+    plt.plot(X_RAW, Y_TRUE, label="Target: sin(x)")
+    plt.plot(X_RAW, y_best, label="Best Approximation", linestyle="--")
+
+    plt.title("EvoNet Fit to sin(x)")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+# Evolution setup
+pop = Pop(
+    config_path="configs/01_sine_approximation.yaml", fitness_function=evonet_fitness
+)
+
+# Evolution loop
+pop.run(on_improvement=on_improvement, on_end=on_end)
