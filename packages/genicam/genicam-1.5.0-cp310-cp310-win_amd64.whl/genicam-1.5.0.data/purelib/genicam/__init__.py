206f096a30a10faae6cb761f476481f3b2d5ad77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:14 +0000

    Add package genicam/1.5.0/genicam-1.5.0-cp310-cp310-win_amd64.whl

---
+++ packages/genicam/genicam-1.5.0-cp310-cp310-win_amd64.whl/genicam-1.5.0.data/purelib/genicam/__init__.py
@@ -0,0 +1,11 @@
+# Do not modify. This is an auto-generated file.
+
+components = ['1.5.0']
+
+if int('0'):
+    # In general, this release should be a patch for the Python Bindings
+    # that is created for an urgent case.
+    components.append('dev' + '7041')
+
+__version__ = '.'.join(components)
+del components
