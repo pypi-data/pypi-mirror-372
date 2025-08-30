2971d7fc1bb46b36674236f79e93f19bfb80a20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:36 +0000

    Add package langchain-google-bigtable/0.8.0/langchain_google_bigtable-0.8.0-py3-none-any.whl

---
+++ packages/langchain-google-bigtable/langchain_google_bigtable-0.8.0-py3-none-any.whl/langchain_google_bigtable/__init__.py
@@ -0,0 +1,63 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from .async_key_value_store import AsyncBigtableByteStore
+from .async_vector_store import (
+    AsyncBigtableVectorStore,
+    ColumnConfig,
+    DistanceStrategy,
+    QueryParameters,
+    VectorDataType,
+    VectorMetadataMapping,
+)
+from .chat_message_history import (
+    BigtableChatMessageHistory,
+    create_chat_history_table,
+    init_chat_history_table,
+)
+from .engine import BigtableEngine
+from .key_value_store import BigtableByteStore, init_key_value_store_table
+from .loader import (
+    BigtableLoader,
+    BigtableSaver,
+    Encoding,
+    MetadataMapping,
+    init_document_table,
+)
+from .vector_store import BigtableVectorStore, init_vector_store_table
+from .version import __version__
+
+__all__ = [
+    "BigtableChatMessageHistory",
+    "create_chat_history_table",
+    "init_document_table",
+    "init_chat_history_table",
+    "init_key_value_store_table",
+    "init_vector_store_table",
+    "BigtableByteStore",
+    "AsyncBigtableByteStore",
+    "BigtableVectorStore",
+    "AsyncBigtableVectorStore",
+    "DistanceStrategy",
+    "VectorDataType",
+    "ColumnConfig",
+    "VectorMetadataMapping",
+    "BigtableEngine",
+    "BigtableLoader",
+    "BigtableSaver",
+    "MetadataMapping",
+    "Encoding",
+    "__version__",
+]
