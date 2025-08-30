30f43daf26cc01efff398ca45e498573760dfcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 06:06:05 +0000

    Add package mcp-server-mikrotik/0.2.0.post21/mcp_server_mikrotik-0.2.0.post21-py3-none-any.whl

---
+++ packages/mcp-server-mikrotik/mcp_server_mikrotik-0.2.0.post21-py3-none-any.whl/mcp_mikrotik/settings/configuration.py
@@ -0,0 +1,10 @@
+DEFAULT_MIKROTIK_HOST = "127.0.0.1"  
+DEFAULT_MIKROTIK_USER = "admin"        
+DEFAULT_MIKROTIK_PASS = ""    
+
+mikrotik_config = {
+    "host": DEFAULT_MIKROTIK_HOST,
+    "username": DEFAULT_MIKROTIK_USER,
+    "password": DEFAULT_MIKROTIK_PASS,
+    "port": 22
+}
