e069ff6c064ab562a5fe3b9cab25432eaf81a6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:48 +0000

    Add package piopiy-ai/0.1.0/piopiy_ai-0.1.0-py3-none-any.whl

---
+++ packages/piopiy-ai/piopiy_ai-0.1.0-py3-none-any.whl/piopiy/config.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2024–2025, TeleCMI
+# SPDX-License-Identifier: BSD 2-Clause License
+
+# Put your signaling URL here (no trailing slash)
+#SIGNALING_URL = "https://signaling.piopiy.ai"
+SIGNALING_URL = "http://192.168.0.139:3001"
