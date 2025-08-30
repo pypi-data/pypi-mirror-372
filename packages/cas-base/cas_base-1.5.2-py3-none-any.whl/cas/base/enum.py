eead0117040b35d28af14d9f59696e0457cad0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:14 +0000

    Add package cas-base/1.5.2/cas_base-1.5.2-py3-none-any.whl

---
+++ packages/cas-base/cas_base-1.5.2-py3-none-any.whl/cas/base/enum.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ConnectType(Enum):
+    # 应用
+    APP = 1
+    # 机器人
+    BOT = 2
+    # 远程监控
+    RC = 3
+    # 服务(数据源等)
+    SVC = 4
+    # 木马
+    TRO = 5
+    # 服务器
+    SVR = 6
