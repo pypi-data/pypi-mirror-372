1c38e3d3a49e01957157ed74130c756cd2ee011c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:04 +0000

    Add package sinapsis-kokoro/0.1.9/sinapsis_kokoro-0.1.9-py3-none-any.whl

---
+++ packages/sinapsis-kokoro/sinapsis_kokoro-0.1.9-py3-none-any.whl/sinapsis_kokoro/helpers/tags.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Tags(Enum):
+    AUDIO = "audio"
+    AUDIO_GENERATION = "audio_generation"
+    KOKORO = "kokoro"
+    SPEECH = "speech"
+    TEXT_TO_SPEECH = "text_to_speech"
