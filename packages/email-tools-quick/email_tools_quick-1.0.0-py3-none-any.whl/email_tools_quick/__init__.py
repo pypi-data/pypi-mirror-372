f0ae5e7e2b7ef26b19ab7c9ad926b20a5d37d30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:46 +0000

    Add package email-tools-quick/1.0.0/email_tools_quick-1.0.0-py3-none-any.whl

---
+++ packages/email-tools-quick/email_tools_quick-1.0.0-py3-none-any.whl/email_tools_quick/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2025 Cosmic-Developers-Union (CDU), All rights reserved.
+
+"""Models Description
+
+"""
+from .common import CommonClient
+from .data import EMail
+from .data import MailBox
+from .data import MailBoxMap
+from .error import BaseEmailError
+from .error import FetchEmailError
+from .error import LoginEmailError
+from .mail import IMAP4Client
+from .mail import IMAP4SSLClient
+from .mail import IMAP4_PORT
+from .mail import IMAP4_SSL_PORT
+from .mail import SocketParams
+from .ms import MSClient
+
+__version__ = '1.0.0'
+
+__all__ = [
+    "CommonClient",
+    "MSClient",
+
+    "BaseEmailError",
+    "LoginEmailError",
+    "FetchEmailError",
+]
