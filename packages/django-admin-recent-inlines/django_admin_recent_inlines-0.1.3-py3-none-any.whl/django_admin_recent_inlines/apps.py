0a34aed983ab929b440f1fc83e0fb118de49f93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:39 +0000

    Add package django-admin-recent-inlines/0.1.3/django_admin_recent_inlines-0.1.3-py3-none-any.whl

---
+++ packages/django-admin-recent-inlines/django_admin_recent_inlines-0.1.3-py3-none-any.whl/django_admin_recent_inlines/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoAdminRecentInlinesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_admin_recent_inlines"
+    verbose_name = "Django Admin Recent Inlines"
