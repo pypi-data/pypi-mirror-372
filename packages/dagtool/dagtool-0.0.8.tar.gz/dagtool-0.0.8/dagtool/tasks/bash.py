64d509e76424f8d00519c23d19dc3bc0912334d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:18 +0000

    Add package dagtool/0.0.8/dagtool-0.0.8.tar.gz

---
+++ packages/dagtool/dagtool-0.0.8.tar.gz/dagtool-0.0.8/dagtool/tasks/bash.py
@@ -0,0 +1,50 @@
+from typing import Literal
+
+try:
+    from airflow.providers.standard.operators.bash import BashOperator
+    from airflow.sdk.definitions.dag import DAG
+except ImportError:
+    from airflow.models.dag import DAG
+    from airflow.operators.bash import BashOperator
+
+from airflow.utils.task_group import TaskGroup
+from pydantic import Field
+
+from dagtool.tasks.__abc import BaseTask, Context, Operator
+
+
+class BashTask(BaseTask):
+    """Bash Task model that will represent to Airflow BashOperator object."""
+
+    uses: Literal["bash"] = Field(description="An tool type for bash model.")
+    command: str = Field(description="A bash command or bash file")
+    env: dict[str, str] | None = Field(
+        default=None,
+        description="A mapping of environment variable.",
+    )
+    append_env: bool = False
+    output_encoding: str = Field(
+        default="utf-8",
+        description="Output encoding of bash command.",
+    )
+    skip_on_exit_code: int | list[int] | None = Field(default=99)
+    cwd: str | None = None
+
+    def build(
+        self,
+        dag: DAG | None = None,
+        task_group: TaskGroup | None = None,
+        context: Context | None = None,
+    ) -> Operator:
+        """Build Airflow Bash Operator object."""
+        return BashOperator(
+            bash_command=self.command,
+            env=self.env,
+            append_env=self.append_env,
+            output_encoding=self.output_encoding,
+            skip_on_exit_code=self.skip_on_exit_code,
+            cwd=self.cwd,
+            dag=dag,
+            task_group=task_group,
+            **self.task_kwargs(),
+        )
