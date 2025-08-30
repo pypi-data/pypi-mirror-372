c6f710b5dc5d1b8eb731d9bbb32a7aad43089ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:24 +0000

    Add package pyastroweatherio/0.73.1.dev8/pyastroweatherio-0.73.1.dev8.tar.gz

---
+++ packages/pyastroweatherio/pyastroweatherio-0.73.1.dev8.tar.gz/pyastroweatherio-0.73.1.dev8/pyastroweatherio/errors.py
@@ -0,0 +1,27 @@
+"""Define package errors."""
+
+
+class AstroWeatherError(Exception):
+    """Define a base error."""
+
+    pass
+
+
+class RequestError(AstroWeatherError):
+    """Define an error related to invalid requests."""
+
+    pass
+
+
+class ResultError(AstroWeatherError):
+    """Define an error related to the result returned from a request."""
+
+    pass
+
+
+class OpenMeteoError(Exception):
+    """Generic OpenMeteo exception."""
+
+
+class OpenMeteoConnectionError(OpenMeteoError):
+    """OpenMeteo connection exception."""
