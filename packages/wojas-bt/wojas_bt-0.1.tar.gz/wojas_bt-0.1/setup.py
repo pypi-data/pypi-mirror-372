fe62bdd62cffb73b2714756cc6ddcfce0dc6f595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 06:05:59 +0000

    Add package wojas-bt/0.1/wojas_bt-0.1.tar.gz

---
+++ packages/wojas-bt/wojas_bt-0.1.tar.gz/wojas_bt-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wojas_bt",
+    version="0.1",
+    description="Create a beautiful text easily in python with wojas beautiful text",
+    author="W0jas",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        
+    ],  # brak dodatkowych zależności
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
