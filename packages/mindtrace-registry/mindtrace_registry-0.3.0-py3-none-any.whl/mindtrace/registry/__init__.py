1214fc1cf322da1b42402ab644bacb00cacd37ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 06:06:09 +0000

    Add package mindtrace-registry/0.3.0/mindtrace_registry-0.3.0-py3-none-any.whl

---
+++ packages/mindtrace-registry/mindtrace_registry-0.3.0-py3-none-any.whl/mindtrace/registry/__init__.py
@@ -0,0 +1,24 @@
+from mindtrace.registry.archivers.config_archiver import ConfigArchiver
+from mindtrace.registry.archivers.default_archivers import (
+    register_default_materializers,  # Registers default archivers to the Registry class
+)
+from mindtrace.registry.backends.gcp_registry_backend import GCPRegistryBackend
+from mindtrace.registry.backends.local_registry_backend import LocalRegistryBackend
+from mindtrace.registry.backends.minio_registry_backend import MinioRegistryBackend
+from mindtrace.registry.backends.registry_backend import RegistryBackend
+from mindtrace.registry.core.archiver import Archiver
+from mindtrace.registry.core.exceptions import LockTimeoutError
+from mindtrace.registry.core.registry import Registry
+
+__all__ = [
+    "Archiver",
+    "ConfigArchiver",
+    "LocalRegistryBackend",
+    "LockTimeoutError",
+    "GCPRegistryBackend",
+    "MinioRegistryBackend",
+    "Registry",
+    "RegistryBackend",
+]
+
+register_default_materializers()
