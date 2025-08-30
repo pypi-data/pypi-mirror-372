afc7f3465ccf4d007651a4b414b7f8e2fabf58eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:43 +0000

    Add package stringvibe/1.0.0/stringvibe-1.0.0.tar.gz

---
+++ packages/stringvibe/stringvibe-1.0.0.tar.gz/stringvibe-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stringvibe",  # this is what ppl type in pip
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],  # if u have other libs u use
+    description="Fun to play around with strings such as reverse, etc!",
+    author="chrxzrbx",
+    url="https://www.google.com",  # optional
+)
