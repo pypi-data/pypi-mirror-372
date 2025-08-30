c6f29824ffcb7a6c729abbfe77512d66d3d28321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:41 +0000

    Add package triggercmd/0.0.28/triggercmd-0.0.28.tar.gz

---
+++ packages/triggercmd/triggercmd-0.0.28.tar.gz/triggercmd-0.0.28/src/triggercmd/client.py
@@ -0,0 +1,39 @@
+import httpx
+from .exceptions import TRIGGERcmdConnectionError
+
+def trigger(token, data):
+    url = "https://www.triggercmd.com/api/run/trigger"
+    headers = {"Authorization": "Bearer " + token}
+    r = httpx.post(url, headers=headers, data=data)
+    return r
+
+def list(token):
+    url = "https://www.triggercmd.com/api/command/simplelist"
+    headers = {"Authorization": "Bearer " + token}
+    r = httpx.get(url, headers=headers)
+    return r
+
+async def async_trigger(token, data):
+    url = "https://www.triggercmd.com/api/run/trigger"
+    headers = {"Authorization": "Bearer " + token}
+    async with httpx.AsyncClient() as client:
+        r = await client.post(url, headers=headers, data=data)
+        return r
+
+async def async_list(token):
+    url = "https://www.triggercmd.com/api/command/simplelist"
+    headers = {"Authorization": "Bearer " + token}
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url, headers=headers)
+        return r
+    
+async def async_connection_test(token):
+    url = "https://www.triggercmd.com/api/ha/connection_test"
+    headers = {"Authorization": "Bearer " + token}
+    async with httpx.AsyncClient() as client:
+        try:
+            r = await client.get(url, headers=headers)
+        except Exception as e:
+            raise TRIGGERcmdConnectionError
+        else:
+            return r.status_code
