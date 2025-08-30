96b2ffe881c473cbae244706a896990f71174c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:14 +0000

    Add package pyodys/0.0.3/pyodys-0.0.3-py3-none-any.whl

---
+++ packages/pyodys/pyodys-0.0.3-py3-none-any.whl/pyodys/__init__.py
@@ -0,0 +1,3 @@
+from .solveurs.runge_kutta.TableauDeButcher import TableauDeButcher
+from .solveurs.runge_kutta.SolveurRKAvecTableauDeButcher import SolveurRKAvecTableauDeButcher
+from .systemes.EDOs import EDOs 
