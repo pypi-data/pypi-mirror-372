a9db6cca583b9b77c5bdc57c3585bb73987b2076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 06:06:05 +0000

    Add package cinetica/0.3.8/cinetica-0.3.8-py3-none-any.whl

---
+++ packages/cinetica/cinetica-0.3.8-py3-none-any.whl/cinetica/__init__.py
@@ -0,0 +1,14 @@
+from .rectilineo import MovimientoRectilineoUniforme, MovimientoRectilineoUniformementeVariado
+from .parabolico import MovimientoParabolicoBase, MovimientoParabolicoAnalisis
+from .circular import MovimientoCircularUniforme, MovimientoCircularUniformementeVariado
+
+__version__ = "0.3.8"
+
+__all__ = [
+    "MovimientoRectilineoUniforme",
+    "MovimientoRectilineoUniformementeVariado",
+    "MovimientoParabolicoBase",
+    "MovimientoParabolicoAnalisis",
+    "MovimientoCircularUniforme",
+    "MovimientoCircularUniformementeVariado",
+]
