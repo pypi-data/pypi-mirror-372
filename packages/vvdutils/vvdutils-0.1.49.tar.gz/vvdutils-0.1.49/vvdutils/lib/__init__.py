39ad12e46f34e0eed078320015526a73639951f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:49 +0000

    Add package vvdutils/0.1.49/vvdutils-0.1.49.tar.gz

---
+++ packages/vvdutils/vvdutils-0.1.49.tar.gz/vvdutils-0.1.49/vvdutils/lib/__init__.py
@@ -0,0 +1,11 @@
+from .data import DataManager, DatamanagerBuilder
+from .evaluator import ClassificationEvaluator, ClassificationMultiLabelEvaluator, MultiLabelEvaluator
+from .evaluator import ClassificationMultiClassEvaluator, MultiClassEvaluator
+from .evaluator import DetectionEvaluator
+from .utils import *
+from .labelme import *
+from .processing import *
+from .mario import *
+from .tools import *
+from .database import *
+from .communication import *
