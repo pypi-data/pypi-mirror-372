87b3cb4c5d869ae5e3f6fae55c1cc1864521c7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:10 +0000

    Add package lica/2.0.2/lica-2.0.2-py3-none-any.whl

---
+++ packages/lica/lica-2.0.2-py3-none-any.whl/lica/sqlalchemy/asyncio/dbase.py
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2024 Rafael Gonzalez.
+#
+# See the LICENSE file for details
+# ----------------------------------------------------------------------
+
+# --------------------
+# System wide imports
+# -------------------
+
+from typing import Tuple, Any
+
+# ---------------------
+# Third party libraries
+# ---------------------
+
+import decouple
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+def create_engine_sessionclass(env_var: str = "DATABASE_URL") -> Tuple[Engine,Any]:
+	url = decouple.config(env_var)
+	# 'check_same_thread' is only needed in SQLite ....
+	engine = create_async_engine(url, connect_args={"check_same_thread": False})
+	Session = async_sessionmaker(engine, expire_on_commit=False)
+	return engine, Session
+
+__all__ = ["create_engine_sessionclass"]
