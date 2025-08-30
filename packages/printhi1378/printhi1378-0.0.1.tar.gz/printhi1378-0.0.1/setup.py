6326a2d4e43ea692b89424257032600fdfa736e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:11 +0000

    Add package printhi1378/0.0.1/printhi1378-0.0.1.tar.gz

---
+++ packages/printhi1378/printhi1378-0.0.1.tar.gz/printhi1378-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="printhi1378",  # this is what ppl type in pip
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],  # if u have other libs u use
+    description="Prints hi!",
+    author="chrxzrbx",
+    url="https://www.google.com",  # optional
+)
