efd6e0f0b46e9dbee2d75d157612b59f2d9dd8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:17:14 +0000

    Add package zodchy-alchemy/0.2.1/zodchy_alchemy-0.2.1-py3-none-any.whl

---
+++ packages/zodchy-alchemy/zodchy_alchemy-0.2.1-py3-none-any.whl/zodchy_alchemy/__init__.py
@@ -0,0 +1,16 @@
+from .assemblers import (
+    QueryAssembler,
+    FilterAssembler,
+    OrdersAssembler,
+    SlicesAssembler,
+    JoinsAssembler,
+    MutationAssembler
+)
+from .contracts import (
+    Clause,
+    ClauseExpression
+)
+from . import (
+    adapters,
+    serializers
+)
