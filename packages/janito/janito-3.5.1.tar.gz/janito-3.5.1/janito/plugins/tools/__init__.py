85dd4e81dd17487c2ae6a9669ffb657657d54a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:17:12 +0000

    Add package janito/3.5.1/janito-3.5.1.tar.gz

---
+++ packages/janito/janito-3.5.1.tar.gz/janito-3.5.1/janito/plugins/tools/__init__.py
@@ -0,0 +1,10 @@
+"""
+Core tools plugin for janito.
+
+This plugin provides the essential tools for file operations, code execution,
+and system interactions that are core to janito's functionality.
+"""
+
+from .core_tools_plugin import CoreToolsPlugin
+
+__all__ = ["CoreToolsPlugin"]
