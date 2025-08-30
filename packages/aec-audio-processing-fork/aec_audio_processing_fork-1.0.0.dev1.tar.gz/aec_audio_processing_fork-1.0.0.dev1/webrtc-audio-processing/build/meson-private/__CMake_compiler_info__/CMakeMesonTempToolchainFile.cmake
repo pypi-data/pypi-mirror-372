1fd28a5d06cd7834a27aa7dd2be92ec172bf1a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:33 +0000

    Add package aec-audio-processing-fork/1.0.0.dev1/aec_audio_processing_fork-1.0.0.dev1.tar.gz

---
+++ packages/aec-audio-processing-fork/aec_audio_processing_fork-1.0.0.dev1.tar.gz/aec_audio_processing_fork-1.0.0.dev1/webrtc-audio-processing/build/meson-private/__CMake_compiler_info__/CMakeMesonTempToolchainFile.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SIZEOF_VOID_P "8")
+set(CMAKE_C_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.EXE")
+set(CMAKE_CXX_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.EXE")
