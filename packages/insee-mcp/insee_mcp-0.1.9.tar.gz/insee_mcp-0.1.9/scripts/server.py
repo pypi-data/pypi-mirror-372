78949603ab64abc8acf90861a4a7b188ce50d347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:05:50 +0000

    Add package insee-mcp/0.1.9/insee_mcp-0.1.9.tar.gz

---
+++ packages/insee-mcp/insee_mcp-0.1.9.tar.gz/insee_mcp-0.1.9/scripts/server.py
@@ -0,0 +1,42 @@
+from fastmcp import FastMCP
+from scripts.sirene import search_sirene_company
+import os
+import pynsee
+from pynsee import init_conn
+from pynsee.utils import clear_all_cache
+from dotenv import load_dotenv
+
+load_dotenv()
+init_conn(sirene_key=os.getenv("API_KEY"))
+
+def run():
+    app = FastMCP("insee-server")
+    @app.tool()
+    async def search_company(
+        company_name: str = None,
+        siren: str = None,
+        siret: str = None,
+        fuzzy: bool = True
+    ):
+        """
+        Recherche une entreprise dans la base SIRENE selon SIREN, SIRET ou nom.
+
+        Args:
+            company_name (str, optional): Nom de l'entreprise.
+            siren (str, optional): Code SIREN.
+            siret (str, optional): Code SIRET.
+            fuzzy (bool): Active la recherche approximative si nom partiel.
+
+        Returns:
+            dict: Informations clés sur l'entreprise.
+        """
+        return search_sirene_company(company_name, siren, siret, fuzzy)
+
+    # Lancement du serveur avec transport streamable-http
+    app.run(transport="streamable-http", host="127.0.0.1", port=9000, path="/mcp")
+
+if __name__ == "__main__":
+    clear_all_cache()
+    print("API_KEY:", os.getenv("API_KEY"))
+    
+    run()
