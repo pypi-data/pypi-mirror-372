15ce3dcc9f6f19efc7fee8d29327482ca380fda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 05:06:25 +0000

    Add package yandex-neurosupport/0.1.0/yandex_neurosupport-0.1.0.tar.gz

---
+++ packages/yandex-neurosupport/yandex_neurosupport-0.1.0.tar.gz/yandex_neurosupport-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yandex_neurosupport',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.0',
+    ],
+    author='Timur Sukharev',
+    author_email='tsukharev@yandex-team.ru',
+    description='Клиент для Yandex NeuroSupport API: обертка для индексации документов и генеративных ответов',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yandex/neurosupport-api',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.8',
+    keywords='yandex api neurosupport client supportgpt',
+)
