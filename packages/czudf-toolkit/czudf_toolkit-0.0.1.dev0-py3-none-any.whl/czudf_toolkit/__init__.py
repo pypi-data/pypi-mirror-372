dbd1bb7ce067d33929e2181f8b167696261d903c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 03:16:54 +0000

    Add package czudf-toolkit/0.0.1.dev0/czudf_toolkit-0.0.1.dev0-py3-none-any.whl

---
+++ packages/czudf-toolkit/czudf_toolkit-0.0.1.dev0-py3-none-any.whl/czudf_toolkit/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2025 Yunqi Inc
+# SPDX-License-Identifier: Apache-2.0
+
+from ._annotate import annotate
+from ._version import __version__
+
+__all__ = [
+    "annotate",
+    "__version__",
+]
