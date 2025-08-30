a6d2e853c5d69724b0ae2ef3bf60c3049ad1876f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-30 04:06:03 +0000

    Add package macrosynergy/1.3.5/macrosynergy-1.3.5.tar.gz

---
+++ packages/macrosynergy/macrosynergy-1.3.5.tar.gz/macrosynergy-1.3.5/macrosynergy/learning/forecasting/__init__.py
@@ -0,0 +1,55 @@
+from .linear_model import (
+    LADRegressor,
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+    ModifiedLinearRegression,
+    ModifiedSignWeightedLinearRegression,
+    ModifiedTimeWeightedLinearRegression,
+    GlobalLocalRegression,
+)
+
+from .model_systems import (
+    CorrelationVolatilitySystem,
+    LADRegressionSystem,
+    LinearRegressionSystem,
+    RidgeRegressionSystem,
+)
+
+from .naive_predictors import (
+    NaiveRegressor,
+)
+
+from .neighbors.nearest_neighbors import KNNClassifier
+
+from .meta_estimators import ProbabilityEstimator, FIExtractor, DataFrameTransformer, CountryByCountryRegression
+
+from .ensemble import (
+    VotingClassifier,
+    VotingRegressor,
+)
+
+__all__ = [
+    "LADRegressor",
+    "KNNClassifier",
+    "SignWeightedLADRegressor",
+    "TimeWeightedLADRegressor",
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+    "NaiveRegressor",
+    "CorrelationVolatilitySystem",
+    "LADRegressionSystem",
+    "LinearRegressionSystem",
+    "RidgeRegressionSystem",
+    "ModifiedLinearRegression",
+    "ModifiedSignWeightedLinearRegression",
+    "ModifiedTimeWeightedLinearRegression",
+    "ProbabilityEstimator",
+    "VotingClassifier",
+    "VotingRegressor",
+    "FIExtractor",
+    "DataFrameTransformer",
+    "GlobalLocalRegression",
+    "CountryByCountryRegression",
+]
